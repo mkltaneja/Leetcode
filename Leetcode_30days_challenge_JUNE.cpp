4612d83b06a8ba73454a75e74fbeb4cd0fd67630
unknown <mkltaneja@gmail.com>
2020-06-02 18:47:47 +0530

    DAY 2

--- Leetcode_30days_challenge_JUNE.cpp
+++ Leetcode_30days_challenge_JUNE.cpp
@@ -26,3 +26,11 @@ TreeNode *invertTree(TreeNode *root)
 
     return root;
 }
+
+//DAY 2(Delete Node in a Linked List)===============================================
+
+void deleteNode(ListNode *&node)
+{
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
