4cb5fd02e03bcda1cc0c67e102d24424c05ca99c
unknown <mkltaneja@gmail.com>
2020-06-04 03:36:59 +0530

    DAY 3 using LAMBDA OPERATOR

--- Leetcode_30days_challenge_JUNE.cpp
+++ Leetcode_30days_challenge_JUNE.cpp
@@ -63,3 +63,23 @@ int twoCitySchedCost(vector<vector<int>> &costs)
 
     return sum;
 }
+
+/////////////OR//////////////////
+
+// (by LAMBDA OPERATION custom operator)
+
+int twoCitySchedCost(vector<vector<int>> &costs)
+{
+    int n = costs.size();
+    int sum = 0;
+    sort(costs.begin(), costs.end(), [](const vector<int> &a, const vector<int> &b) {
+        return (a[0] - a[1]) < b[0] - b[1];
+    });
+
+    for (int i = 0; i < n / 2; i++)
+        sum += costs[i][0];
+    for (int i = n / 2; i < n; i++)
+        sum += costs[i][1];
+
+    return sum;
+}
