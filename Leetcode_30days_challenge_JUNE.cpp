d0b3924b290e6b1a0c1954d5e05f3b1529efda09
unknown <mkltaneja@gmail.com>
2020-06-10 18:46:10 +0530

    DAY 10 <=O(logn)

--- Leetcode_30days_challenge_JUNE.cpp
+++ Leetcode_30days_challenge_JUNE.cpp
@@ -408,7 +408,7 @@ int searchInsert(vector<int> &nums, int target)
     int l = 0;
     int r = nums.size() - 1;
 
-    while (l < r)
+    while (l <= r)
     {
         int mid = l + (r - l) / 2;
         if (nums[mid] == target)
@@ -416,7 +416,7 @@ int searchInsert(vector<int> &nums, int target)
         else if (nums[mid] < target)
             l = mid + 1;
         else if (nums[mid] > target)
-            r = target + 1;
+            r = mid - 1;
     }
     return l;
 }
