a1246fc92127eb0805dbaaf1febceed5b2e17a70
unknown <mkltaneja@gmail.com>
2020-06-12 21:00:10 +0530

    DAY 12

--- Leetcode_30days_challenge_JUNE.cpp
+++ Leetcode_30days_challenge_JUNE.cpp
@@ -424,7 +424,7 @@ int searchInsert(vector<int> &nums, int target)
 //DAY 11(Sort colors)=======================================================================================
 
 // O(n)
-// by counting 0,1,2 --> (O(n)) space
+// by counting 0,1,2 -> 2 pass algo
 void sortColors(vector<int> &nums)
 {
     int count0 = 0;
@@ -467,7 +467,7 @@ void sortColors(vector<int> &nums)
 /////////////////OR////////////////
 
 // O(n)
-// by swapping
+// by swapping --> 1 pass algo
 void sortColors(vector<int> &nums)
 {
     int i = 0, p1 = 0, p2 = nums.size() - 1;
@@ -495,3 +495,52 @@ void sortColors(vector<int> &nums)
         }
     }
 }
+
+// DAY 12(Insert Delete GetRandom O(1))====================================================================
+
+// this code will return random in O(n) and not in O(1)
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    set<int> s;     
+    RandomizedSet() 
+    {
+
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) 
+    {
+        if(s.find(val) == s.end())
+        {
+            s.insert(val);
+            return true;
+        }
+        return false;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) 
+    {
+        if(s.find(val) != s.end())
+        {
+            s.erase(val);
+            return true;
+        }
+        return false;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() 
+    {
+        return *next(s.begin(),rand()%s.size());    // --> next will iterate to the next one but rand will give any random address
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
