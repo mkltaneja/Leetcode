733a48498f9ac1b03950e25219950f2af1315315
unknown <mkltaneja@gmail.com>
2020-06-16 03:19:08 +0530

    DAY 15 method2

--- Leetcode_30days_challenge_JUNE.cpp
+++ Leetcode_30days_challenge_JUNE.cpp
@@ -802,3 +802,17 @@ TreeNode *searchBST(TreeNode *root, int val)
     findnode(root, val);
     return node;
 }
+
+////////////////OR///////////////////
+
+// Method 2 -> direct returning
+TreeNode *searchBST(TreeNode *root, int val)
+{
+    if (root == nullptr || root->val == val)
+        return root;
+    if (root->val < val)
+        return searchBST(root->right, val);
+    else
+        return searchBST(root->left, val);
+    // return root;
+}
