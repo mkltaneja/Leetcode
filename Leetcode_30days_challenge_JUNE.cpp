59d2e9c398bcb1dfe41bfb45dc578f954a5b189c
unknown <mkltaneja@gmail.com>
2020-06-29 19:28:31 +0530

    DAY 29 tab

--- Leetcode_30days_challenge_JUNE.cpp
+++ Leetcode_30days_challenge_JUNE.cpp
@@ -1570,3 +1570,25 @@ int uniquePaths(int m, int n)
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     return dfs(1, 1, n, m, dp);
 }
+
+// Method 3 (tabulation)=================================================
+int uniquePaths(int m, int n)
+{
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    for (int i = m - 1; i >= 0; i--)
+    {
+        for (int j = n - 1; j >= 0; j--)
+        {
+            if (i == m - 1 && j == n - 1)
+            {
+                dp[i][j] = 1;
+                continue;
+            }
+            if (i + 1 <= m - 1)
+                dp[i][j] += dp[i + 1][j];
+            if (j + 1 <= n - 1)
+                dp[i][j] += dp[i][j + 1];
+        }
+    }
+    return dp[0][0];
+}
