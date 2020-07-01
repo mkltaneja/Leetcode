473d5391b5f28676dfaf2e35c7e31a47df08a582
unknown <mkltaneja@gmail.com>
2020-07-01 17:44:38 +0530

    JULY DAY 30

---
+++ Leetcode_30days_challenge_JULY.cpp
@@ -0,0 +1,15 @@
+// DAY 1 (Arranging coins)=======================================
+
+// Brute force
+// O(sqrt(n))
+int arrangeCoins(int n)
+{
+    int i = 1, count = 0;
+    while ((n - i) >= 0)
+    {
+        count++;
+        n -= i;
+        i++;
+    }
+    return count;
+}
