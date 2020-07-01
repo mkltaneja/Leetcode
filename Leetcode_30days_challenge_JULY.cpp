f6eb2ea04ce4f3efec5558fa7df5f3afadac4844
unknown <mkltaneja@gmail.com>
2020-07-01 19:35:44 +0530

    DAY 1 O(logn)

--- Leetcode_30days_challenge_JULY.cpp
+++ Leetcode_30days_challenge_JULY.cpp
@@ -14,6 +14,23 @@ int arrangeCoins(int n)
     return count;
 }
 
+// O(logn)
+int arrangeCoins(int n)
+{
+    int si = 0, ei = n;
+    while (si <= ei)
+    {
+        long int mid = si + (ei - si) / 2;
+        if ((mid * (mid + 1)) / 2 == n)
+            return mid;
+        else if ((mid * (mid + 1)) / 2 < n)
+            si = mid + 1;
+        else
+            ei = mid - 1;
+    }
+    return ei;
+}
+
 ///////////////////OR////////////////////\
 
 // O(1)
