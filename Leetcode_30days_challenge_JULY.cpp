a5df48ed977cd63fbb750246fdf8569912be7aa4
unknown <mkltaneja@gmail.com>
2020-07-05 06:25:01 +0530

    DAY 4 OPTIMIZED

--- Leetcode_30days_challenge_JULY.cpp
+++ Leetcode_30days_challenge_JULY.cpp
@@ -196,3 +196,30 @@ int nthUglyNumber(int n)
     }
     return i - 1;
 }
+
+///////////////OR////////////////
+
+int nthUglyNumber(int n)
+{
+    int ugly[n];
+    ugly[0] = 1;
+    int i = 1, i2 = 0, i3 = 0, i5 = 0;
+
+    while (i < n)
+    {
+        int next2 = ugly[i2] * 2;
+        int next3 = ugly[i3] * 3;
+        int next5 = ugly[i5] * 5;
+        int next = min(next2, min(next3, next5));
+        if (next == next2)
+            i2++;
+        if (next == next3)
+            i3++;
+        if (next == next5)
+            i5++;
+        ugly[i++] = next;
+    }
+    // for(int i=0;i<n;i++)
+    //     cout<<ugly[i]<<" ";
+    return ugly[n - 1];
+}
