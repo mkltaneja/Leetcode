da743e0fd26afb05baa11b609e130f43a1f8a70c
unknown <mkltaneja@gmail.com>
2020-08-03 05:09:11 +0530

    AUGUST - DAY 2 list

--- Leetcode_30days_challenge_AUGUST.cpp
+++ Leetcode_30days_challenge_AUGUST.cpp
@@ -62,3 +62,43 @@ public:
  * obj->remove(key);
  * bool param_3 = obj->contains(key);
  */
+
+//////////////////////////OR//////////////////////////////////////
+
+// 2nd -> by using list  -->  finding is slow but insertion and deletion is fast
+int numbuckets;
+vector<list<int>> buckets;
+int hash_function(int key)
+{
+    return key % numbuckets;
+}
+
+public:
+/** Initialize your data structure here. */
+MyHashSet()
+{
+    this->numbuckets = 15000;
+    this->buckets.assign(numbuckets, list<int>());
+}
+
+void add(int key)
+{
+    int address = hash_function(key);
+    if (find(buckets[address].begin(), buckets[address].end(), key) == buckets[address].end())
+        buckets[address].push_back(key);
+}
+
+void remove(int key)
+{
+    int address = hash_function(key);
+    auto itr = find(buckets[address].begin(), buckets[address].end(), key);
+    if (itr != buckets[address].end())
+        buckets[address].erase(itr);
+}
+
+/** Returns true if this set contains the specified element */
+bool contains(int key)
+{
+    int address = hash_function(key);
+    return (find(buckets[address].begin(), buckets[address].end(), key) != buckets[address].end());
+}
