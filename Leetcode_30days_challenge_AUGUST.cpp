71545f0b65c9f601ebdc1996a5bcf0b1a5959d4a
MUKUL <mkltaneja@gmail.com>
2020-08-26 19:18:35 +0530

    DAY 26 m2

--- Leetcode_30days_challenge_AUGUST.cpp
+++ Leetcode_30days_challenge_AUGUST.cpp
@@ -1468,3 +1468,24 @@ vector<string> fizzBuzz(int n)
     }
     return ans;
 }
+
+///////////////////////////////OR/////////////////////////
+
+// Method 2
+
+vector<string> fizzBuzz(int n) 
+{
+    vector<string> ans(n);
+    for(int i = 1 ; i <= n; i++)
+    {
+        string temp = "";
+        if(i % 3 == 0)
+            temp += "Fizz";
+        if(i % 5 == 0)
+            temp += "Buzz";
+        if(temp == "")
+            temp += to_string(i);
+        ans[i-1] = temp;
+    }
+    return ans;
+}
