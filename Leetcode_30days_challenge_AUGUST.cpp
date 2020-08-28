c49e0acc7c0471ac1ddf73942d2193107978bdff
MUKUL <mkltaneja@gmail.com>
2020-08-28 23:58:55 +0530

    DAY 28 m2

--- Leetcode_30days_challenge_AUGUST.cpp
+++ Leetcode_30days_challenge_AUGUST.cpp
@@ -1574,3 +1574,19 @@ int rand10()
 {
     return rand() % 10 + 1;
 }
+
+////////////////////////////////////////OR/////////////////////////////////
+
+// Method 2(actual approach)
+int rand10() 
+{
+    int a = rand7();
+    while(a > 5)
+        a = rand7();
+    
+    int b = rand7();
+    while(b == 7)
+        b = rand7();
+    
+    return b <= 3 ? a : a + 5;
+}
