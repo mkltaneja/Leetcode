189f7d2bff4f6915f3ad790692ac2e9b0fbe335a
MUKUL <mkltaneja@gmail.com>
2020-09-13 22:59:30 +0530

    DAY 13

--- Leetcode_30days_challenge_SEPTEMBER.cpp
+++ Leetcode_30days_challenge_SEPTEMBER.cpp
@@ -543,3 +543,28 @@ vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInter
         res.push_back({st,end});
     return res;
 }
+
+//////////////////////////////////////////////OR///////////////////////////////////
+
+// METHOD 2
+vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+{
+    int n = intervals.size(), i = 0;
+    int st = newInterval[0], end = newInterval[1];
+    vector<vector<int>> res;
+    
+    while(i < n && intervals[i][1] < st)
+        res.push_back({intervals[i][0], intervals[i++][1]});
+    
+    while(i < n && intervals[i][0] <= end)
+    {
+        st = min(st, intervals[i][0]);
+        end = max(end, intervals[i++][1]);
+    }
+    res.push_back({st,end});
+        
+    while(i < n)
+        res.push_back({intervals[i][0],intervals[i++][1]});
+    
+    return res;
+}
