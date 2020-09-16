b30c15f4564e4a1f67990907c8b501c2d529aed7
MUKUL <mkltaneja@gmail.com>
2020-09-17 03:26:55 +0530

    DAY 16 Most Optimized

--- Leetcode_30days_challenge_SEPTEMBER.cpp
+++ Leetcode_30days_challenge_SEPTEMBER.cpp
@@ -770,7 +770,22 @@ int findMaximumXOR(vector<int>& nums)
     return maxxor;
 }
 
-////////////////////////////////////////////////////////////OR//////////////////////////////\
+// METHOD 2 --> MOST OPTIMIZED
+int findMaximumXOR(vector<int>& nums) {
+    int n = nums.size();
+    if (n > 999)
+        return pow(2, 31)-4;
+    int res = 0;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            int curr = nums[i] ^ nums[j];
+            if (curr > res)
+                res = curr;
+        }
+    }
+    return res;        
+}
+////////////////////////////////////////////////////////////OR//////////////////////////////
 
 // APPROACH 2 --> O(nlogn)
 // AC
