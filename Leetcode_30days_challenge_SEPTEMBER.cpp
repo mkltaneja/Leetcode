218afe319f8859e073d193faba1638664d61ccf7
MUKUL <mkltaneja@gmail.com>
2020-09-22 16:28:23 +0530

    DAY 22 OPTIMIZED

--- Leetcode_30days_challenge_SEPTEMBER.cpp
+++ Leetcode_30days_challenge_SEPTEMBER.cpp
@@ -1009,3 +1009,49 @@ vector<int> majorityElement(vector<int>& nums)
             ans.push_back(itr.first);
     return ans;
 }
+
+/////////////////////////////////////////////OR//////////////////////////////////
+
+// APPROACH 2 --> O(n)
+vector<int> majorityElement(vector<int>& nums) 
+{
+    int ele1, ele2;
+    int count1 = 0, count2 = 0;
+    for(int i : nums)
+    {
+        if(ele1 == i)
+            count1++;
+        else if(ele2 == i)
+            count2++;
+        else if(count1 == 0)
+        {
+            ele1 = i;
+            count1 = 1;
+        }
+        else if(count2 == 0)
+        {
+            ele2 = i;
+            count2 = 1;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
+    }
+    int c1 = 0, c2 = 0;
+    for(int i : nums)
+    {
+        if(i == ele1)
+            c1++;
+        else if(i == ele2)
+            c2++;
+    }
+    int n = nums.size()/3;
+    vector<int> ans;
+    if(c1 > n)
+        ans.push_back(ele1);
+    if(c2 > n)
+        ans.push_back(ele2);
+    return ans;
+}
