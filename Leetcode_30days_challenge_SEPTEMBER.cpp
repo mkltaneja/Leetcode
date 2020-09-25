f2dcbdff795599dcc824ca0a029722e952166b5a
MUKUL <mkltaneja@gmail.com>
2020-09-25 20:31:58 +0530

    DAY 25

--- Leetcode_30days_challenge_SEPTEMBER.cpp
+++ Leetcode_30days_challenge_SEPTEMBER.cpp
@@ -1170,3 +1170,23 @@ char findTheDifference(string s, string t)
         ans ^= c;
     return ans;
 }
+
+// DAY 25 (Largest Number)======================================================================
+
+string largestNumber(vector<int>& nums) 
+{
+    int n = nums.size();
+    vector<string> res(n);
+    for(int i = 0; i < n; i++)
+        res[i] = to_string(nums[i]);
+    sort(res.begin(), res.end(), [](const string &a, const string &b)
+            {
+                return (a + b) > (b + a);
+            });
+    if(res[0] == "0")
+        return "0";
+    string ans = "";
+    for(string s : res)
+        ans += s;
+    return ans;
+}
