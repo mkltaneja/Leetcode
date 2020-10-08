d3cff4c9fbd7bee7febe1b3106baebda2bd95fda
Mukul <mkltaneja@gmail.com>
2020-10-08 13:34:31 +0530

    DAY 8 OPTIMIZED

--- Leetcode_30days_challenge_OCTOBER.cpp
+++ Leetcode_30days_challenge_OCTOBER.cpp
@@ -285,3 +285,20 @@ int search(vector<int> &nums, int target)
             return i;
     return -1;
 }
+
+// METHOD 2 --> O(log n)
+int search(vector<int> &nums, int target)
+{
+    int si = 0, ei = nums.size() - 1;
+    while (si <= ei)
+    {
+        int mid = (si + ei) / 2;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] < target)
+            si = mid + 1;
+        else
+            ei = mid - 1;
+    }
+    return -1;
+}
