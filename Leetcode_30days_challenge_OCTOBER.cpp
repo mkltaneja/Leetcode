d0b02643f27d21a48cf90b53230b2d5f1d20461d
Mukul <mkltaneja@gmail.com>
2020-10-17 02:32:56 +0530

    DAY 16 METHOD 2

--- Leetcode_30days_challenge_OCTOBER.cpp
+++ Leetcode_30days_challenge_OCTOBER.cpp
@@ -634,3 +634,18 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
     }
     return false;
 }
+
+// METHOD 2 (Linear Search) --> O(n*m)
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size();
+    if (n == 0)
+        return false;
+    int m = matrix[0].size();
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (matrix[i][j] == target)
+                return true;
+    return false;
+}
