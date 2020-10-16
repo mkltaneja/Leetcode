656bcfc4b7a2ded4317e07a1f45ef1f0be83c80a
Mukul <mkltaneja@gmail.com>
2020-10-17 02:29:58 +0530

    DAY 16

--- Leetcode_30days_challenge_OCTOBER.cpp
+++ Leetcode_30days_challenge_OCTOBER.cpp
@@ -608,3 +608,29 @@ void rotate(vector<int> &nums, int k)
 
     nums = res;
 }
+
+// $ DAY 16 (Search a 2D Matrix)==========================================================
+
+// Method 1 (Binary Search) --> O(log(n*m))
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size();
+    if (n == 0)
+        return false;
+    int m = matrix[0].size();
+    int si = 0, ei = n * m - 1;
+
+    while (si <= ei)
+    {
+        int mid = (si + ei) / 2;
+        int r = mid / m;
+        int c = mid % m;
+        if (matrix[r][c] == target)
+            return true;
+        if (matrix[r][c] < target)
+            si = mid + 1;
+        else
+            ei = mid - 1;
+    }
+    return false;
+}
