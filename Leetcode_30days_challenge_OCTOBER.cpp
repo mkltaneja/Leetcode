a2ca260a38fc20e11d36d0e7f9131d3e8786b01b
Mukul <mkltaneja@gmail.com>
2020-10-24 04:34:22 +0530

    DAY 23

--- Leetcode_30days_challenge_OCTOBER.cpp
+++ Leetcode_30days_challenge_OCTOBER.cpp
@@ -959,3 +959,24 @@ int minDepth(TreeNode *root)
 
     return min(minDepth(root->left), minDepth(root->right)) + 1;
 }
+
+// $ DAY 23 (132 Pattern)============================================================
+
+// METHOD 1 (Using Stack) --> O(n)------------------------------
+bool find132pattern(vector<int> &nums)
+{
+    stack<int> st;
+    int two = INT_MIN;
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        if (nums[i] < two)
+            return true;
+        while (!st.empty() && st.top() < nums[i])
+        {
+            two = st.top();
+            st.pop();
+        }
+        st.push(nums[i]);
+    }
+    return false;
+}
