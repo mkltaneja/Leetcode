dfa007ab8b5b1f1e6eac8f65dcad7ab175e94dcf
Mukul <mkltaneja@gmail.com>
2020-11-03 20:45:38 +0530

    DAY 2

--- Leetcode_30days_challenge_NOVEMBER.cpp
+++ Leetcode_30days_challenge_NOVEMBER.cpp
@@ -1,5 +1,6 @@
 
 // $ DAY 1 (Convert Binary Number in a Linked List to Integer)========================================================
+
 int getDecimalValue(ListNode *head)
 {
     int ans = 0;
@@ -10,4 +11,25 @@ int getDecimalValue(ListNode *head)
         temp = temp->next;
     }
     return ans;
+}
+
+// $ DAY 2 (Insertion Sort List)========================================================================
+
+ListNode *insertionSortList(ListNode *head)
+{
+    ListNode *ans = new ListNode(INT_MIN);
+
+    while (head)
+    {
+        ListNode *tmp = ans;
+        while (tmp->next && tmp->next->val < head->val)
+            tmp = tmp->next;
+
+        ListNode *nextnode = head->next;
+        head->next = tmp->next;
+        tmp->next = head;
+        head = nextnode;
+    }
+
+    return ans->next;
 }
