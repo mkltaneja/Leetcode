12e71506a7ad6f441741f0882c2baeb42f2b56d5
Mukul <mkltaneja@gmail.com>
2020-11-11 11:30:57 +0530

    DAY 10

--- Leetcode_30days_challenge_NOVEMBER.cpp
+++ Leetcode_30days_challenge_NOVEMBER.cpp
@@ -322,3 +322,21 @@ int maxAncestorDiff(TreeNode* root)
     
     return maxdiff;
 }
+
+// DAY 10 ()=====================================================================
+
+// METHOD 1 
+vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) 
+{
+    for(int i = 0; i < A.size(); i++)
+    {
+        int j = 0, k = A[i].size()-1;
+        while(j < k)
+            swap(A[i][j++], A[i][k--]);
+    }
+    
+    for(int i = 0; i < A.size(); i++)
+        for(int j = 0; j < A[i].size(); j++)
+            A[i][j] = (A[i][j] == 0) ? 1 : 0;
+    return A;
+}
