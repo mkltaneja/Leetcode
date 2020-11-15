a6c5ebff9100194bf967bfc1db6c477f0a4cfeac
Mukul <mkltaneja@gmail.com>
2020-11-16 00:21:00 +0530

    DAY 15

--- Leetcode_30days_challenge_NOVEMBER.cpp
+++ Leetcode_30days_challenge_NOVEMBER.cpp
@@ -450,3 +450,23 @@ Node* connect(Node* root)
     }
     return root;
 }
+
+// DAY 14 ()======================================================================
+
+
+
+// DAY 15 (Range Sum of BST)======================================================================
+
+// METHOD 1 --> O(n)
+int rangeSumBST(TreeNode* root, int low, int high) 
+{
+    if(!root)
+        return 0;
+    int sum = 0;
+    if(root->val >= low && root->val <= high)
+        sum += root->val;
+    sum += rangeSumBST(root->left, low, high);
+    sum += rangeSumBST(root->right, low, high);
+    
+    return sum;
+}
