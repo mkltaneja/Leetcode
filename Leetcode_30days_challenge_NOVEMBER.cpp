5f28e695b70e99bb7a3760203eeb5e7ce2847317
Mukul <mkltaneja@gmail.com>
2020-11-16 00:23:04 +0530

    DAY 15 OPTIMIZED

--- Leetcode_30days_challenge_NOVEMBER.cpp
+++ Leetcode_30days_challenge_NOVEMBER.cpp
@@ -470,3 +470,19 @@ int rangeSumBST(TreeNode* root, int low, int high)
     
     return sum;
 }
+
+// METHOD 2 -->  < O(n)
+int rangeSumBST(TreeNode* root, int low, int high) 
+{
+    if(!root)
+        return 0;
+    int sum = 0;
+    if(root->val >= low && root->val <= high)
+        sum += root->val;
+    if(root->val >= low)
+        sum += rangeSumBST(root->left, low, high);
+    if(root->val <= high)
+        sum += rangeSumBST(root->right, low, high);
+    
+    return sum;
+}
