552c6895ec261cc4111e530050b52299f62c6060
Mukul <mkltaneja@gmail.com>
2020-12-26 20:16:03 +0530

    DAY 26 METHOD 2

--- Leetcode_30days_challenge_DECEMBER.cpp
+++ Leetcode_30days_challenge_DECEMBER.cpp
@@ -1263,3 +1263,34 @@ int numDecodings(string s)
 {
     return countways(0, s);
 }
+
+// METHOD 2 (DP - Memoized)
+
+int countways(int i, string &s, vector<int> &dp)
+{
+    if (i >= s.size())
+        return 1;
+    if (s[i] == '0')
+        return dp[i] = 0;
+    if (i == s.size() - 1)
+        return 1;
+    if (dp[i] != -1)
+        return dp[i];
+
+    int count = 0;
+    count += countways(i + 1, s, dp);
+
+    int num = s[i] - '0';
+    num *= 10;
+    num += s[i + 1] - '0';
+    if (num <= 26)
+        count += countways(i + 2, s, dp);
+
+    return dp[i] = count;
+}
+
+int numDecodings(string s)
+{
+    vector<int> dp(s.size(), -1);
+    return countways(0, s, dp);
+}
