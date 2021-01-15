b0f664f427dca085a5fba773034488611afceb2e
Mukul <mkltaneja@gmail.com>
2021-01-16 00:46:33 +0530

    DAY 15

--- Leetcode - 30-days Challenge 2021/JANUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JANUARY Challenge.cpp
@@ -548,3 +548,26 @@ int minOperations(vector<int> &nums, int x)
         return minops;
     return -1;
 }
+
+// DAY 15 (Get Maximum in Generated Array)====================================================
+
+int getMaximumGenerated(int n)
+{
+    if (n == 0)
+        return 0;
+    vector<int> arr(n + 1);
+    arr[0] = 0;
+    arr[1] = 1;
+    int maxi = 1;
+    for (int i = 1; 2 * i <= n; i++)
+    {
+        arr[2 * i] = arr[i];
+        maxi = max(maxi, arr[2 * i]);
+        if (2 * i + 1 <= n)
+        {
+            arr[2 * i + 1] = arr[i] + arr[i + 1];
+            maxi = max(maxi, arr[2 * i + 1]);
+        }
+    }
+    return maxi;
+}
