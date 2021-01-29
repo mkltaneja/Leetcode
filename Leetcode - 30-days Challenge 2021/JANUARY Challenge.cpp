66181788ce881b6b7ee1a050bc7193f7a7af2ddc
Mukul <mkltaneja@gmail.com>
2021-01-29 19:08:25 +0530

    DAY 29

--- Leetcode - 30-days Challenge 2021/JANUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JANUARY Challenge.cpp
@@ -1378,3 +1378,58 @@ string getSmallestString(int n, int k)
     }
     return ans;
 }
+
+// DAY 29 (Vertical Order Traversal of a Binary Tree)=========================================================
+
+void dfs(TreeNode *node, int width, int &lm, int &rm)
+{
+    if (!node)
+        return;
+    lm = min(lm, width);
+    rm = max(rm, width);
+    dfs(node->left, width - 1, lm, rm);
+    dfs(node->right, width + 1, lm, rm);
+}
+
+void bfs(TreeNode *node, vector<vector<int>> &ans, int lm, int rm)
+{
+    queue<pair<TreeNode *, int>> que;
+    que.push({node, 0});
+    ans[-lm].push_back(node->val);
+    while (!que.empty())
+    {
+        vector<vector<int>> tmp(ans.size(), vector<int>());
+        int sz = que.size();
+        while (sz--)
+        {
+            auto tp = que.front();
+            que.pop();
+            if (tp.first->left)
+            {
+                que.push({tp.first->left, tp.second - 1});
+                tmp[-lm + (tp.second - 1)].push_back(tp.first->left->val);
+            }
+            if (tp.first->right)
+            {
+                que.push({tp.first->right, tp.second + 1});
+                tmp[-lm + (tp.second + 1)].push_back(tp.first->right->val);
+            }
+        }
+        for (int i = 0; i < tmp.size(); i++)
+        {
+            sort(tmp[i].begin(), tmp[i].end());
+            for (int j = 0; j < tmp[i].size(); j++)
+                ans[i].push_back(tmp[i][j]);
+        }
+    }
+}
+
+vector<vector<int>> verticalTraversal(TreeNode *root)
+{
+    int leftmost = INT_MAX, rightmost = INT_MIN;
+    dfs(root, 0, leftmost, rightmost);
+    // cout<<leftmost<<" "<<rightmost<<endl;
+    vector<vector<int>> ans(rightmost - leftmost + 1, vector<int>());
+    bfs(root, ans, leftmost, rightmost);
+    return ans;
+}
