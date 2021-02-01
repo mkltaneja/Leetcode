f6575d9ed00adf3f58f99f03e8793f2d01a4269e
Mukul <mkltaneja@gmail.com>
2021-02-01 18:05:52 +0530

    FEBRUARY Challenge - DAY 1 Java

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -1,5 +1,6 @@
 // DAY 1 (Number of 1 Bits)================================================================
 
+// in C++
 int hammingWeight(uint32_t n) 
 {
     int cnt = 0;
@@ -9,4 +10,26 @@ int hammingWeight(uint32_t n)
         n &= (n-1);
     }
     return cnt;
+}
+
+// in Java
+// you need to treat n as an unsigned value
+public int hammingWeight(int n) {
+    int cnt = 0;
+    if((n & (1<<31)) != 0)
+    {        
+        n ^= (1<<31);
+        cnt++;
+    }
+    System.out.println(n);
+    while(n>0)
+    {
+        cnt++;
+        n &= (n-1);
+    }
+    return cnt;
+}
+// OR
+public int hammingWeight(int n) {
+    return Integer.bitCount(n);
 }
