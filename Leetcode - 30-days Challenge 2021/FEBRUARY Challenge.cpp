095dd8561f8b7b3b410080ef04af3ce455be3a85
Mukul <mkltaneja@gmail.com>
2021-02-03 02:09:49 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -1,35 +1,54 @@
 // DAY 1 (Number of 1 Bits)================================================================
 
 // in C++
-int hammingWeight(uint32_t n) 
+int hammingWeight(uint32_t n)
 {
     int cnt = 0;
-    while(n)
+    while (n)
     {
         cnt++;
-        n &= (n-1);
+        n &= (n - 1);
     }
     return cnt;
 }
 
 // in Java
 // you need to treat n as an unsigned value
-public int hammingWeight(int n) {
+public
+int hammingWeight(int n)
+{
     int cnt = 0;
-    if((n & (1<<31)) != 0)
-    {        
-        n ^= (1<<31);
+    if ((n & (1 << 31)) != 0)
+    {
+        n ^= (1 << 31);
         cnt++;
     }
     System.out.println(n);
-    while(n>0)
+    while (n > 0)
     {
         cnt++;
-        n &= (n-1);
+        n &= (n - 1);
     }
     return cnt;
 }
 // OR
-public int hammingWeight(int n) {
+public
+int hammingWeight(int n)
+{
     return Integer.bitCount(n);
+}
+
+// DAY 2 (Trim a Binary Search Tree)=====================================================
+
+TreeNode *trimBST(TreeNode *root, int low, int high)
+{
+    if (!root)
+        return nullptr;
+    if (root->val < low)
+        return trimBST(root->right, low, high);
+    if (root->val > high)
+        return trimBST(root->left, low, high);
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    return root;
 }
