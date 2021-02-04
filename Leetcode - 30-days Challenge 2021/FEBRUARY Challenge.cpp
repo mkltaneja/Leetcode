d51986341f8496dab0b23fa81f0b357190251a27
Mukul <mkltaneja@gmail.com>
2021-02-04 18:02:14 +0530

    DAY 4

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -67,3 +67,32 @@ bool hasCycle(ListNode *head)
     }
     return false;
 }
+
+// DAY 4 (Longest Harmonious Subsequence)===============================================================
+
+// APPROACH 1 (taking record of maximum and minimum) --> O(n^2)
+int findLHS(vector<int> &nums)
+{
+    int n = nums.size();
+    int maxlen = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a = nums[i], b = nums[i] - 1, c = nums[i] + 1;
+        int ca = 1, cb = 0, cc = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[j] == a)
+                ca++;
+            else if (nums[j] == b)
+                cb++;
+            else if (nums[j] == c)
+                cc++;
+
+            if (cb != 0)
+                maxlen = max(maxlen, ca + cb);
+            if (cc != 0)
+                maxlen = max(maxlen, ca + cc);
+        }
+    }
+    return maxlen;
+}
