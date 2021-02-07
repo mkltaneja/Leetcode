2794573d3ba1dce52966298c8d0da448a4f7553d
Mukul <mkltaneja@gmail.com>
2021-02-07 09:52:23 +0530

    Weekly Contest 227 - A

---
+++ Weekly_Contest_#227/1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,26 @@
+
+bool check(vector<int> &nums)
+{
+    int n = nums.size();
+    int idx = -1;
+    bool dec = false;
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            dec = true;
+            idx = i;
+            break;
+        }
+    }
+    if (dec == false)
+        return true;
+    int cnt = 0;
+    vector<int> sor(n);
+    for (int i = idx; cnt < n; i = (i + 1) % n)
+    {
+        sor[cnt++] = nums[i];
+    }
+    sort(nums.begin(), nums.end());
+    return nums == sor;
+}
