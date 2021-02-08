0e0dae0e5cd477069061d2e774ce2cc04ca21f0b
Mukul <mkltaneja@gmail.com>
2021-02-08 18:00:03 +0530

    DAY 8

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -251,3 +251,54 @@ vector<int> shortestToChar(string s, char c)
         a[i] = min(a[i], b[i]);
     return a;
 }
+
+// DAY 8 (Peeking Iterator)=====================================================
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    int nxt;
+    bool hasnxt;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    nxt = Iterator::next();
+        hasnxt = Iterator::hasNext();
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return nxt;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    int ans = nxt;
+        hasnxt = Iterator::hasNext();
+        if(hasnxt)
+            nxt = Iterator::next();
+        return ans;
+	}
+	
+	bool hasNext() const {
+	    return hasnxt;
+	}
+};
