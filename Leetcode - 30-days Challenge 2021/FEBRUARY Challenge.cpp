d3b239a6499f160f47ae2ada44c5204014d4e0a5
Mukul <mkltaneja@gmail.com>
2021-02-09 19:13:10 +0530

    DAY 9 OPTIMIZD

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -332,3 +332,19 @@ TreeNode *convertBST(TreeNode *root)
     convertBST(root, 0);
     return root;
 }
+
+// METHOD 2 --> O(n)
+
+int sum = 0;
+TreeNode *convertBST(TreeNode *root)
+{
+    if (!root)
+        return nullptr;
+
+    convertBST(root->right);
+    sum += root->val;
+    root->val = sum;
+    convertBST(root->left);
+
+    return root;
+}
