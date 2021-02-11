327f7bd30ba54eeb22842590c0b4bb072379c3e5
Mukul <mkltaneja@gmail.com>
2021-02-11 14:57:00 +0530

    DAY 11

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -416,3 +416,20 @@ Node *copyRandomList(Node *head)
     }
     return ans;
 }
+
+// DAY 11 (Valid Anagram)======================================================
+
+bool isAnagram(string s, string t) 
+{
+    if(s.size() != t.size())
+        return false;
+    vector<int> m(26,0);
+    for(char c : s) m[c-'a']++;
+    for(char c : t) m[c-'a']--;
+    
+    for(int x : m)
+        if(x != 0)
+            return false;
+    
+    return true;
+}
