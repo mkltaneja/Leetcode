9334ff65a2dc3b40efa91ecbc87a1da21b5a86d4
Mukul <mkltaneja@gmail.com>
2021-02-14 11:26:32 +0530

    Weekly Contest 227 - D - OPTIMIZED

--- Weekly_Contest_#228/1761_Minimum_Degree_of_a_Connected_Trio_in_a_Graph.cpp
+++ Weekly_Contest_#228/1761_Minimum_Degree_of_a_Connected_Trio_in_a_Graph.cpp
@@ -1,5 +1,6 @@
 
-// METHOD 1 (Naive)
+// METHOD 1 (Naive) >O(n^3)
+// TLE
 
 int indeg(int i, vector<vector<int>> &gp)
 {
@@ -64,4 +65,27 @@ int minTrioDegree(int n, vector<vector<int>> &edges)
     }
     cout << endl;
     return hastrio ? mindeg : -1;
+}
+
+// APPROACH 2 (By making the list of connected edges and counting the degree of every vertex) --> O(n^3)
+
+int minTrioDegree(int n, vector<vector<int>> &edges)
+{
+    vector<vector<bool>> e(n + 1, vector<bool>(n + 1, 0));
+    vector<int> d(n + 1, 0);
+    for (vector<int> v : edges)
+    {
+        e[v[0]][v[1]] = e[v[1]][v[0]] = 1;
+        d[v[0]]++;
+        d[v[1]]++;
+    }
+
+    int ans = INT_MAX;
+    for (int i = 1; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (e[i][j])
+                for (int k = j + 1; k <= n; k++)
+                    if (e[j][k] && e[i][k])
+                        ans = min(ans, d[i] + d[j] + d[k] - 6);
+    return (ans == INT_MAX) ? -1 : ans;
 }
