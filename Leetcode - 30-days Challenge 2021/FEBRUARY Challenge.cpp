a929197483bfac1dc14230036e2915b488d9f1f1
Mukul <mkltaneja@gmail.com>
2021-02-20 14:54:58 +0530

    DAY 20

--- Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/FEBRUARY Challenge.cpp
@@ -783,3 +783,24 @@ string minRemoveToMakeValid(string s)
     }
     return ans;
 }
+
+// DAY 20 (Roman To Intger)==================================================================
+
+int romanToInt(string s)
+{
+    unordered_map<char, int> m;
+    m['I'] = 1, m['V'] = 5, m['X'] = 10, m['L'] = 50, m['C'] = 100, m['D'] = 500, m['M'] = 1000;
+
+    char p = NULL;
+    int ans = 0;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        int num = toint(s[i]);
+        if (p == NULL || num >= m[p])
+            ans += m[s[i]];
+        else
+            ans -= m[s[i]];
+        p = s[i];
+    }
+    return ans;
+}
