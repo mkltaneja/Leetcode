c3c18fd28d6dcb0948a444aa649cdd21fc67bfa3
Mukul <mkltaneja@gmail.com>
2021-02-21 13:52:52 +0530

    Weekly Contest 229 - B OPTIMIZED

--- Weekly_Contest_#229/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
+++ Weekly_Contest_#229/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
@@ -1,4 +1,5 @@
 
+// APPROACH 1 (Using Map) --> O(n^2)
 vector<int> minOperations(string boxes)
 {
     int n = boxes.size();
@@ -12,5 +13,25 @@ vector<int> minOperations(string boxes)
         for (int x : st)
             // cout<<x-i<<" ";
             ans[i] += abs(x - i);
+    return ans;
+}
+
+// APPROACH 2 (by storing total jumps before and after ith position) --> O(n)
+vector<int> minOperations(string boxes)
+{
+    int n = boxes.size();
+    vector<int> ans(n, 0);
+    for (int i = 0, ones = 0, jumps = 0; i < n; i++)
+    {
+        jumps += ones;
+        ans[i] = jumps;
+        ones += boxes[i] - '0';
+    }
+    for (int i = n - 1, ones = 0, jumps = 0; i >= 0; i--)
+    {
+        jumps += ones;
+        ans[i] += jumps;
+        ones += boxes[i] - '0';
+    }
     return ans;
 }
