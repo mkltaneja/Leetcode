4d091b15d721ec7b00f067184200814170fdfa04
Mukul <mkltaneja@gmail.com>
2021-02-28 13:47:16 +0530

    Weekly Constest 230 - B OPTIMIZED

--- Weekly_Contest_#230/1774_Closest_Dessert_Cost.cpp
+++ Weekly_Contest_#230/1774_Closest_Dessert_Cost.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Coin change) --> <<O(n^n)
+
 int minabs = INT_MAX, ans;
 
 void coin_change(int idx, int m, int val, int tar, vector<int> &coins)
@@ -28,5 +30,22 @@ int closestCost(vector<int> &baseCosts, vector<int> &toppingCosts, int target)
     ans = target;
     for (int i = 0; i < n; i++)
         coin_change(0, m, a[i], target, b);
+    return ans;
+}
+
+// APPROACH 2 (Using "bitset" Data Structure to store all the possible costs (including 0, 1, 2 amount of every topping)) --> O(n)
+
+int closestCost(vector<int> &baseCosts, vector<int> &toppingCosts, int target)
+{
+    bitset<20005> f;
+    for (int x : baseCosts)
+        f[x] = 1;
+    for (int x : toppingCosts)
+        f |= (f << x) | (f << 2 * x);
+
+    int ans = -10000;
+    for (int x = 0; x <= 20000; ++x)
+        if (f[x] && abs(x - target) < abs(ans - target))
+            ans = x;
     return ans;
 }
