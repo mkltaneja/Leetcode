c719e9cc7a50d232b6ae24b19956971fc9720877
Mukul <mkltaneja@gmail.com>
2021-03-02 01:59:04 +0530

    DAY 1 OPTIMIZED

--- Leetcode - 30-days Challenge 2021/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MARCH Challenge.cpp
@@ -9,3 +9,16 @@ int distributeCandies(vector<int> &candyType)
         s.insert(x);
     return (s.size() < n / 2) ? s.size() : n / 2;
 }
+
+// METHOD 2 (Using Bitset) --> O(n)
+
+int distributeCandies(vector<int> &candyType)
+{
+    int n = candyType.size();
+    bitset<200005> f;
+    for (int x : candyType)
+        f.set(x + 100000);
+    int count = f.count();
+    // cout<<count<<endl;
+    return (count < n / 2) ? count : n / 2;
+}
