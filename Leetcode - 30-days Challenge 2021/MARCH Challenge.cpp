0b3a51aff40ef50b269c446f8dcb9caee94e3943
Mukul <mkltaneja@gmail.com>
2021-03-19 01:14:15 +0530

    DAY 18

--- Leetcode - 30-days Challenge 2021/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MARCH Challenge.cpp
@@ -765,3 +765,33 @@ public:
  * Solution* obj = new Solution(radius, x_center, y_center);
  * vector<double> param_1 = obj->randPoint();
  */
+
+// DAY 18 (Wiggle Sequence)========================================================================
+
+// METHOD 1 (DP -- Storing ups and downs till ith position)
+int wiggleMaxLength(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> up(n), down(n);
+    up[0] = down[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] > nums[i - 1])
+        {
+            up[i] = down[i - 1] + 1;
+            down[i] = down[i - 1];
+        }
+        else if (nums[i] < nums[i - 1])
+        {
+            down[i] = up[i - 1] + 1;
+            up[i] = up[i - 1];
+        }
+        else
+        {
+            up[i] = up[i - 1];
+            down[i] = down[i - 1];
+        }
+        // cout<<nums[i]<<" --> "<<up[i]<<" "<<down[i]<<endl;
+    }
+    return max(up[n - 1], down[n - 1]);
+}
