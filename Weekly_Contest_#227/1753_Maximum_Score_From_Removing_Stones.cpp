7dfe9ddbff06859f7279b0c73b9c7f99dddcb02c
Mukul <mkltaneja@gmail.com>
2021-03-26 00:14:12 +0530

    UPDATE -Using priority Queue

--- Weekly_Contest_#227/1753_Maximum_Score_From_Removing_Stones.cpp
+++ Weekly_Contest_#227/1753_Maximum_Score_From_Removing_Stones.cpp
@@ -1,4 +1,31 @@
+// APPROACH 1 (Using Priority Queue) --> O(d[1])
 
+int maximumScore(int a, int b, int c)
+{
+    priority_queue<int> pq;
+    pq.push(a);
+    pq.push(b);
+    pq.push(c);
+
+    int cnt = 0;
+    while (pq.size() > 1)
+    {
+        int x = pq.top();
+        pq.pop();
+        int y = pq.top();
+        pq.pop();
+        // cout<<x<<" "<<y<<endl;
+        if (x - 1 > 0)
+            pq.push(x - 1);
+        if (y - 1 > 0)
+            pq.push(y - 1);
+
+        cnt++;
+    }
+    return cnt;
+}
+
+// APPROACH 2 (Using array)  --> O(d[1])
 int maximumScore(int a, int b, int c)
 {
     int d[3] = {a, b, c};
