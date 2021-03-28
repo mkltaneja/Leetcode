f65525d84e6eeaf3a7b0189c012960911d1b40f7
Mukul <mkltaneja@gmail.com>
2021-03-28 10:59:04 +0530

    Weekly Contest 234 B Method 2 OPTIMIZED

--- Weekly_Contest_#234/1806_Minimum_Number_of_Operations_to_Reinitialize_a_Permutation.cpp
+++ Weekly_Contest_#234/1806_Minimum_Number_of_Operations_to_Reinitialize_a_Permutation.cpp
@@ -1,4 +1,6 @@
 
+// METHOD 1 (Brute Force) --> O(n^2)
+
 int reinitializePermutation(int n)
 {
     vector<int> perm(n), arr, temp;
@@ -16,5 +18,26 @@ int reinitializePermutation(int n)
         temp = arr;
     } while (arr != perm);
 
+    return ans;
+}
+
+// METHOD 2 (Inverse Method) --> O(n)
+
+int reinitializePermutation(int n)
+{
+    vector<int> perm(n), arr(n);
+    for (int i = 0; i < n; i++)
+        perm[i] = i;
+    for (int i = 0; i < n; i++)
+        arr[i] = (i & 1) ? perm[n / 2 + i / 2] : perm[i / 2];
+    int ans = 0;
+    int x = 1;
+    while (1)
+    {
+        x = arr[x];
+        ans++;
+        if (x == 1)
+            break;
+    }
     return ans;
 }
