01d3be24927fe78b742beeff707bc01edd2cf3f3
Mukul <mkltaneja@gmail.com>
2021-04-01 14:33:24 +0530

    APRIL - DAY 1 METHOD 2

--- Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
@@ -1,7 +1,7 @@
 
 // DAY 1 (Palindrome Linked List)===============================================================
 
-// APPROACH 1 (Using Extra Space (Stack))
+// METHOD 1 (Using Extra Space (Stack))
 bool isPalindrome(ListNode *head)
 {
     int n = 0;
@@ -30,4 +30,42 @@ bool isPalindrome(ListNode *head)
         st.pop();
     }
     return (st.empty() && !tmp);
+}
+
+// METHOD 2 (Const Extra Space) -- (By reversing the lower half of LL)
+
+bool isPalindrome(ListNode *head)
+{
+    int n = 0;
+    ListNode *itr = head;
+    while (itr)
+    {
+        n++;
+        itr = itr->next;
+    }
+    int i = 0;
+    ListNode *tmp = head;
+    ListNode *curr = head, *prev = nullptr, *forw = head;
+
+    while (i++ < n / 2)
+    {
+        tmp = tmp->next;
+        forw = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forw;
+    }
+    ListNode *tmp1 = prev;
+
+    if (n & 1)
+        tmp = tmp->next;
+    while (tmp && tmp1)
+    {
+        // cout<<tmp->val<<" "<<tmp1->val;
+        if (tmp->val != tmp1->val)
+            return false;
+        tmp = tmp->next;
+        tmp1 = tmp1->next;
+    }
+    return (!tmp && !tmp1);
 }
