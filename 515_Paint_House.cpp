14ff8e289c01cdbad3562b65a4d04ac19d0b1c51
Mukul <mkltaneja@gmail.com>
2021-04-05 20:28:43 +0530

    515

--- 515_Paint_House.cpp
+++ 515_Paint_House.cpp
@@ -40,3 +40,26 @@ int minCost(vector<vector<int>> &costs)
     return mincost(-1, -1, costs, dp);
     // write your code here
 }
+
+// METHOD 3 (Tabulation)
+
+int mincost(int n, vector<vector<int>> &costs, vector<vector<int>> &dp)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        dp[i][0] = costs[i][0] + min(dp[i + 1][1], dp[i + 1][2]);
+        dp[i][1] = costs[i][1] + min(dp[i + 1][0], dp[i + 1][2]);
+        dp[i][2] = costs[i][2] + min(dp[i + 1][0], dp[i + 1][1]);
+    }
+    return min(dp[0][0], min(dp[0][1], dp[0][2]));
+}
+
+int minCost(vector<vector<int>> &costs)
+{
+    int n = costs.size();
+    if (n == 0)
+        return 0;
+    vector<vector<int>> dp(n + 1, vector<int>(3, 0));
+    return mincost(n, costs, dp);
+    // write your code here
+}
