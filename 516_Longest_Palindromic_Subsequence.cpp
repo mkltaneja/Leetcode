ef7bd3967e649bc691686070023ce47fe498fe38
Mukul <mkltaneja@gmail.com>
2021-04-07 20:19:47 +0530

    LPS TABU

--- 516_Longest_Palindromic_Subsequence.cpp
+++ 516_Longest_Palindromic_Subsequence.cpp
@@ -15,3 +15,32 @@ int longestPalindromeSubseq(string s)
     vector<vector<int>> dp(s.size(), vector<int>(s.size(),-1));
     return lps(0, s.size()-1, s, dp);
 }
+
+// METHOD 2 --> (TABULATION)
+
+int lps(int n, string &s, vector<vector<int>> &dp)
+{
+    for(int gap = 0; gap < n; gap++)
+    {
+        for(int i = 0, j = gap; j < n; i++, j++)
+        {
+            if(gap == 0)
+            {
+                dp[i][j] = 1;
+                continue;
+            }
+            if(s[i] == s[j])
+                dp[i][j] = dp[i+1][j-1] + 2;
+            else
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+        }
+    }
+    return dp[0][n-1];
+}
+
+int longestPalindromeSubseq(string s) 
+{
+    int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    return lps(n, s, dp);
+}
