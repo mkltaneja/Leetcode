ae9cee5009c20f3331c095fd1e8c22561c153e84
Mukul <mkltaneja@gmail.com>
2021-04-20 14:46:13 +0530

    DAY 20 APPROACH 2

--- Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
@@ -828,6 +828,7 @@ int combinationSum4(vector<int> &nums, int target)
 
 // DAY 20 (N-ary Tree Preorder Traversal)===========================================================
 
+// APPROACH 1 (Recursively - DFS)  --> O(n)
 void preorder_(Node *node, vector<int> &ans)
 {
     if (!node)
@@ -842,4 +843,48 @@ vector<int> preorder(Node *root)
     vector<int> ans;
     preorder_(root, ans);
     return ans;
+}
+
+// APPROACH 2 (Iteratively - Using Stack) --> O(n)-----------------------------------------
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    vector<int> preorder(Node *root)
+    {
+        if (!root)
+            return {};
+        vector<int> ans;
+        stack<Node *> st;
+        st.push(root);
+        while (!st.empty())
+        {
+            Node *node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            for (int i = node->children.size() - 1; i >= 0; i--)
+                st.push(node->children[i]);
+        }
+        return ans;
+    }
 }
