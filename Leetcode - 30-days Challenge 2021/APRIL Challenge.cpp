ea58fa140f6ce68710122afc725a1040f68dfad6
MUKUL <mkltaneja@gmail.com>
2021-04-28 13:59:56 +0530

    DAY 28

--- Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
@@ -1162,3 +1162,29 @@ bool isPowerOfThree(int n)
     int maxi = pow(3, 19);
     return (maxi % n == 0);
 }
+
+// DAY 28 (Unique Paths 2)====================================================================================
+
+// METHOD 1 (Memoized)
+int dfs(int i, int j, int n, int m, vector<vector<int>> &arr, vector<vector<int>> &dp)
+{
+    if (i == n || j == m || arr[i][j] == 1)
+        return 0;
+    if (i == n - 1 && j == m - 1)
+        return dp[i][j] = 1;
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    int d = dfs(i + 1, j, n, m, arr, dp);
+    int r = dfs(i, j + 1, n, m, arr, dp);
+
+    return dp[i][j] = d + r;
+}
+
+int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+{
+    int n = obstacleGrid.size();
+    int m = obstacleGrid[0].size();
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    return dfs(0, 0, n, m, obstacleGrid, dp);
+}
