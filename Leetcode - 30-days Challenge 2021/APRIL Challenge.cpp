eb9e9f5d838906c5023548a1f495292b522a3e1e
Mukul <mkltaneja@gmail.com>
2021-04-29 17:05:54 +0530

    DAY 29 OPTIMIZED

--- Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/APRIL Challenge.cpp
@@ -1217,6 +1217,7 @@ int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
 
 // DAY 29 (Find First and Last Position of Element in Sorted Array)=============================================================================
 
+// APPROACH 1 (Linear) --> O(n)
 vector<int> searchRange(vector<int> &nums, int target)
 {
     int si = -1, ei = -1;
@@ -1229,5 +1230,48 @@ vector<int> searchRange(vector<int> &nums, int target)
             ei = i;
         }
     }
+    return {si, ei};
+}
+
+// APPROACH 2 (Binary) --> O(2*logn)
+
+int findsi(vector<int> nums, int target)
+{
+    int si = -1;
+    int st = 0, end = nums.size() - 1;
+    while (st <= end)
+    {
+        int mid = (st + end) >> 1;
+        if (nums[mid] < target)
+            st = mid + 1;
+        else if (nums[mid] > target)
+            end = mid - 1;
+        else
+            si = mid, end = mid - 1;
+    }
+    return si;
+}
+
+int findei(vector<int> nums, int target)
+{
+    int ei = -1;
+    int st = 0, end = nums.size() - 1;
+    while (st <= end)
+    {
+        int mid = (st + end) >> 1;
+        if (nums[mid] < target)
+            st = mid + 1;
+        else if (nums[mid] > target)
+            end = mid - 1;
+        else
+            ei = mid, st = mid + 1;
+    }
+    return ei;
+}
+
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int si = findsi(nums, target);
+    int ei = findei(nums, target);
     return {si, ei};
 }
