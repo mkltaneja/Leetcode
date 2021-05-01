69154358987716c757d4bb89fcc7b14428111866
Mukul <mkltaneja@gmail.com>
2021-05-02 02:52:10 +0530

    Weekly Contest 51 - C

--- Biweekly_Contest_#51/1845. Seat Reservation Manager.cpp
+++ Biweekly_Contest_#51/1845. Seat Reservation Manager.cpp
@@ -1,3 +1,7 @@
+
+// APPROACH 1(Using set) --> O(n*logn)
+// AC
+
 class SeatManager
 {
 public:
@@ -18,6 +22,46 @@ public:
     void unreserve(int seatNumber)
     {
         v.insert(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
+
+// APPROACH 2 (Using vector) --> O(n^2)
+// TLE
+class SeatManager
+{
+public:
+    int focc;
+    vector<int> v;
+    SeatManager(int n)
+    {
+        focc = 0;
+        v.assign(n + 1, 0);
+    }
+
+    int reserve()
+    {
+        for (int i = focc + 1; i < v.size(); i++)
+        {
+            if (v[i] == 0)
+            {
+                v[i] = 1;
+                return focc = i;
+            }
+        }
+        return -1;
+    }
+
+    void unreserve(int seatNumber)
+    {
+        v[seatNumber] = 0;
+        focc = min(focc, seatNumber - 1);
     }
 };
 
