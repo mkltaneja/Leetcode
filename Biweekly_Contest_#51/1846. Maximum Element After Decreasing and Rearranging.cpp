20dbe9d7e23579ec6d8394cd72ba4fb3d6632e24
Mukul <mkltaneja@gmail.com>
2021-05-02 02:53:10 +0530

    Weekly Contest 51 - C

---
+++ Biweekly_Contest_#51/1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,9 @@
+
+int maximumElementAfterDecrementingAndRearranging(vector<int> &arr)
+{
+    sort(arr.begin(), arr.end());
+    arr[0] = 1;
+    for (int i = 1; i < arr.size(); i++)
+        arr[i] = min(arr[i], arr[i - 1] + 1);
+    return arr[arr.size() - 1];
+}
