29605d0aaa8fe2c13b009a3b6cd32410dc48e8b0
Mukul <mkltaneja@gmail.com>
2021-05-03 03:12:41 +0530

    DAY 2 OPTIMIZED

--- Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
@@ -23,3 +23,32 @@ int scheduleCourse(vector<vector<int>> &courses)
     });
     return subseq(0, 0, courses);
 }
+
+// APPROACH 2 (Greedy -- Taking the minimum durations to maximize course count) --> O(n*logn)
+// AC
+
+int scheduleCourse(vector<vector<int>> &courses)
+{
+    sort(courses.begin(), courses.end(), [](auto const &a, auto const &b) {
+        return (a[1] == b[1]) ? a[0] < b[0] : a[1] < b[1];
+    });
+
+    priority_queue<int> pq;
+    int day = 0;
+    for (vector<int> v : courses)
+    {
+        int dur = v[0], lim = v[1];
+        if (day + dur <= lim)
+        {
+            day += dur;
+            pq.push(dur);
+        }
+        else if (!pq.empty() && pq.top() > dur)
+        {
+            day += dur - pq.top();
+            pq.pop();
+            pq.push(dur);
+        }
+    }
+    return pq.size();
+}
