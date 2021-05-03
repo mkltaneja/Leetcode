a7861356e98dca1a977bdebb6747419da287e788
Mukul <mkltaneja@gmail.com>
2021-05-03 14:49:18 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
@@ -77,3 +77,12 @@ int scheduleCourse(vector<vector<int>> &courses)
     }
     return pq.size();
 }
+
+// DAY 3 (Running Sum of 1D Array)============================================
+
+vector<int> runningSum(vector<int> &nums)
+{
+    for (int i = 1; i < nums.size(); i++)
+        nums[i] += nums[i - 1];
+    return nums;
+}
