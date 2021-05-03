7260d0a72e2a7287e6e2ae53b1089971e89ff91c
Mukul <mkltaneja@gmail.com>
2021-05-03 21:07:18 +0530

    462

---
+++ 462. Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,12 @@
+
+int minMoves2(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+
+    int mid = nums[nums.size() / 2];
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i++)
+        ans += abs(nums[i] - mid);
+
+    return ans;
+}
