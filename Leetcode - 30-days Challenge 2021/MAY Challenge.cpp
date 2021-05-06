ba9cb18026c0091397ea39a62929a0b8f656ff33
MUKUL <mkltaneja@gmail.com>
2021-05-06 22:54:52 +0530

    DAY 6

--- Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
@@ -145,3 +145,49 @@ int jump(vector<int>& nums)
     }
     return 0;
 }
+
+// DAY 6 (Convert Sorted List to Binary Search Tree)===================================================
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* constructBST(int st, int end, vector<int> &arr)
+    {
+        if(st > end) return nullptr;
+        if(st == end) return new TreeNode(arr[st]);
+        int mid = (st + end)>>1;
+        return new TreeNode(arr[mid], constructBST(st, mid-1, arr), constructBST(mid+1, end, arr));
+    }
+    
+    TreeNode* sortedListToBST(ListNode* head)
+    {
+        vector<int> arr;
+        while(head)
+        {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+        return constructBST(0, arr.size()-1, arr);
+    }
+};
