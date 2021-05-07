4bd0bc0c3aa7a18fd503da91798e50853e04be3a
Mukul <mkltaneja@gmail.com>
2021-05-07 22:15:13 +0530

    DAY 7

--- Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
@@ -115,29 +115,30 @@ bool checkPossibility(vector<int> &nums)
 
 // APPROACH 1 (Using DP) --> O(n^2)
 
-int jump(vector<int>& nums) 
+int jump(vector<int> &nums)
 {
     int n = nums.size();
     vector<int> jumps(n, n);
-    jumps[n-1] = 0;
-    for(int i = n-2; i >= 0; i--)
-        for(int j = i+1; j <= i+nums[i] && j < n; j++)
-            jumps[i] = min(jumps[i], jumps[j]+1);
+    jumps[n - 1] = 0;
+    for (int i = n - 2; i >= 0; i--)
+        for (int j = i + 1; j <= i + nums[i] && j < n; j++)
+            jumps[i] = min(jumps[i], jumps[j] + 1);
     return jumps[0];
 }
 
 // APPROACH 2 (Storing max jumps at every "i" and updating total "jumps" whenever moves become 0) --> O(n)
 
-int jump(vector<int>& nums) 
+int jump(vector<int> &nums)
 {
     int n = nums.size();
     int jumps = 1;
     int jumplen = nums[0], moves = jumplen;
-    for(int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        if(i == n-1) return jumps;
-        jumplen = max(jumplen, i+nums[i]);
-        if(--moves == 0)
+        if (i == n - 1)
+            return jumps;
+        jumplen = max(jumplen, i + nums[i]);
+        if (--moves == 0)
         {
             moves = jumplen - i;
             jumps++;
@@ -169,25 +170,50 @@ int jump(vector<int>& nums)
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    
-    TreeNode* constructBST(int st, int end, vector<int> &arr)
+    TreeNode *constructBST(int st, int end, vector<int> &arr)
     {
-        if(st > end) return nullptr;
-        if(st == end) return new TreeNode(arr[st]);
-        int mid = (st + end)>>1;
-        return new TreeNode(arr[mid], constructBST(st, mid-1, arr), constructBST(mid+1, end, arr));
+        if (st > end)
+            return nullptr;
+        if (st == end)
+            return new TreeNode(arr[st]);
+        int mid = (st + end) >> 1;
+        return new TreeNode(arr[mid], constructBST(st, mid - 1, arr), constructBST(mid + 1, end, arr));
     }
-    
-    TreeNode* sortedListToBST(ListNode* head)
+
+    TreeNode *sortedListToBST(ListNode *head)
     {
         vector<int> arr;
-        while(head)
+        while (head)
         {
             arr.push_back(head->val);
             head = head->next;
         }
-        return constructBST(0, arr.size()-1, arr);
+        return constructBST(0, arr.size() - 1, arr);
     }
 };
+
+// DAY 7 (Delete Operation of 2 Strings)==============================================
+
+int minDistance(string word1, string word2)
+{
+    int n = word1.size(), m = word2.size();
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int a = (i - 1 >= 0) ? dp[i - 1][j] : 0;
+            int b = (j - 1 >= 0) ? dp[i][j - 1] : 0;
+            int c = (i - 1 >= 0 && j - 1 >= 0) ? dp[i - 1][j - 1] : 0;
+            if (word1[i] == word2[j])
+                dp[i][j] = c + 1;
+            else
+                dp[i][j] = max(a, b);
+        }
+    }
+    int len = dp[n - 1][m - 1];
+    return (n - len) + (m - len);
+}
