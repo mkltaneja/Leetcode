ec593c16462a03e991053e41801190b17151cc83
Mukul <mkltaneja@gmail.com>
2021-05-16 12:10:42 +0530

    Weekly Contest 241 - A OPTIMIZED

--- Weekly_Contest_241/1863. Sum of All Subset XOR Totals.cpp
+++ Weekly_Contest_241/1863. Sum of All Subset XOR Totals.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Brute Force -- all subsequences) --> O(2^n)
+
 void subseq(int i, vector<int> &nums, int &ans, int x)
 {
     if (i == nums.size())
@@ -15,4 +17,14 @@ int subsetXORSum(vector<int> &nums)
     int ans = 0;
     subseq(0, nums, ans, 0);
     return ans;
+}
+
+// APPROACH 2 (Making the bits of every number on, so it will occur exactly n/2 times in all the subsequences) --> O(n)
+
+int subsetXORSum(vector<int> &nums)
+{
+    int on = 0, n = nums.size();
+    for (int i = 0; i < n; i++)
+        on |= nums[i];
+    return on * pow(2, n - 1);
 }
