7d85107d17102839f8928d962507b1cf63b943f2
Mukul <mkltaneja@gmail.com>
2021-05-23 11:30:54 +0530

    Weekly Contest 242 - C(Approach 2)

--- Weekly_Contest_#242/1871. Jump Game VII.cpp
+++ Weekly_Contest_#242/1871. Jump Game VII.cpp
@@ -9,14 +9,41 @@ bool canReach(string s, int minJump, int maxJump)
     queue<int> que;
     que.push(0);
     vector<bool> vis(s.size());
-    while(!que.empty())
+    while (!que.empty())
     {
         int j = que.front();
         que.pop();
-        if(j == s.size()-1) return true;
-        for(int i = j+minJump; i < s.size() && i <= j+maxJump; i++)
-            if(s[i] == '0' && !vis[i])
+        if (j == s.size() - 1)
+            return true;
+        for (int i = j + minJump; i < s.size() && i <= j + maxJump; i++)
+            if (s[i] == '0' && !vis[i])
                 vis[i] = true, que.push(i);
     }
     return false;
+}
+
+// APPROACH 2 (DFS using DP) --> O(n^2)
+// TLE
+
+bool dfs(int i, int n, int minj, int maxj, string &s, vector<int> &dp)
+{
+    if (i == n)
+        return dp[i] = true;
+    if (i + minj > n)
+        return false;
+    if (dp[i] != -1)
+        return dp[i];
+    for (int j = i + minj; j <= n && j <= i + maxj; j++)
+        if (s[j] == '0')
+            if (dfs(j, n, minj, maxj, s, dp))
+                return dp[j] = true;
+    return dp[i] = false;
+}
+
+bool canReach(string s, int minJump, int maxJump)
+{
+    if (s[s.size() - 1] == '1')
+        return false;
+    vector<int> dp(s.size(), -1);
+    return dfs(0, s.size() - 1, minJump, maxJump, s, dp);
 }
