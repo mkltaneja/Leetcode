e8be6c72a2ffaca5980783949e3dc33f6823bbb6
Mukul <mkltaneja@gmail.com>
2021-05-28 01:21:35 +0530

    DAY 27

--- Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/MAY Challenge.cpp
@@ -1024,3 +1024,20 @@ int minPartitions(string n)
     }
     return ans;
 }
+
+// DAY 27 (Maximum Product of Word Lengths)============================================================================
+
+int maxProduct(vector<string> &words)
+{
+    int n = words.size();
+    vector<int> map(n, 0);
+    for (int i = 0; i < n; i++)
+        for (char c : words[i])
+            map[i] |= (1 << (c - 'a'));
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if ((map[i] & map[j]) == 0)
+                ans = max(ans, (int)(words[i].size() * words[j].size()));
+    return ans;
+}
