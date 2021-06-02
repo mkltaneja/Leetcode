c894f78505977411a3542718914f32f019fde330
Mukul <mkltaneja@gmail.com>
2021-06-02 16:14:07 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2021/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JUNE Challenge.cpp
@@ -24,3 +24,44 @@ int maxAreaOfIsland(vector<vector<int>> &grid)
                 maxarea = max(maxarea, dfs(i, j, n, m, grid));
     return maxarea;
 }
+
+// DAY 2 (Interleaving Strings)=================================================================================
+
+bool check(int bi, int ci, string &a, string &b, string &c, vector<bool> &vis)
+{
+    if (bi == b.size())
+    {
+        string aa = "", bb = "";
+        for (int i = 0; i < c.size(); i++)
+        {
+            if (!vis[i])
+                aa += c[i];
+            else
+                bb += c[i];
+        }
+        cout << aa << ", " << bb << endl;
+        if (aa == a)
+            return true;
+        return false;
+    }
+    for (int i = ci; i < c.size(); i++)
+    {
+        if (c[i] == b[bi] && !vis[i])
+        {
+            vis[i] = true;
+            if (check(bi + 1, i + 1, a, b, c, vis))
+                return true;
+            vis[i] = false;
+        }
+    }
+    return false;
+}
+
+bool isInterleave(string s1, string s2, string s3)
+{
+    if (s3.size() != s1.size() + s2.size())
+        return false;
+    vector<bool> vis(s3.size(), false);
+
+    return check(0, 0, s1, s2, s3, vis);
+}
