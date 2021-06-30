3e2b6edc924d1068b1597131af134ab567ee17f9
Mukul <mkltaneja@gmail.com>
2021-07-01 01:11:46 +0530

    DAY 30

--- Leetcode - 30-days Challenge 2021/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JUNE Challenge.cpp
@@ -1102,3 +1102,23 @@ int longestOnes(vector<int> &nums, int k)
     }
     return maxcon;
 }
+
+// DAY 30 (Lowest Common Ancestor)=======================================================================================
+
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if (!root)
+        return nullptr;
+
+    if (root->val == p->val || root->val == q->val)
+        return root;
+
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+    if (left && right)
+        return root;
+    if (!left && !right)
+        return nullptr;
+    return left ? left : right;
+}
