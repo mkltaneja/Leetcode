caaf2759a83945a94f095c04b99b956efdcbc9eb
Mukul <mkltaneja@gmail.com>
2021-07-01 20:28:08 +0530

    DAY 27

--- Leetcode - 30-days Challenge 2021/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JUNE Challenge.cpp
@@ -1059,6 +1059,27 @@ vector<int> countSmaller(vector<int> &nums)
     return ans;
 }
 
+// DAY 27 (Candy)===========================================================================
+
+int candy(vector<int>& ratings) 
+{
+    int n = ratings.size();
+    vector<int> candy(n,1);
+    
+    for(int i = 1; i < n; i++)
+        if(ratings[i] > ratings[i-1])
+            candy[i] = candy[i-1] + 1;
+    for(int i = n-2; i >= 0; i--)
+        if(ratings[i] > ratings[i+1])
+            candy[i] = max(candy[i], candy[i+1] + 1);
+    
+    int ans = 0;
+    for(int i = 0; i < n; i++)
+        ans += candy[i];
+    
+    return ans;
+}
+
 // DAY 28 (Remove All Adjacent Adjacent Duplicates in String)=======================================================
 
 string removeDuplicates(string s)
