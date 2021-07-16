bf902614443e19be0172c96759149ea34fb82eec
Mukul <mkltaneja@gmail.com>
2021-07-17 00:58:48 +0530

    DAY 16

--- Leetcode - 30-days Challenge 2021/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JULY Challenge.cpp
@@ -428,3 +428,24 @@ int triangleNumber(vector<int> &nums)
     }
     return ans;
 }
+
+// DAY 16 (4 Sum)==========================================================================================
+
+vector<vector<int>> fourSum(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    set<vector<int>> s;
+    vector<vector<int>> ans;
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            for (int k = j + 1; k < n; k++)
+                for (int l = k + 1; l < n; l++)
+                    if (nums[i] + nums[j] + nums[k] + nums[l] == target)
+                        s.insert({nums[i], nums[j], nums[k], nums[l]});
+    for (auto v : s)
+        ans.push_back(v);
+
+    return ans;
+}
