967cdd9a1c4f4cf9e9b51f61d880af3414a70a31
Mukul <mkltaneja@gmail.com>
2021-08-01 01:50:19 +0530

    DAY 31

--- Leetcode - 30-days Challenge 2021/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/JULY Challenge.cpp
@@ -882,3 +882,34 @@ public:
  * obj->insert(key,val);
  * int param_2 = obj->sum(prefix);
  */
+
+// DAY 31 (Trapping Rain water)========================================================================
+
+int trap(vector<int> &height)
+{
+    int n = height.size();
+    if (n <= 2)
+        return 0;
+    int lmax = height[0], rmax = height[n - 1];
+    int l = 1, r = n - 2;
+
+    int water = 0;
+    while (l <= r)
+    {
+        if (lmax < rmax)
+        {
+            if (height[l] < lmax)
+                water += lmax - height[l++];
+            else
+                lmax = height[l++];
+        }
+        else
+        {
+            if (height[r] < rmax)
+                water += rmax - height[r--];
+            else
+                rmax = height[r--];
+        }
+    }
+    return water;
+}
