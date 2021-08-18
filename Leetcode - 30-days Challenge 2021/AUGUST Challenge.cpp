6316ab28db441815b7783977ffa3b2f3bb11a474
Mukul <mkltaneja@gmail.com>
2021-08-18 23:11:41 +0530

    DAY 18

--- Leetcode - 30-days Challenge 2021/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/AUGUST Challenge.cpp
@@ -438,3 +438,32 @@ public:
  * NumArray* obj = new NumArray(nums);
  * int param_1 = obj->sumRange(left,right);
  */
+
+// DAY 18 (Decode Ways)=============================================================
+
+int dfs(int i, string &s, vector<int> &dp)
+{
+    if (i == s.size())
+        return 1;
+    if (s[i] == '0')
+        return 0;
+    if (dp[i] != -1)
+        return dp[i];
+
+    int ans = 0;
+    ans += dfs(i + 1, s, dp);
+
+    if (i != s.size() - 1)
+    {
+        int num = (s[i] - '0') * 10 + (s[i + 1] - '0') <= 26;
+        if (num >= 1 && num <= 26)
+            ans += dfs(i + 2, s, dp);
+    }
+    return dp[i] = ans;
+}
+
+int numDecodings(string s)
+{
+    vector<int> dp(s.size(), -1);
+    return dfs(0, s, dp);
+}
