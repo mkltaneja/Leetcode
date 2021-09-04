ae659be3c8664ead5e6f18aa3947109195a52328
Mukul <mkltaneja@gmail.com>
2021-09-05 04:05:49 +0530

    DAY 4

--- Leetcode - 30-days Challenge 2021/SEPTEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/SEPTEMBER Challenge.cpp
@@ -46,3 +46,45 @@ vector<TreeNode *> generateTrees(int n)
     vector<vector<vector<TreeNode *>>> dp(n + 1, vector<vector<TreeNode *>>(n + 1));
     return makeTree(1, n, dp);
 }
+
+// DAY 4 (Sum of Distances in Tree)===========================================================================
+
+vector<int> cnt, ans;
+void dfs1(int u, int p, vector<vector<int>> &gp)
+{
+    for (int v : gp[u])
+    {
+        if (v != p)
+        {
+            dfs1(v, u, gp);
+            cnt[u] += cnt[v];
+            ans[u] += ans[v] + cnt[v];
+        }
+    }
+}
+void dfs2(int u, int p, vector<vector<int>> &gp)
+{
+    for (int v : gp[u])
+    {
+        if (v != p)
+        {
+            ans[v] = ans[u] - cnt[v] + (gp.size() - cnt[v]);
+            dfs2(v, u, gp);
+        }
+    }
+}
+
+vector<int> sumOfDistancesInTree(int n, vector<vector<int>> &edges)
+{
+    vector<vector<int>> gp(n);
+    for (auto e : edges)
+    {
+        gp[e[0]].push_back(e[1]);
+        gp[e[1]].push_back(e[0]);
+    }
+    cnt.resize(n, 1), ans.resize(n, 0);
+    dfs1(0, -1, gp);
+    dfs2(0, -1, gp);
+
+    return ans;
+}
