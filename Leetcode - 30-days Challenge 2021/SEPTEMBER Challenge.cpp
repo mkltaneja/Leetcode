aeac257d89b9565b347a11a7ab3f4ff4c6c19a2f
Mukul <mkltaneja@gmail.com>
2021-09-05 23:45:53 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2021/SEPTEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/SEPTEMBER Challenge.cpp
@@ -47,6 +47,62 @@ vector<TreeNode *> generateTrees(int n)
     return makeTree(1, n, dp);
 }
 
+// DAY 3 (Erect The Fence)=============================================================================
+
+int crossProduct(vector<int> &a, vector<int> &b, vector<int> &c)
+{
+    // CROSS PRODUCT OF AB and AC --> x1*y2 - x2*y1
+    return ((b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1]));
+}
+
+vector<vector<int>> outerTrees(vector<vector<int>> &trees)
+{
+    int n = trees.size();
+    if (n <= 3)
+        return trees;
+    sort(trees.begin(), trees.end(), [](auto const &a, auto const &b)
+         { return a[0] == b[0] ? a[1] < b[1] : a[0] < b[0]; });
+
+    vector<vector<int>> upper;
+    upper.push_back(trees[0]);
+    upper.push_back(trees[1]);
+    for (int i = 2; i < n; i++)
+    {
+        int sz = upper.size();
+        while (sz >= 2 && crossProduct(upper[sz - 2], upper[sz - 1], trees[i]) > 0)
+        {
+            upper.pop_back();
+            sz--;
+        }
+        upper.push_back(trees[i]);
+    }
+
+    vector<vector<int>> lower;
+    lower.push_back(trees[n - 1]);
+    lower.push_back(trees[n - 2]);
+    for (int i = n - 3; i >= 0; i--)
+    {
+        int sz = lower.size();
+        while (sz >= 2 && crossProduct(lower[sz - 2], lower[sz - 1], trees[i]) > 0)
+        {
+            lower.pop_back();
+            sz--;
+        }
+        lower.push_back(trees[i]);
+    }
+
+    upper.pop_back();
+    lower.pop_back();
+
+    for (auto v : lower)
+        upper.push_back(v);
+
+    sort(upper.begin(), upper.end());
+    upper.erase(unique(upper.begin(), upper.end()), upper.end());
+
+    return upper;
+}
+
 // DAY 4 (Sum of Distances in Tree)===========================================================================
 
 vector<int> cnt, ans;
@@ -91,22 +147,24 @@ vector<int> sumOfDistancesInTree(int n, vector<vector<int>> &edges)
 
 // DAY 5 (Orderly Queue)===================================================================
 
-string orderlyQueue(string s, int k) 
+string orderlyQueue(string s, int k)
 {
-    if(k > 1)
+    if (k > 1)
     {
         sort(s.begin(), s.end());
         return s;
     }
     char minc = 'z';
-    for(char c : s) minc = min(minc, c);
+    for (char c : s)
+        minc = min(minc, c);
     string ans = s;
-    for(int i = 0; i < s.size(); i++)
+    for (int i = 0; i < s.size(); i++)
     {
-        if(s[i] == minc)
+        if (s[i] == minc)
         {
             string x = s.substr(i, s.size()) + s.substr(0, i);
-            if(x < ans) ans = x;
+            if (x < ans)
+                ans = x;
         }
     }
     return ans;
