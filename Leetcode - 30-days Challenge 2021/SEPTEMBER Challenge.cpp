cbcf3a3be4e576384e21b88a9fb3a4d4f49cde93
Mukul Taneja <mkltaneja@gmail.com>
2021-09-20 18:11:41 +0530

    DAY 19

--- Leetcode - 30-days Challenge 2021/SEPTEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/SEPTEMBER Challenge.cpp
@@ -478,3 +478,24 @@ vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
     }
     return ans;
 }
+
+// DAY 19 (Distinct Subsequences)============================================================
+
+int numDistinct(string s, string t)
+{
+    int n = s.size(), m = t.size();
+    vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(m + 1, 0));
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (j == 0)
+                dp[i][j] = 1;
+            else if (i == 0)
+                dp[i][j] = 0;
+            else
+                dp[i][j] = dp[i - 1][j] + ((s[i - 1] == t[j - 1]) ? dp[i - 1][j - 1] : 0);
+        }
+    }
+    return dp[n][m];
+}
