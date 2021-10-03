3ae1e6dffa0f6f3cd1282f944ed073f995144747
Mukul Taneja <mkltaneja@gmail.com>
2021-10-04 03:58:28 +0530

    OCTOBER DAY - 2

--- Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
@@ -20,3 +20,27 @@ int longestCommonSubsequence(string text1, string text2)
     }
     return ans;
 }
+
+// DAY 2 (Dungeon Game)================================================================================
+
+int calculateMinimumHP(vector<vector<int>> &dungeon)
+{
+    int n = dungeon.size(), m = dungeon[0].size();
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    dp[n - 1][m - 1] = (dungeon[n - 1][m - 1] <= 0) ? (-dungeon[n - 1][m - 1] + 1) : 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = m - 1; j >= 0; j--)
+        {
+            if (i == n - 1 && j == m - 1)
+                continue;
+
+            int right = (j != m - 1) ? dp[i][j + 1] : INT_MAX;
+            int down = (i != n - 1) ? dp[i + 1][j] : INT_MAX;
+
+            dp[i][j] = max(-dungeon[i][j] + min(right, down), 1);
+        }
+    }
+
+    return dp[0][0];
+}
