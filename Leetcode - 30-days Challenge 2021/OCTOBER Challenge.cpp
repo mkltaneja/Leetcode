0be419fd9828bed9c38620e576e8a77ffa023af7
Mukul Taneja <mkltaneja@gmail.com>
2021-10-07 00:50:44 +0530

    DAY 6

--- Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
@@ -95,3 +95,17 @@ int climbStairs(int n)
     }
     return ans;
 }
+
+// DAY 6 (Find All Duplicates in an Array)===============================================================
+
+vector<int> findDuplicates(vector<int>& nums)
+{
+    vector<int> ans;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(nums[abs(nums[i])-1] > 0)
+            nums[abs(nums[i])-1] *= -1;
+        else ans.push_back(abs(nums[i]));
+    }
+    return ans;
+}
