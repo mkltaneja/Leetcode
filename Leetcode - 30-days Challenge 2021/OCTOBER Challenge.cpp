ace448970d55dc0cab470f410bb4fa74d05efcad
Mukul Taneja <mkltaneja@gmail.com>
2021-10-20 00:34:25 +0530

    DAY 19

--- Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
@@ -430,3 +430,31 @@ bool isCousins(TreeNode *root, int x, int y)
 
     return false;
 }
+
+// DAY 19 (Next Greater Element 1)========================================================================
+
+vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+{
+    stack<int> st;
+    unordered_map<int, int> ng;
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        while (!st.empty() && nums2[st.top()] < nums2[i])
+        {
+            ng[nums2[st.top()]] = nums2[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    while (!st.empty())
+    {
+        ng[nums2[st.top()]] = -1;
+        st.pop();
+    }
+
+    vector<int> ans(nums1.size());
+    for (int i = 0; i < nums1.size(); i++)
+        ans[i] = ng[nums1[i]];
+
+    return ans;
+}
