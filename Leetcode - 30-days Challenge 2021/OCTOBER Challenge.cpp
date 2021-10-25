1f4ad1534ef41a95482291059d86fe73062328b4
Mukul Taneja <mkltaneja@gmail.com>
2021-10-25 12:31:57 +0530

    DAY 25

--- Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
@@ -618,3 +618,58 @@ int countNodes(TreeNode *root)
 
     return l + r + 1;
 }
+
+// DAY 25 (Min Stack)==========================================================================
+class MinStack {
+public:
+    
+    stack<long> st;
+    long mn;
+    MinStack() 
+    {
+        this->mn = INT_MAX;
+    }
+    
+    void push(int val)
+    {
+        if(st.empty())
+        {
+            st.push(val);
+            mn = val;
+        }
+        else if(val < mn)
+        {
+            st.push(val - mn + val);
+            mn = val;
+        }
+        else st.push(val);
+    }
+    
+    void pop() 
+    {
+        if(st.top() < mn)
+            mn = mn - st.top() + mn; 
+        st.pop();
+    }
+    
+    int top() 
+    {
+        if(st.top() < mn)
+            return mn;
+        return st.top();
+    }
+    
+    int getMin() 
+    {
+        return mn;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
