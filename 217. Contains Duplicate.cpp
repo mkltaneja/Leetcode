e80f531b9d3ce2a2d625fb2e31cbfdcfed8099c1
Mukul Taneja <mkltaneja@gmail.com>
2021-10-26 16:17:47 +0530

    217

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,12 @@
+
+bool containsDuplicate(vector<int> &nums)
+{
+    unordered_set<int> st;
+    for (int x : nums)
+    {
+        if (st.count(x))
+            return true;
+        st.insert(x);
+    }
+    return false;
+}
