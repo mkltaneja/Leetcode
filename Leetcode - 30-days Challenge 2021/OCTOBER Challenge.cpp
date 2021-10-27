a77346951e7e723d30906ee21130ed58a568979c
Mukul Taneja <mkltaneja@gmail.com>
2021-10-28 02:36:33 +0530

    DAY 27

--- Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/OCTOBER Challenge.cpp
@@ -690,3 +690,19 @@ TreeNode *invertTree(TreeNode *root)
 
     return root;
 }
+
+// DAY 27 (Sort Colors)===================================================================
+
+void sortColors(vector<int> &nums)
+{
+    int i = 0, j = 0, k = nums.size() - 1;
+    while (i <= k)
+    {
+        if (nums[i] == 0)
+            swap(nums[i++], nums[j++]);
+        else if (nums[i] == 2)
+            swap(nums[i], nums[k--]);
+        else
+            i++;
+    }
+}
