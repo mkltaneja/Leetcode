c5c064dc68451a657746713d3645c2aa9e13e7f3
Mukul Taneja <mkltaneja@gmail.com>
2021-11-13 14:21:17 +0530

    DAY 13

--- Leetcode - 30-days Challenge 2021/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/NOVEMBER Challenge.cpp
@@ -303,3 +303,28 @@ ListNode *removeElements(ListNode *head, int val)
 
     return head;
 }
+
+// DAY 13 (739. Daily Temperatures)============================================================================
+
+vector<int> dailyTemperatures(vector<int>& temperatures) 
+{
+    int n = temperatures.size();
+    vector<int> ans(n);
+    stack<int> st;
+    for(int i = 0; i < n; i++)
+    {
+        while(!st.empty() && temperatures[i] > temperatures[st.top()])
+        {
+            ans[st.top()] = i - st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty())
+    {
+        ans[st.top()] = 0;
+        st.pop();
+    }
+    
+    return ans;
+}
