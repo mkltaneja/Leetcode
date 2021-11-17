372bc4c224f8fce775112b9eb6617ee504371089
Mukul Taneja <mkltaneja@gmail.com>
2021-11-17 19:22:11 +0530

    DAY 17

--- Leetcode - 30-days Challenge 2021/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/NOVEMBER Challenge.cpp
@@ -433,3 +433,26 @@ int findKthNumber(int n, int m, int k)
 
     return si;
 }
+
+// DAY 17 (62. Unique Paths)=================================================================================
+
+int uniquePaths(int n, int m) 
+{
+    vector<vector<int>> paths(n, vector<int>(m, 0));
+    paths[n-1][m-1] = 1;
+    
+    for(int i = n-1; i >= 0; i--)
+    {
+        for(int j = m-1; j >= 0; j--)
+        {
+            if(i == n-1 && j == m-1)
+                continue;
+            int dans = (i == n-1)? 0 : paths[i+1][j];
+            int rans = (j == m-1)? 0 : paths[i][j+1];
+            
+            paths[i][j] = dans + rans;
+        }
+    }
+    
+    return paths[0][0];
+}
