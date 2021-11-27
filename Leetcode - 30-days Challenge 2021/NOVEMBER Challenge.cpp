5adf0a5c6df3b99a1832b961cc07344986f80537
Mukul Taneja <mkltaneja@gmail.com>
2021-11-27 11:09:24 +0530

    DAY 27

--- Leetcode - 30-days Challenge 2021/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/NOVEMBER Challenge.cpp
@@ -656,3 +656,24 @@ int searchInsert(vector<int> &nums, int target)
     }
     return si;
 }
+
+// DAY 27 (238. Product of Array Except Self)================================================================================
+
+vector<int> productExceptSelf(vector<int>& nums) 
+{
+    int count0 = 0, totprod = 1;
+    for(int x : nums)
+    {
+        if(x == 0) count0++;
+        totprod *= x? x : 1;
+    }
+    
+    vector<int> ans(nums.size());
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(nums[i]) ans[i] = count0? 0 : totprod/nums[i];
+        else ans[i] = count0 > 1? 0 : totprod;
+    }
+    
+    return ans;
+}
