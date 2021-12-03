6999a7717af5ec1ded52c1573c26b1766d9fcdd3
Mukul Taneja <mkltaneja@gmail.com>
2021-12-04 01:24:04 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2021/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/DECEMBER Challenge.cpp
@@ -10,3 +10,23 @@ int rob(vector<int> &nums)
 
     return dp[0];
 }
+
+// DAY 3 (152. Maximum Product Subarray)================================================================================
+
+int maxProduct(vector<int> &nums)
+{
+    int ans = nums[0];
+    int maxp = nums[0], minp = nums[0];
+    for (int i = 1; i < nums.size(); i++)
+    {
+        int x = nums[i];
+        if (x < 0)
+            swap(minp, maxp);
+        maxp = max(maxp * x, x);
+        minp = min(minp * x, x);
+
+        ans = max({ans, maxp, minp});
+    }
+
+    return ans;
+}
