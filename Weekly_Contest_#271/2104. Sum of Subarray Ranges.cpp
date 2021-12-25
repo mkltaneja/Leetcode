b8bade8b190fb560d379bfb649e90c5055851560
Mukul Taneja <mkltaneja@gmail.com>
2021-12-26 00:53:25 +0530

    Weekly Contest 271 - B - Approach 2 [OPTIMIZED]

--- Weekly_Contest_#271/2104. Sum of Subarray Ranges.cpp
+++ Weekly_Contest_#271/2104. Sum of Subarray Ranges.cpp
@@ -19,3 +19,74 @@ long long subArrayRanges(vector<int> &nums)
 
     return ans;
 }
+
+// APPROACH 2 (By Finding next greater on left, right and next smaller on left and right) --> O(n)
+
+class Pair
+{
+public:
+    int cgl, csl, cgr, csr;
+    Pair()
+    {
+        this->cgl = 0;
+        this->csl = 0;
+        this->cgr = 0;
+        this->csr = 0;
+    }
+};
+
+void fill_cgl_cgr(int n, vector<int> &arr, vector<Pair> &conf)
+{
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr[i] >= arr[st.top()])
+        {
+            conf[st.top()].cgr = i - st.top();
+            st.pop();
+        }
+        if (!st.empty())
+            conf[i].cgl = i - st.top();
+        st.push(i);
+    }
+}
+
+void fill_csl_csr(int n, vector<int> &arr, vector<Pair> &conf)
+{
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr[i] <= arr[st.top()])
+        {
+            conf[st.top()].csr = i - st.top();
+            st.pop();
+        }
+        if (!st.empty())
+            conf[i].csl = i - st.top();
+        st.push(i);
+    }
+}
+
+long long subArrayRanges(vector<int> &nums)
+{
+    int n = nums.size();
+    long long ans = 0;
+
+    vector<Pair> conf(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        conf[i].cgl = i + 1;
+        conf[i].csl = i + 1;
+        conf[i].cgr = n - i;
+        conf[i].csr = n - i;
+    }
+
+    fill_cgl_cgr(n, nums, conf);
+    fill_csl_csr(n, nums, conf);
+
+    for (int i = 0; i < n; i++)
+        ans += nums[i] * (((long long)conf[i].cgl * conf[i].cgr) - ((long long)conf[i].csl * conf[i].csr));
+
+    return ans;
+}
