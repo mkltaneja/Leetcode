6882f6288cd69bc76a95b0e7efbef189f431687e
Mukul Taneja <mkltaneja@gmail.com>
2021-12-26 11:36:23 +0530

    Weekly Contest 273 - D Approach 2

--- Weekly_Contest_#273/2122. Recover the Original Array.cpp
+++ Weekly_Contest_#273/2122. Recover the Original Array.cpp
@@ -30,3 +30,42 @@ vector<int> recoverArray(vector<int> &nums)
     }
     return {};
 }
+
+// APPROACH 2 (Brute Force -- [Using visited array]) --> O(n^2)
+
+vector<int> recoverArray(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+
+    vector<int> ans;
+    for (int i = 1; i < n; i++)
+    {
+        int k = nums[i] - nums[0];
+        if (k == 0 || (k & 1))
+            continue;
+
+        vector<int> vis(n);
+        for (int i = 0; i < n; i++)
+        {
+            if (vis[i])
+                continue;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (!vis[j] && nums[j] == nums[i] + k)
+                {
+                    vis[i] = 1;
+                    vis[j] = 1;
+                    break;
+                }
+            }
+            if (!vis[i])
+                break;
+            ans.push_back(nums[i] + k / 2);
+        }
+        if (ans.size() == n / 2)
+            return ans;
+        ans.clear();
+    }
+    return {};
+}
