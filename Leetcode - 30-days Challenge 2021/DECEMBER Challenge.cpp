8cfd48bc456fb91b3d6bcc37f099be8af9fc47f9
Mukul Taneja <mkltaneja@gmail.com>
2021-12-29 00:44:12 +0530

    DAY 28

--- Leetcode - 30-days Challenge 2021/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/DECEMBER Challenge.cpp
@@ -848,3 +848,16 @@ vector<vector<int>> kClosest(vector<vector<int>> &points, int k)
 
     return ans;
 }
+
+// DAY 28 (876. Middle of the Linked List)=========================================================================
+
+ListNode *middleNode(ListNode *head)
+{
+    ListNode *fast = head, *slow = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
