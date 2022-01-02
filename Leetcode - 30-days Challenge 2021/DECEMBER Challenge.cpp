b7608e0c5eeaa5347aef5291636462e35e0c6c82
Mukul Taneja <mkltaneja@gmail.com>
2022-01-02 13:14:40 +0530

    DAY 30

--- Leetcode - 30-days Challenge 2021/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2021/DECEMBER Challenge.cpp
@@ -891,6 +891,20 @@ ListNode *middleNode(ListNode *head)
     return slow;
 }
 
+// DAY 30 (1015. Smallest Integer Divisible by K)=============================================================================
+
+int smallestRepunitDivByK(int k)
+{
+    int rem = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        rem = (rem * 10 + 1) % k;
+        if (rem == 0)
+            return i;
+    }
+    return -1;
+}
+
 // DAY 31 (1026. Maximum Difference Between Node and Ancestor)================================================================================
 
 // METHOD 1 (return type - pair<int,int>)
