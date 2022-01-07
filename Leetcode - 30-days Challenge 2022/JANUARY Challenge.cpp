a15ea407e2b2e7a99d92942440f3f2a3ed5447a4
Mukul Taneja <mkltaneja@gmail.com>
2022-01-07 18:06:20 +0530

    DAY 7

--- Leetcode - 30-days Challenge 2022/JANUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JANUARY Challenge.cpp
@@ -137,3 +137,40 @@ bool carPooling(vector<vector<int>> &trips, int capacity)
 
     return true;
 }
+
+// DAY 7 (382. Linked List Random Node)=========================================================================
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> nodes;
+    Solution(ListNode *head)
+    {
+        ListNode *node = head;
+        while (node)
+        {
+            this->nodes.push_back(node->val);
+            node = node->next;
+        }
+    }
+
+    int getRandom()
+    {
+        return nodes[rand() % nodes.size()];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
