ff1326ada8d7fee3a2c4928bcb0c06bb20af2bfb
Mukul Taneja <mkltaneja@gmail.com>
2022-01-09 02:11:43 +0530

    DAY 8

--- Leetcode - 30-days Challenge 2022/JANUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JANUARY Challenge.cpp
@@ -174,3 +174,27 @@ public:
  * Solution* obj = new Solution(head);
  * int param_1 = obj->getRandom();
  */
+
+//  DAY 8 (1463. Cherry Pickup II)===============================================================================
+
+    int dfs(int i, int j1, int j2, int n, int m, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
+    {
+        if(i == n || j1 == -1 || j1 == m || j2 == -1 || j2 == m)
+            return 0;
+        
+        if(dp[i][j1][j2] != -1)
+            return dp[i][j1][j2];
+        
+        int ans = INT_MIN;
+        for(int x = -1; x <= 1; x++)
+            for(int y = -1; y <= 1; y++)
+                ans = max(ans, dfs(i+1, j1+x, j2+y, n, m, grid, dp));
+        return dp[i][j1][j2] = ans + grid[i][j1] + ((j1 == j2)? 0 : grid[i][j2]);
+    }
+    
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(m,-1)));
+        return dfs(0, 0, m-1, n, m, grid, dp);
+    }
