58c613e6b9cd059e5b9c82eee97b0e4048595430
Mukul Taneja <mkltaneja@gmail.com>
2022-01-22 22:41:20 +0530

    Biweekly Contest - 70 - B Method 2

--- Biweekly_Contest_#70/2145. Count the Hidden Sequences.cpp
+++ Biweekly_Contest_#70/2145. Count the Hidden Sequences.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Taking max and min vals)
+
 int numberOfArrays(vector<int> &diff, int lower, int upper)
 {
     int n = diff.size();
@@ -14,4 +16,23 @@ int numberOfArrays(vector<int> &diff, int lower, int upper)
     int end = mx + (st - lower);
     // cout<<l<<";  "<<mn<<", "<<mx<<" -> "<<st<<", "<<end<<endl;
     return max(end, st) <= upper ? upper - max(end, st) + 1 : 0;
+}
+
+// APPROACH 2 (Range Difference)
+
+int numberOfArrays(vector<int> &diff, int lower, int upper)
+{
+    int n = diff.size();
+    long l = 0, r = 0;
+    long curr = 0;
+    for (int x : diff)
+    {
+        curr += x;
+        if (curr < l)
+            l = curr;
+        else if (curr > r)
+            r = curr;
+    }
+    long myRange = r - l, xRange = upper - lower;
+    return myRange <= xRange ? xRange - myRange + 1 : 0;
 }
