2496ca23522b5e732e4dc8dafd1b3f3bf458fe5d
Mukul Taneja <mkltaneja@gmail.com>
2022-02-07 02:36:48 +0530

    121

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,11 @@
+
+int maxProfit(vector<int> &prices)
+{
+    int mn = INT_MAX, ans = 0;
+    for (int x : prices)
+    {
+        ans = max(ans, x - mn);
+        mn = min(mn, x);
+    }
+    return ans;
+}
