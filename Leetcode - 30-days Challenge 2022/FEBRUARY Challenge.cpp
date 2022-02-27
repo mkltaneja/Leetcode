8f58e762cb08ac4943181df381807f540e71aafa
Mukul Taneja <mkltaneja@gmail.com>
2022-02-27 20:07:24 +0530

    DAY 25

--- Leetcode - 30-days Challenge 2022/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/FEBRUARY Challenge.cpp
@@ -634,3 +634,24 @@ ListNode* sortList(ListNode* head)
 
     return ans;
 }
+
+// DAY 25 (165. Compare Version Numbers)======================================================================
+
+int compareVersion(string version1, string version2) 
+{
+    int i = 0, j = 0, n = version1.size(), m = version2.size();
+
+    while(i < n || j < m)
+    {
+        int x = 0, y = 0;
+        while(i < n && version1[i] != '.')
+            x = x*10 + (version1[i++]-'0');
+        while(j < m && version2[j] != '.')
+            y = y*10 + (version2[j++]-'0');
+        i++, j++;
+
+        if(x > y) return 1;
+        else if(x < y) return -1;
+    }
+    return 0;
+}
