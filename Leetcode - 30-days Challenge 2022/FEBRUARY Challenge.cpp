7a1c47525dc202a8d90e67017f1fea7bcf2a03bd
Mukul Taneja <mkltaneja@gmail.com>
2022-02-27 20:01:46 +0530

    DAY 20

--- Leetcode - 30-days Challenge 2022/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/FEBRUARY Challenge.cpp
@@ -495,3 +495,22 @@ int minimumDeviation(vector<int>& nums)
 
     return mndev;
 }
+
+// DAY 20 (1288. Remove Covered Intervals)===========================================================================================
+
+int removeCoveredIntervals(vector<vector<int>>& intervals) 
+{
+    sort(intervals.begin(), intervals.end(), [](vector<int> &v1, vector<int> &v2){
+        return v1[0] == v2[0]? v1[1] > v2[1] : v1[0] < v2[0];
+    });
+
+    int mxend = intervals[0][1];
+    int rem = 0;
+    for(int i = 1; i < intervals.size(); i++)
+    {
+        if(intervals[i][1] <= mxend) rem++;
+        else mxend = intervals[i][1];
+    }
+
+    return intervals.size() - rem;
+}
