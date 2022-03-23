786eed237f389f57a4f950d322509f3a7fbc99f9
Mukul Taneja <mkltaneja@gmail.com>
2022-03-23 12:37:42 +0530

    DAY 23

--- Leetcode - 30-days Challenge 2022/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MARCH Challenge.cpp
@@ -500,3 +500,17 @@ string getSmallestString(int n, int k)
     }
     return ans;
 }
+
+// DAY 23 (991. Broken Calculator)=========================================================================================================================
+
+int brokenCalc(int startValue, int target) 
+{
+    int ans = 0;
+    while(target > startValue)
+    {
+        if(target&1) target++;
+        else target >>= 1;
+        ans++;
+    }
+    return ans + (startValue - target);
+}
