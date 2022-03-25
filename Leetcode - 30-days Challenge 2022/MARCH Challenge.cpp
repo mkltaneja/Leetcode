7b311d8c7b4543f9e7fba14a2f9acff044933873
Mukul Taneja <mkltaneja@gmail.com>
2022-03-26 01:48:45 +0530

    DAY 25

--- Leetcode - 30-days Challenge 2022/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MARCH Challenge.cpp
@@ -530,3 +530,19 @@ int numRescueBoats(vector<int>& people, int limit)
     }
     return boats;
 }
+
+// DAY 25 (1029. Two City Scheduling)=========================================================================================================================
+
+int twoCitySchedCost(vector<vector<int>>& costs) 
+{
+    int n = costs.size();
+    sort(costs.begin(), costs.end(), [](vector<int> &a, vector<int> &b){
+        return (a[0] - a[1]) < (b[0] - b[1]);
+    });
+    int ans = 0;
+    for(int i = 0; i < n>>1; i++)
+        ans += costs[i][0];
+    for(int i = n>>1; i < n; i++)
+        ans += costs[i][1];
+    return ans;
+}
