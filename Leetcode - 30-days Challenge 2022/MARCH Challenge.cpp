0ba9d254f61e159ebfd2687a3943ffcebf24ccd3
Mukul Taneja <mkltaneja@gmail.com>
2022-03-27 02:28:12 +0530

    DAY 27

--- Leetcode - 30-days Challenge 2022/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MARCH Challenge.cpp
@@ -546,3 +546,18 @@ int twoCitySchedCost(vector<vector<int>>& costs)
         ans += costs[i][1];
     return ans;
 }
+
+// DAY 26 (704. Binary Search)=========================================================================================================================
+
+int search(vector<int>& nums, int target) 
+{
+    int st = 0, end = nums.size()-1;
+    while(st <= end)
+    {
+        int mid = (st + end) / 2;
+        if(nums[mid] == target) return mid;
+        if(nums[mid] < target) st = mid + 1;
+        else end = mid - 1;
+    }
+    return -1;
+}
