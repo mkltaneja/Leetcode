c81cdf477ae146c14fbc7a87151811b0e3fc4708
Mukul Taneja <mkltaneja@gmail.com>
2022-04-03 01:47:38 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2022/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/APRIL Challenge.cpp
@@ -4,3 +4,24 @@ void reverseString(vector<char>& s)
 {
     reverse(s.begin(), s.end());
 }
+
+// DAY 2 (680. Valid Palindrome II)=================================================================================================
+
+bool ispal(string &s, int i, int j)
+{
+    while(i <= j && s[i] == s[j]) i++, j--;
+
+    return i > j;
+}
+
+bool validPalindrome(string s) 
+{
+    int i = 0, j = s.size()-1;
+    while(i <= j)
+    {
+        if(s[i] != s[j])
+            return ispal(s, i+1, j) || ispal(s, i, j-1);
+        i++, j--;
+    }
+    return true;
+}
