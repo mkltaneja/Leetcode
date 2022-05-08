47c95b363dfabc7070052e9ec7457afa982ff46b
Mukul Taneja <mkltaneja@gmail.com>
2022-05-09 01:15:31 +0530

    DAY 9

--- Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
@@ -195,3 +195,60 @@ bool find132pattern(vector<int>& nums)
 
     return false;
 }
+
+// DAY 8 (341. Flatten Nested List Iterator)=========================================================================================================================
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+public:
+    
+    int itr = 0;
+    vector<int> list;
+    void flatten(vector<NestedInteger> &nlist)
+    {
+        for(int i = 0; i < nlist.size(); i++)
+        {
+            if(!nlist[i].isInteger())
+                flatten(nlist[i].getList());
+            else list.push_back(nlist[i].getInteger());
+        }
+    }
+    
+    NestedIterator(vector<NestedInteger> &nestedList) 
+    {
+        flatten(nestedList);
+    }
+    
+    int next()
+    {
+        return list[itr++];
+    }
+    
+    bool hasNext() 
+    {
+        return itr < list.size();
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
