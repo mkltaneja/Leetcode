a21ca0f3d63043a2e7ce7cdf9ae5086b5205ac57
Mukul Taneja <mkltaneja@gmail.com>
2022-05-14 00:54:53 +0530

    DAY 13

--- Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
@@ -339,3 +339,48 @@ vector<vector<int>> permuteUnique(vector<int>& nums)
 
     return ans;
 }
+
+// DAY 13 (117. Populating Next Right Pointers in Each Node II)=========================================================================================================================
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) 
+    {
+        if(!root) return root;
+        queue<Node*> que;
+        que.push(root);
+        
+        while(!que.empty())
+        {
+            int sz = que.size();
+            while(sz--)
+            {
+                Node* node = que.front();
+                que.pop();
+                if(sz) node->next = que.front();
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+            }
+        }
+        
+        return root;
+    }
+};
