b61ee50a2603609f8674d47f90c408454916d269
Mukul Taneja <mkltaneja@gmail.com>
2022-05-21 03:10:15 +0530

    DAY 20

--- Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
@@ -553,3 +553,26 @@ vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections)
 
     return ans;
 }
+
+// DAY 20 (63. Unique Paths II)=========================================================================================================================
+
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+{
+    int n = obstacleGrid.size(), m = obstacleGrid[0].size();
+    if(obstacleGrid[0][0] || obstacleGrid[n-1][m-1]) return 0;
+
+    vector<vector<long>> dp(n, vector<long> (m,0));
+    dp[n-1][m-1] = 1;
+
+    for(int i = n-1; i >= 0; i--)
+    {
+        for(int j = m-1; j >= 0; j--)
+        {
+            if((i == n-1 && j == m-1) || obstacleGrid[i][j]) continue;
+            long dans = i+1 < n? dp[i+1][j] : 0;
+            long rans = j+1 < m? dp[i][j+1] : 0;
+            dp[i][j] = dans + rans;
+        }
+    }
+    return dp[0][0];
+}
