2a36cf8a99a85fffabeab4ebd37d8d83282a8c35
Mukul Taneja <mkltaneja@gmail.com>
2022-05-22 04:44:09 +0530

    DAY 21

--- Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/MAY Challenge.cpp
@@ -576,3 +576,18 @@ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
     }
     return dp[0][0];
 }
+
+// DAY 21 (322. Coin Change)=========================================================================================================================
+
+int coinChange(vector<int>& coins, int amount) 
+{
+    int n = coins.size();
+    vector<int> dp(amount+1, INT_MAX);
+    dp[0] = 0;
+
+    for(int i = 0; i < n; i++)
+        for(int x = coins[i]; x <= amount; x++)
+            dp[x] = min(dp[x], dp[x - coins[i]]==INT_MAX? INT_MAX : dp[x - coins[i]] + 1);
+
+    return dp[amount] == INT_MAX? -1 : dp[amount];
+}
