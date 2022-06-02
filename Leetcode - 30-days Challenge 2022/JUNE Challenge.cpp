332f4178e8d1e6dfcdc9dc6d70fcb36f8a6b1042
Mukul Taneja <mkltaneja@gmail.com>
2022-06-02 20:52:12 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2022/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JUNE Challenge.cpp
@@ -6,3 +6,15 @@ vector<int> runningSum(vector<int>& nums)
         nums[i] += i? nums[i-1] : 0;
     return nums;
 }
+
+// DAY 2 (867. Transpose Matrix)=========================================================================================================================
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) 
+{
+    int n = matrix.size(), m = matrix[0].size();
+    vector<vector<int>> ans(m, vector<int>(n));
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            ans[j][i] = matrix[i][j];
+    return ans;
+}
