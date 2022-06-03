b179a4bbe359f046986c6a759a5bfb7250143973
Mukul Taneja <mkltaneja@gmail.com>
2022-06-04 01:00:29 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2022/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JUNE Challenge.cpp
@@ -18,3 +18,40 @@ vector<vector<int>> transpose(vector<vector<int>>& matrix)
             ans[j][i] = matrix[i][j];
     return ans;
 }
+
+// DAY 3 (304. Range Sum Query 2D - Immutable)=========================================================================================================================
+
+class NumMatrix {
+public:
+    
+    vector<vector<int>> msum;
+    int n, m;
+    NumMatrix(vector<vector<int>>& matrix) 
+    {
+        n = matrix.size();
+        m = matrix[0].size();
+        msum.resize(n, vector<int>(m));
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                int u = i? msum[i-1][j] : 0;
+                int l = j? msum[i][j-1] : 0;
+                int d = i && j? msum[i-1][j-1] : 0;
+                msum[i][j] = l + u + matrix[i][j] - d;
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) 
+    {
+        return msum[row2][col2] - (row1? msum[row1-1][col2] : 0) - (col1? msum[row2][col1-1] : 0) + (row1 && col1? msum[row1-1][col1-1] : 0);
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
