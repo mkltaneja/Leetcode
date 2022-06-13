d3ff745a38e201e14a8209eb54b176ae433fdbbe
Mukul Taneja <mkltaneja@gmail.com>
2022-06-14 00:50:48 +0530

    DAY 13

--- Leetcode - 30-days Challenge 2022/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JUNE Challenge.cpp
@@ -262,3 +262,14 @@ int maximumUniqueSubarray(vector<int>& nums)
     }
     return maxscore;
 }
+
+// DAY 13 (120. Triangle)=========================================================================================================================
+
+int minimumTotal(vector<vector<int>>& triangle) 
+{
+    int n = triangle.size();
+    for(int i = n-2; i >= 0; i--)
+        for(int j = 0; j < triangle[i].size(); j++)
+            triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
+    return triangle[0][0];
+}
