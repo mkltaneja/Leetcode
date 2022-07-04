75ed435db53d1befdc2937d25804d48c370039e4
Mukul Taneja <mkltaneja@gmail.com>
2022-07-04 14:11:20 -0700

    DAY 4 OPTIMIZED

--- Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
@@ -80,3 +80,23 @@ int candy(vector<int>& ratings)
 
     return ans;
 }
+
+// APPROACH 2 --> O(n)
+
+int candy(vector<int>& ratings) 
+{
+    int n = ratings.size();
+    int ans = 0;
+
+    vector<int> candies(n,1);
+    for(int i = 1; i < n; i++)
+        if(ratings[i] > ratings[i-1])
+            candies[i] = candies[i-1] + 1;
+    for(int i = n-2; i >= 0; i--)
+        if(ratings[i] > ratings[i+1])
+            candies[i] = max(candies[i], candies[i+1] + 1);
+    for(int i = 0; i < n; i++)
+        ans += candies[i];
+
+    return ans;
+}
