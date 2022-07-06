bf834a34fdaf9deb49989bec85dc1194f509ea89
Mukul Taneja <mkltaneja@gmail.com>
2022-07-06 11:48:08 +0530

    DAY 6

--- Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
@@ -133,3 +133,18 @@ int longestConsecutive(vector<int>& nums)
     
     return ans;
 }
+
+// DAY 6 (509. Fibonacci Number)===================================================================================================
+
+int fib(int n) 
+{
+    if(n <= 1) return n;
+    int a = 0, b = 1, ans = 1;
+    for(int i = 2; i <= n; i++)
+    {
+        ans = a + b;
+        a = b;
+        b = ans;
+    }
+    return ans;
+}
