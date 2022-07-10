7beac478290d7fc73d2c6bfa378e24b074015e7a
Mukul Taneja <mkltaneja@gmail.com>
2022-07-11 01:43:21 +0530

    DAY 10

--- Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
@@ -231,3 +231,15 @@ int maxResult(vector<int>& nums, int k)
     }
     return nums[0];
 }
+
+// DAY 10 (746. Min Cost Climbing Stairs)============================================================================================
+
+int minCostClimbingStairs(vector<int>& cost) 
+{
+    int n = cost.size();
+    for(int i = n-2; i >= 0; i--)
+    {
+        cost[i] += min(cost[i+1], (i == n-2? 0 : cost[i+2]));
+    }
+    return min(cost[0], cost[1]);
+}
