39af0e58024c6ea90209218af88313f7cffdb24e
Mukul Taneja <mkltaneja@gmail.com>
2022-07-20 01:11:26 +0530

    DAY 19

--- Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/JULY Challenge.cpp
@@ -465,3 +465,21 @@ int numSubmatrixSumTarget(vector<vector<int>> &matrix, int target)
 
     return ans;
 }
+
+// DAY 19 (118. Pascal's Triangle)==================================================================================================================
+
+vector<vector<int>> generate(int numRows) 
+{
+    vector<vector<int>> ans;
+    ans.push_back({1});
+    
+    for(int i = 1; i < numRows; i++)
+    {
+        ans.push_back({1});
+        for(int j = 0; j < i-1; j++)
+            ans.back().push_back(ans[i-1][j] + ans[i-1][j+1]);
+        ans.back().push_back(1);
+    }
+    
+    return ans;
+}
