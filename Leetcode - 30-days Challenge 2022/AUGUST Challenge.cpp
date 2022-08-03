6cb24620c46389e2e16ee99c2e50d8e98690b85b
Mukul Taneja <mkltaneja@gmail.com>
2022-08-04 01:34:49 +0530

    AUGUST - DAY 2

--- Leetcode - 30-days Challenge 2022/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/AUGUST Challenge.cpp
@@ -15,3 +15,36 @@ int uniquePaths(int m, int n)
     }
     return dp[m - 1][n - 1];
 }
+
+// DAY 2 (378. Kth Smallest Element in a Sorted Matrix)============================================================
+
+int kthSmallest(vector<vector<int>> &matrix, int k)
+{
+    int n = matrix.size(), m = matrix[0].size();
+    int lo = matrix[0][0], hi = matrix[n - 1][m - 1];
+
+    while (lo < hi)
+    {
+        int x = lo + ((hi - lo) >> 1);
+        int tot = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int st = 0, end = m;
+            while (st < end)
+            {
+                int mid = st + ((end - st) >> 1);
+                if (matrix[i][mid] <= x)
+                    st = mid + 1;
+                else
+                    end = mid;
+            }
+            tot += st;
+        }
+        if (tot < k)
+            lo = x + 1;
+        else
+            hi = x;
+    }
+
+    return lo;
+}
