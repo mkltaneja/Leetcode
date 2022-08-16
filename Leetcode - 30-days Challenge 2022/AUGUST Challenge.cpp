efcaffa9507f1def35263536a90eefbe8b3fe641
Mukul Taneja <mkltaneja@gmail.com>
2022-08-16 10:55:29 +0530

    DAY 16

--- Leetcode - 30-days Challenge 2022/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/AUGUST Challenge.cpp
@@ -661,3 +661,18 @@ int romanToInt(string s)
 
     return ans;
 }
+
+// DAY 16 (387. First Unique Character in a String)==============================================================================
+
+int firstUniqChar(string s) 
+{
+    int cnt[26] = {0};
+    for(int i = 0; i < s.size(); i++)
+        cnt[s[i]-'a']++;
+
+    for(int i = 0; i < s.size(); i++)
+        if(cnt[s[i]-'a'] == 1) 
+            return i;
+
+    return -1;
+}
