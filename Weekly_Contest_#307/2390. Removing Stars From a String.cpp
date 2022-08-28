2415717818b007e70d1019d354973e423a630679
Mukul Taneja <mkltaneja@gmail.com>
2022-08-28 19:25:21 +0530

    Weekly Contest - B

---
+++ Weekly_Contest_#307/2390. Removing Stars From a String.cpp
@@ -0,0 +1,13 @@
+
+string removeStars(string s) 
+{
+    string ans = "";
+    for(char c : s)
+    {
+        if(c == '*')
+            ans.pop_back();
+        else ans += c;
+    }
+
+    return ans;
+}
