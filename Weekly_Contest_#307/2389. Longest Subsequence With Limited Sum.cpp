a1c42c5dfb57da5a0e809cb5ee4f3a1d48746680
Mukul Taneja <mkltaneja@gmail.com>
2022-08-28 19:24:52 +0530

    Weekly Contest 307 - A

---
+++ Weekly_Contest_#307/2389. Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,12 @@
+vector<int> answerQueries(vector<int>& nums, vector<int>& queries) 
+{
+    sort(nums.begin(), nums.end());
+    int m = queries.size();
+    for(int i = 1; i < nums.size(); i++)
+        nums[i] += nums[i-1];
+    vector<int> ans(m);
+    for(int i = 0; i < m; i++)
+        ans[i] = upper_bound(nums.begin(), nums.end(), queries[i]) - nums.begin();
+
+    return ans;
+}
