f76464b573f8ff7f38d6370c3d80b39b27beeb85
Mukul Taneja <mkltaneja@gmail.com>
2022-08-30 18:58:49 +0530

    DAY 30

--- Leetcode - 30-days Challenge 2022/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/AUGUST Challenge.cpp
@@ -985,3 +985,16 @@ int numIslands(vector<vector<char>>& grid)
                 islands++, dfs(i, j, n, m, grid);
     return islands;
 }
+
+// DAY 30 (48. Rotate Image)===================================================================================
+
+void rotate(vector<vector<int>>& matrix) 
+{
+    int n = matrix.size(), m = matrix[0].size();
+    for(int i = 0; i < n; i++)
+        for(int j = i+1; j < m; j++)
+            swap(matrix[i][j], matrix[j][i]);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m/2; j++)
+            swap(matrix[i][j], matrix[i][m-j-1]);
+}
