c3ed22f87d8d9923abac822603fb8ed9917ec807
Mukul Taneja <mkltaneja@gmail.com>
2022-09-03 01:11:36 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2022/SEPTEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/SEPTEMBER Challenge.cpp
@@ -12,3 +12,32 @@ int goodNodes(TreeNode* root)
 {
     return dfs(root, INT_MIN);
 }
+
+// DAY 2 (637. Average of Levels in Binary Tree)=======================================================================
+
+vector<double> averageOfLevels(TreeNode* root) 
+{
+    vector<double> ans;
+    queue<TreeNode*> que;
+    que.push(root);
+
+    while(!que.empty())
+    {
+        int sz = que.size();
+        double sum = 0, tot = 0;
+        while(sz--)
+        {
+            TreeNode* node = que.front();
+            que.pop();
+            sum += node->val;
+            tot++;
+            if(node->left)
+                que.push(node->left);
+            if(node->right)
+                que.push(node->right);
+        }
+        ans.push_back(sum/tot);
+    }
+
+    return ans;
+}
