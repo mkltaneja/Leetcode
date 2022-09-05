0ed889ece3418717a74fe75e312d47363a90951a
Mukul Taneja <mkltaneja@gmail.com>
2022-09-05 23:41:46 +0530

    DAY 5

--- Leetcode - 30-days Challenge 2022/SEPTEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/SEPTEMBER Challenge.cpp
@@ -110,3 +110,54 @@ vector<vector<int>> verticalTraversal(TreeNode* root)
 
     return ans;
 }
+
+// DAY 5 (429. N-ary Tree Level Order Traversal)===================================================================================
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) 
+    {
+        if(!root) return {};
+        vector<vector<int>> ans;
+        queue<Node*> que;
+        que.push(root);
+        
+        while(!que.empty())
+        {
+            int sz = que.size();
+            vector<int> curr;
+            while(sz--)
+            {
+                Node* node = que.front();
+                que.pop();
+                curr.push_back(node->val);
+                for(Node* child : node->children)
+                    que.push(child);
+            }
+            
+            ans.push_back(curr);
+        }
+        
+        return ans;
+    }
+};
