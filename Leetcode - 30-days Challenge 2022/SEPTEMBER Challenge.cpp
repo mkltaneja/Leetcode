388550bb1d6d6d54b6d60922bf2f46caf7925d52
Mukul Taneja <mkltaneja@gmail.com>
2022-09-11 02:13:01 +0530

    DAY 10

--- Leetcode - 30-days Challenge 2022/SEPTEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/SEPTEMBER Challenge.cpp
@@ -258,3 +258,20 @@ int numberOfWeakCharacters(vector<vector<int>>& properties)
 
     return ans;
 }
+
+// DAY 10 (188. Best Time to Buy and Sell Stock IV)===================================================================================
+
+int maxProfit(int k, vector<int>& prices) 
+{
+    vector<int> dp0(k+1, 0), dp1(k+1, INT_MIN);
+    for(int x : prices)
+    {
+        for(int i = k; i > 0; i--)
+        {
+            dp0[i] = max(dp0[i], dp1[i] + x);
+            dp1[i] = max(dp1[i], dp0[i-1] - x);
+        }
+    }
+
+    return dp0[k];
+}
