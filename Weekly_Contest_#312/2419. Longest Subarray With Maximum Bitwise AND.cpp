f43adafb6186ea1bececc79507cfb8b723d90362
Mukul Taneja <mkltaneja@gmail.com>
2022-09-26 02:39:34 +0530

    Weekly Contest 312 - B

---
+++ Weekly_Contest_#312/2419. Longest Subarray With Maximum Bitwise AND.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) 
+    {
+        int n = heights.size();
+        vector<pair<int,string>> v(n);
+        for(int i = 0; i < n; i++)
+            v[i] = {heights[i], names[i]};
+        sort(v.rbegin(), v.rend());
+        
+        vector<string> ans;
+        for(auto p : v)
+            ans.push_back(p.second);
+        
+        return ans;
+    }
+};
