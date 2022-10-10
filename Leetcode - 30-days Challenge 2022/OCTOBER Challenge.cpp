e5662f1f18cb424eb25342b4d2686910d6191005
Mukul Taneja <mkltaneja@gmail.com>
2022-10-11 02:05:23 +0530

    DAY 10

--- Leetcode - 30-days Challenge 2022/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/OCTOBER Challenge.cpp
@@ -245,3 +245,23 @@ bool findTarget(TreeNode* root, int k)
 
     return false;
 }
+
+// DAY 10 (1328. Break a Palindrome)======================================================================================================
+
+string breakPalindrome(string palindrome) 
+{
+    int n = palindrome.size();
+    if(n == 1) return "";
+
+    for(int i = 0; i < n/2; i++)
+    {
+        if(palindrome[i] != 'a')
+        {
+            palindrome[i] = 'a';
+            return palindrome;
+        }
+    }
+    palindrome[n-1] = 'b';
+
+    return palindrome;
+}
