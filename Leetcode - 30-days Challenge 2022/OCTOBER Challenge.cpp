1008547fa2020587ef5e1be02f25a66a88ae2692
Mukul Taneja <mkltaneja@gmail.com>
2022-10-12 23:50:17 +0530

    day 12

--- Leetcode - 30-days Challenge 2022/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/OCTOBER Challenge.cpp
@@ -280,3 +280,14 @@ bool increasingTriplet(vector<int>& nums)
 
     return false;
 }
+
+// DAY 12 (976. Largest Perimeter Triangle)===========================================================================================
+
+int largestPerimeter(vector<int>& nums) 
+{
+    sort(nums.begin(), nums.end());
+    for(int i = nums.size()-3; i >= 0; i--)
+        if(nums[i] + nums[i+1] > nums[i+2])
+            return nums[i] + nums[i+1] + nums[i+2];
+    return 0;
+}
