8d396dbb6e0e8d6f4e4587d68b8e5f489cf174f6
Mukul Taneja <mkltaneja@gmail.com>
2022-10-23 12:31:50 +0530

    Weekly Contest 316 - A

---
+++ Weekly_Contest_#316/2446. Determine if Two Events Have Conflict.cpp
@@ -0,0 +1,34 @@
+// APPROACH 1 (Creating minutes array)
+
+class Solution {
+public:
+    
+    int mins[25];
+    int minn(int h, int m)
+    {
+        return mins[h] + m;
+    }
+    
+    bool haveConflict(vector<string>& event1, vector<string>& event2) 
+    {
+        int hs1 = stoi(event1[0].substr(0,2));
+        int ms1 = stoi(event1[0].substr(3,2));
+        int he1 = stoi(event1[1].substr(0,2));
+        int me1 = stoi(event1[1].substr(3,2));
+        
+        int hs2 = stoi(event2[0].substr(0,2));
+        int ms2 = stoi(event2[0].substr(3,2));
+        int he2 = stoi(event2[1].substr(0,2));
+        int me2 = stoi(event2[1].substr(3,2));
+        
+        // mins[0] = 0;
+        for(int i = 1; i < 25; i++)
+            mins[i] = i*60 + mins[i-1];
+        
+        int arrive = max(minn(hs1, ms1), minn(hs2, ms2));
+        int dep = min(minn(he1, me1), minn(he2, me2));
+        
+        int tot = dep - arrive + 1;
+        return tot > 0;
+    }
+};
