d84213d5e719ff31626d697d90fdaa83f5da3530
Mukul Taneja <mkltaneja@gmail.com>
2022-10-30 16:08:33 +0530

    Weekly Contest 317 - C

---
+++ Weekly_Contest_#317/2457. Minimum Addition to Make Integer Beautiful.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    #define ll long long
+    
+    int sod(long long x)
+    {
+        int sum = 0;
+        while(x)
+        {
+            sum += x%10;
+            x /= 10;
+        }
+        return sum;
+    }
+    
+    long long makeIntegerBeautiful(long long n, int target) 
+    {
+        ll i = 1, p = 1;
+        while(n/p)
+        {
+            if(i == 1)
+            {
+                if(sod(n) <= target) return 0;
+                int k = (10 - (n%10)) % 10;
+                if(sod(n + k) <= target) return k;
+                i++;
+            }
+            for(ll j = (1 << i)-1; j >= (1 << (i-1))-1; j--)
+            {
+                ll tmp = j, nn = n, pp = 1, x = 0;
+                while(tmp)
+                {
+                    ll ppp = 1;
+                    while(tmp&1)
+                    {
+                        tmp >>= 1;
+                        ppp *= 10;
+                    }
+                    x += ((ppp - (nn % ppp)) % ppp) * pp;
+                    tmp >>= 1;
+                    nn /= ppp*10;
+                    pp *= 10;
+                }
+                
+                if(sod(n+x) <= target) 
+                    return x;
+            }
+            i++;
+            p *= 10;
+        }
+        
+        return -1;
+    }
+};
