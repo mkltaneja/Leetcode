be9f9fd99dc84bb1397f067eb9007883c0df17ad
Mukul Taneja <mkltaneja@gmail.com>
2022-11-01 00:15:43 +0530

    DAY 31

--- Leetcode - 30-days Challenge 2022/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/OCTOBER Challenge.cpp
@@ -758,3 +758,16 @@ int shortestPath(vector<vector<int>>& grid, int k)
 
     return -1;
 }
+
+// DAY 31 (766. Toeplitz Matrix)=================================================================================
+
+bool isToeplitzMatrix(vector<vector<int>>& matrix) 
+{
+    int n = matrix.size(), m = matrix[0].size();
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            if(i && j && matrix[i][j] != matrix[i-1][j-1])
+                return false;
+
+    return true;
+}
