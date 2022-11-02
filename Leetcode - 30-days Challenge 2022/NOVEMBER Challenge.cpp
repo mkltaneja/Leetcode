bf64bacef2ae13dfd738b7b662609ee3820eeae7
Mukul Taneja <mkltaneja@gmail.com>
2022-11-02 18:16:27 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2022/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/NOVEMBER Challenge.cpp
@@ -19,3 +19,47 @@ vector<int> findBall(vector<vector<int>>& grid)
 
     return ans;
 }
+
+// DAY 2 (433. Minimum Genetic Mutation)=================================================================================
+
+int minMutation(string start, string end, vector<string>& bank) 
+{
+    if(start == end) return 0;
+
+    unordered_set<string> st;
+    for(string &s : bank)
+        st.insert(s);
+
+    queue<string> que;
+    que.push(start);
+
+    int lvl = 0;
+    while(!que.empty())
+    {
+        int sz = que.size();
+        while(sz--)
+        {
+            string curr = que.front();
+            que.pop();
+
+            for(int i = 0; i < 8; i++)
+            {
+                for(char c : {'A', 'C', 'G', 'T'})
+                {
+                    if(c == curr[i]) continue;
+
+                    string nxt = curr.substr(0,i) + c + curr.substr(i+1);
+                    if(!st.count(nxt)) continue;
+                    st.erase(nxt);
+
+                    if(nxt == end) return lvl+1;
+
+                    que.push(nxt);
+                }
+            }
+        }
+        lvl++;
+    }
+
+    return -1;
+}
