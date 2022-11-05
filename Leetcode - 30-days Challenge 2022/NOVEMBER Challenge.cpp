2f5f29d95bef1312e18f37a32d81396c84ebaa1c
Mukul Taneja <mkltaneja@gmail.com>
2022-11-05 18:42:54 +0530

    DAY 5

--- Leetcode - 30-days Challenge 2022/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/NOVEMBER Challenge.cpp
@@ -113,3 +113,74 @@ string reverseVowels(string s)
 
     return s;
 }
+
+// DAY 5 (212. Word Search II)========================================================================================================
+
+class Trie
+{
+    public:
+    vector<Trie*> trie;
+    bool wordEnd;
+    Trie()
+    {
+        this->trie.assign(26, nullptr);
+        this->wordEnd = false;
+    }
+
+    void insert(Trie* &node, string &word)
+    {
+        Trie* itr = node;
+        for(char &c : word)
+        {
+            if(!itr->trie[c-'a'])
+                itr->trie[c-'a'] = new Trie();
+            itr = itr->trie[c-'a'];
+        }
+        itr->wordEnd = true;
+    }
+};
+Trie* root = new Trie();
+
+unordered_set<string> res;
+void dfs(int i, int j, int n, int m, string &curr, vector<vector<char>> &board, Trie* node, vector<vector<bool>> &vis)
+{
+    if(i == -1 || j == -1 || i == n || j == m || !node->trie[board[i][j]-'a'] || vis[i][j])
+        return;
+
+    curr += board[i][j];
+    vis[i][j] = true;
+
+    if(node->trie[board[i][j]-'a']->wordEnd) res.insert(curr);
+
+    dfs(i+1, j, n, m, curr, board, node->trie[board[i][j]-'a'], vis);
+    dfs(i, j+1, n, m, curr, board, node->trie[board[i][j]-'a'], vis);
+    dfs(i-1, j, n, m, curr, board, node->trie[board[i][j]-'a'], vis);
+    dfs(i, j-1, n, m, curr, board, node->trie[board[i][j]-'a'], vis);
+
+    curr.pop_back();
+    vis[i][j] = false;
+}
+
+vector<string> findWords(vector<vector<char>>& board, vector<string>& words) 
+{
+    int n = board.size(), m = board[0].size();
+
+    for(string &word : words)
+        root->insert(root, word);
+
+    vector<vector<bool>> vis(n, vector<bool>(m, false));
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            string curr = "";
+            dfs(i, j, n, m, curr, board, root, vis);
+        }
+    }
+
+    vector<string> ans;
+    for(string s : res)
+        ans.push_back(s);
+
+    return ans;
+}
