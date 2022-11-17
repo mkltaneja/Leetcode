4c79c2270e8e076e1ca84f988e51b1b3bd4012b1
Mukul Taneja <mkltaneja@gmail.com>
2022-11-18 01:02:00 +0530

    DAY 17

--- Leetcode - 30-days Challenge 2022/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/NOVEMBER Challenge.cpp
@@ -404,3 +404,24 @@ public:
         return st;
     }
 };
+
+// DAY 17 (223. Rectangle Area)===========================================================================================
+
+int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) 
+{
+    int a1 = (ax2-ax1) * (ay2-ay1);
+    int a2 = (bx2-bx1) * (by2-by1);
+    int a3 = 0;
+
+    int x1 = max(ax1, bx1);
+    int x2 = min(ax2, bx2);
+    int y1 = max(ay1, by1);
+    int y2 = min(ay2, by2);
+
+    int x = x2 - x1;
+    int y = y2 - y1;
+    if(x > 0 && y > 0) 
+        a3 = x*y;
+
+    return a1 + a2 - a3;
+}
