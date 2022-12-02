00dd629b2c1f2c5168f277997f55fdf4741ebebb
Mukul Taneja <mkltaneja@gmail.com>
2022-12-02 18:28:03 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
@@ -18,3 +18,27 @@ bool halvesAreAlike(string s)
     }
     return cnt == 0;
 }
+
+// DAY 2 (1657. Determine if Two Strings Are Close)==========================================================================
+
+bool closeStrings(string word1, string word2) 
+{
+    vector<int> map1(26), map2(26);
+    for(char &c : word1)
+        map1[c-'a']++;
+    for(char &c : word2)
+        map2[c-'a']++;
+
+    vector<int> unique1, unique2;
+    for(int i = 0; i < 26; i++)
+        if(map1[i])
+            unique1.push_back(i);
+    for(int i = 0; i < 26; i++)
+        if(map2[i])
+            unique2.push_back(i);
+
+    sort(map1.begin(), map1.end());
+    sort(map2.begin(), map2.end());
+
+    return (unique1 == unique2) && (map1 == map2);
+}
