60a54dff9cca0c61dd41e73f8f6d9b6016bdccc9
Mukul Taneja <mkltaneja@gmail.com>
2022-12-07 20:48:53 +0530

    DAY 7

--- Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
@@ -124,3 +124,15 @@ ListNode* oddEvenList(ListNode* head)
 
     return head;
 }
+
+// DAY 7 (938. Range Sum of BST)===================================================================================
+
+int rangeSumBST(TreeNode* root, int low, int high) 
+{
+    if(!root) return 0;
+    if(root->val < low)
+        return rangeSumBST(root->right, low, high);
+    if(root->val > high)
+        return rangeSumBST(root->left, low, high);
+    return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high) + root->val;
+}
