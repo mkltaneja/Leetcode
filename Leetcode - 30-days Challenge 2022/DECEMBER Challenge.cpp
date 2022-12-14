5c9d44b6a1719dc9c16b209d6314a016a678e477
Mukul Taneja <mkltaneja@gmail.com>
2022-12-14 19:56:22 +0530

    DAY 14

--- Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
@@ -271,3 +271,16 @@ int minFallingPathSum(vector<vector<int>>& matrix)
 
     return ans;
 }
+
+// DAY 14 (198. House Robber)===================================================================================================
+
+int rob(vector<int>& nums) 
+{
+    int n = nums.size();
+    vector<int> dp(n+1, 0);
+    dp[n-1] = nums[n-1];
+    for(int i = n-2; i >= 0; i--)
+        dp[i] = max(dp[i+1], nums[i] + dp[i+2]);
+
+    return dp[0];
+}
