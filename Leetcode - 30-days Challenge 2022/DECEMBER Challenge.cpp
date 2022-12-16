c507507a01b1dadef41f3eccf107a35699ed3750
Mukul Taneja <mkltaneja@gmail.com>
2022-12-16 10:51:14 -0800

    DAY 15

--- Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
@@ -306,3 +306,59 @@ int longestCommonSubsequence(string text1, string text2)
 
     return ans;
 }
+
+// DAY 16 (232. Implement Queue using Stacks)================================================================================================
+
+class MyQueue {
+public:
+
+    stack<int> st1, st2;
+    MyQueue() {
+        
+    }
+
+    void fill()
+    {
+        if(st2.empty())
+        {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+    }
+    
+    void push(int x) 
+    {
+        st1.push(x);
+    }
+    
+    int pop() 
+    {
+        fill();
+        int ans = st2.top();
+        st2.pop();
+        return ans;
+    }
+    
+    int peek() 
+    {
+        fill();
+        return st2.top();
+    }
+    
+    bool empty() 
+    {
+        return st1.empty() && st2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
