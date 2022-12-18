0bb952410e5802c25bcb364c84534b11b0de0fcd
Mukul Taneja <mkltaneja@gmail.com>
2022-12-18 20:17:23 +0530

    Weekly Contest 324 - D OPTIMIZED

--- Weekly_Contest_#324/2509. Cycle Length Queries in a Tree.cpp
+++ Weekly_Contest_#324/2509. Cycle Length Queries in a Tree.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (Finding height by logarithm)
+
 class Solution {
 public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) 
@@ -32,6 +35,33 @@ public:
             int dp = log2(a);
             
             ans.push_back((da - dp) + (db - dp) + 1);
+        }
+        
+        return ans;
+    }
+};
+
+// APPROACH 2 (Without finding height by logarithm)  [OPTIMIZED]
+
+class Solution {
+public:
+    vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) 
+    {
+        vector<int> ans;
+        for(auto &q : queries)
+        {
+            int a = q[0];
+            int b = q[1];
+            
+            int ht = 0;
+            while(a != b)
+            {
+                if(a > b) a >>= 1;
+                else b >>= 1;
+                ht++;
+            }
+            
+            ans.push_back(ht+1);
         }
         
         return ans;
