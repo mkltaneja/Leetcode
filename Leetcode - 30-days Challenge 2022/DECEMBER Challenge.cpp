6ee110ed3fff86f5502751918635b856eef57c11
Mukul Taneja <mkltaneja@gmail.com>
2022-12-18 20:32:21 +0530

    DAY 18

--- Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2022/DECEMBER Challenge.cpp
@@ -397,3 +397,23 @@ int evalRPN(vector<string>& tokens)
 
     return st.top();
 }
+
+// DAY 18 (739. Daily Temperatures)================================================================================
+
+vector<int> dailyTemperatures(vector<int>& temperatures) 
+{
+    int n = temperatures.size();
+    stack<int> st;
+    vector<int> ans(n, 0);
+    for(int i = 0; i < n; i++)
+    {
+        while(!st.empty() && temperatures[i] > temperatures[st.top()])
+        {
+            ans[st.top()] = i - st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    return ans;
+}
