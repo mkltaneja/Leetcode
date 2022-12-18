8f80422361dc02fd51bfbe5d626a650019e1881b
Mukul Taneja <mkltaneja@gmail.com>
2022-12-18 20:13:24 +0530

    Weekly Contest 324 - A Approach 2

--- Weekly_Contest_#324/2506. Count Pairs Of Similar Strings.cpp
+++ Weekly_Contest_#324/2506. Count Pairs Of Similar Strings.cpp
@@ -1,3 +1,35 @@
+
+// APPROACH 1 (Using set)
+
+class Solution {
+public:
+    int similarPairs(vector<string>& words) 
+    {
+        unordered_map<string,int> mp;
+        for(string &s : words)
+        {
+            string tmp = "";
+            unordered_set<char> st;
+            for(char &c : s)
+            {
+                if(st.count(c)) continue;
+                st.insert(c);
+                tmp += c;
+            }
+            sort(tmp.begin(), tmp.end());
+            mp[tmp]++;
+        }
+        
+        int ans = 0;
+        for(auto &p : mp)
+            ans += (p.second * (p.second-1))/2;
+        
+        return ans;
+    }
+};
+
+// APPROACH 2 (Using Bits)  [OPTIMIZED]
+
 class Solution {
 public:
     int similarPairs(vector<string>& words) 
