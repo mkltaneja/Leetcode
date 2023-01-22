ae32a0fe55734aa9b128c766aee280eb68903409
Mukul Taneja <mkltaneja@gmail.com>
2023-01-22 14:19:40 +0530

    Weekly Contest 329 - b

---
+++ Weekly_Contest_#329/2545. Sort the Students by Their Kth Score.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) 
+    {
+        sort(score.begin(), score.end(), [&](auto const &a, auto const &b){
+            return a[k] > b[k];
+        });
+        
+        return score;
+    }
+};
