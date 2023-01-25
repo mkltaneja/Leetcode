e775cf3640a698324178a3140bea40b6b6f0ed0a
Mukul Taneja <mkltaneja@gmail.com>
2023-01-26 01:02:48 +0530

    DAY 25

--- Leetcode - 30-days Challenge 2023/JANUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/JANUARY Challenge.cpp
@@ -748,3 +748,40 @@ int snakesAndLadders(vector<vector<int>>& board)
 
     return -1;
 }
+
+// DAY 25 (2359. Find Closest Node to Given Two Nodes)==========================================================================
+
+void dfs(int u, vector<int> &dist, vector<int>& edges)
+{
+    if(edges[u] == -1) return;
+
+    if(dist[edges[u]] == -1)
+    {
+        dist[edges[u]] = dist[u] + 1;
+        dfs(edges[u], dist, edges);
+    }
+}
+
+int closestMeetingNode(vector<int>& edges, int node1, int node2) 
+{
+    int n = edges.size();
+    vector<int> dist1(n,-1), dist2(n,-1);
+    dist1[node1] = 0;
+    dist2[node2] = 0;
+    dfs(node1, dist1, edges);
+    dfs(node2, dist2, edges);
+
+    int ans = -1, minDist = INT_MAX;
+    for(int i = 0; i < n; i++)
+    {
+        if(dist1[i] == -1 || dist2[i] == -1) continue;
+        int maxDist = max(dist1[i], dist2[i]);
+        if(maxDist < minDist)
+        {
+            minDist = maxDist;
+            ans = i;
+        }
+    }
+
+    return ans;
+}
