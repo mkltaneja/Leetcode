60ae4fba72ef1182a3e409be484c12052d027a27
Mukul Taneja <mkltaneja@gmail.com>
2023-02-04 22:48:11 +0530

    Biweekly Contest 97 - C OPTIMIZED

--- Biweekly_Contest_#97/2555. Maximize Win From Two Segments.cpp
+++ Biweekly_Contest_#97/2555. Maximize Win From Two Segments.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (Finding the previous valid pointer by Binary Search every time) --> time = O(n*logn)
+
 class Solution {
 public:
     int maximizeWin(vector<int>& pp, int k) 
@@ -18,6 +21,26 @@ public:
             int ub = upper_bound(pp.begin(), pp.begin()+i+1, pp[i]-k-1) - pp.begin();
             int lans = i - ub + 1;
             ans = max(ans, lans + (i+1 < n? rans[i+1] : 0));
+        }
+        
+        return ans;
+    }
+};
+
+// APPROACH 2 (2 Pointer approach) --> time = O(n)
+
+class Solution {
+public:
+    int maximizeWin(vector<int>& pp, int k) 
+    {
+        int n = pp.size();
+        int ans = 0;
+        vector<int> cmax(n);
+        for(int i = 0, j = 0; i < n; i++)
+        {
+            while(pp[j] + k < pp[i]) j++;
+            cmax[i] = max(i-j+1, (i? cmax[i-1] : 0));
+            ans = max(ans, i-j+1 + (j? cmax[j-1] : 0));
         }
         
         return ans;
