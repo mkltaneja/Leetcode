52b200d065a5fb61cb731e9b06aa441673074483
Mukul Taneja <mkltaneja@gmail.com>
2023-02-14 01:40:55 +0530

    Weekly Contest 332 - B OPTIMIZED

--- Weekly_Contest_#332/2563. Count the Number of Fair Pairs.cpp
+++ Weekly_Contest_#332/2563. Count the Number of Fair Pairs.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (Finding lb and ub with binary search everytime) --> time = O(n*logn)
+
 class Solution {
 public:
 
@@ -13,6 +16,28 @@ public:
             int ub = upper_bound(nums.begin()+i+1, nums.end(), upper - nums[i]) - nums.begin();
             if(lb == n || nums[i] + nums[lb] > upper) continue;
             ans += (ll)(ub - lb);
+        }
+        
+        return ans;
+    }
+};
+
+// APPROACH 2 (Finding lb and ub linearly) --> time = O(n) [OPTIMIZED]
+
+class Solution {
+public:
+
+    #define ll long long
+    long long countFairPairs(vector<int>& nums, int lower, int upper) 
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        ll ans = 0;
+        for(int i = 0, lo = n-1, hi = n-1; i < n && hi > i; i++)
+        {
+            while(lo >= 0 && nums[i] + nums[lo] >= lower) lo--;
+            while(hi > i && nums[i] + nums[hi] > upper) hi--;
+            ans += hi - max(lo, i);
         }
         
         return ans;
