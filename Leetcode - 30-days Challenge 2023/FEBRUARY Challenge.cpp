f9fc58ef39300aeddb175ce75c0f02a2055ba004
Mukul Taneja <mkltaneja@gmail.com>
2023-02-27 00:50:50 +0530

    DAY 26

--- Leetcode - 30-days Challenge 2023/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/FEBRUARY Challenge.cpp
@@ -679,3 +679,28 @@ int maxProfit(vector<int>& prices)
     }
     return ans;
 }
+
+// DAY 26 (72. Edit Distance)=======================================================================================
+
+int minDistance(string word1, string word2)
+{
+    int n = word1.size(), m = word2.size();
+    vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+    for(int i = 0; i <= n; i++)
+    {
+        for(int j = 0; j <= m; j++)
+        {
+            if(!i || !j)
+            {
+                dp[i][j] = i? i : j;
+                continue;
+            }
+            if(word1[i-1] == word2[j-1]) 
+                dp[i][j] = i && j? dp[i-1][j-1] : 0;
+            else 
+                dp[i][j] = min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]}) + 1;
+        }
+    }
+
+    return dp[n][m];
+}
