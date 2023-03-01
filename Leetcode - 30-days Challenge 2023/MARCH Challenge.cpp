9e9b5895133f7fd14843f2df3046f5e66505382e
Mukul Taneja <mkltaneja@gmail.com>
2023-03-01 19:03:40 +0530

    DAY 1 OPTIMIZED

--- Leetcode - 30-days Challenge 2023/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/MARCH Challenge.cpp
@@ -1,5 +1,7 @@
 // DAY 1 (912. Sort an Array)=====================================================================================================
 
+// APPROACH 2 (Merge Sort) --> Time = O(nlogn), Space = O(n)
+
 vector<int> merge(vector<int> &l, vector<int> &r)
 {
     int n = l.size(), m = r.size();
@@ -32,4 +34,41 @@ vector<int> mergeSort(int lo, int hi, vector<int> &nums)
 vector<int> sortArray(vector<int>& nums) 
 {
     return mergeSort(0, nums.size()-1, nums);
+}
+
+// APPROACH 2 (Heap Sort) --> Time = O(nlogn), Space = O(1)
+
+void maxHeapify(int i, int n, vector<int> &nums)
+{
+    int l = 2*i + 1, r = 2*i + 2;
+    int largest = i;
+    if(l < n && nums[l] > nums[largest])
+        largest = l;
+    if(r < n && nums[r] > nums[largest])
+        largest = r;
+
+    if(i != largest)
+    {
+        swap(nums[i], nums[largest]);
+        maxHeapify(largest, n, nums);
+    }
+}
+
+void heapSort(vector<int> &nums)
+{
+    int n = nums.size();
+    for(int i = n/2-1; i >= 0; i--)
+        maxHeapify(i, n, nums);
+
+    for(int i = n-1; i > 0; i--)
+    {
+        swap(nums[0], nums[i]);
+        maxHeapify(0, i, nums);
+    }
+}
+
+vector<int> sortArray(vector<int>& nums) 
+{
+    heapSort(nums);
+    return nums;
 }
