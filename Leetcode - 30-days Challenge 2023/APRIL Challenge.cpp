c0c4895c99261b8bfe44147d1e5ea61a3ff3ad99
Mukul Taneja <mkltaneja@gmail.com>
2023-04-06 20:55:34 +0530

    DAY 6

--- Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
@@ -87,3 +87,49 @@ int minimizeArrayValue(vector<int>& nums)
 
     return ans;
 }
+
+// DAY 6 (1254. Number of Closed Islands)=========================================================================
+
+int n = 0, m = 0;
+void dfs(int i, int j, vector<vector<int>> &grid)
+{
+    if(i == -1 || j == -1 || i == n || j == m || grid[i][j])
+        return;
+    grid[i][j] = 1;
+    dfs(i+1, j, grid);
+    dfs(i, j+1, grid);
+    dfs(i-1, j, grid);
+    dfs(i, j-1, grid);
+}
+
+int closedIsland(vector<vector<int>>& grid) 
+{
+    n = grid.size(), m = grid[0].size();
+    for(int i = 0; i < n; i++)
+    {
+        if(grid[i][0] == 0)
+            dfs(i, 0, grid);
+        if(grid[i][m-1] == 0)
+            dfs(i, m-1, grid);
+    }
+    for(int i = 0; i < m; i++)
+    {
+        if(grid[0][i] == 0)
+            dfs(0, i, grid);
+        if(grid[n-1][i] == 0)
+            dfs(n-1, i, grid);
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            if(grid[i][j]) continue;
+            dfs(i, j, grid);
+            ans++;
+        }
+    }
+
+    return ans;
+}
