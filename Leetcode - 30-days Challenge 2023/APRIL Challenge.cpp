8cf6daa620132f4df7530a56bd0b6b471432d639
Mukul Taneja <mkltaneja@gmail.com>
2023-04-09 01:50:20 +0530

    DAY 7

--- Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
@@ -172,3 +172,46 @@ int numEnclaves(vector<vector<int>>& grid)
 
     return cells;
 }
+
+// DAY 8 (133. Clone Graph)================================================================================
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    unordered_map<int, Node*> vis;
+    Node* cloneGraph(Node* node) 
+    {
+        if(!node) return nullptr;
+        if(vis.count(node->val)) return vis[node->val];
+        
+        Node* root = new Node(node->val);
+        vis[node->val] = root;
+
+        for(Node* neighbor : node->neighbors)
+            root->neighbors.push_back(cloneGraph(neighbor));
+        
+
+        return root;
+    }
+};
