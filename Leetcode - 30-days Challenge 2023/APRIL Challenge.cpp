da819f5a2ab66b02488bee0873c36ec20b8e5ba6
Mukul Taneja <mkltaneja@gmail.com>
2023-04-12 01:15:43 +0530

    DAY 11

--- Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
@@ -280,3 +280,25 @@ bool isValid(string s)
 
     return st.empty();
 }
+
+// DAY 11 (2390. Removing Stars From a String)===============================================================
+
+string removeStars(string s) 
+{
+    stack<char> st;
+    for(char &c : s)
+    {
+        if(c != '*')
+            st.push(c);
+        else st.pop();
+    }
+    string ans = "";
+    while(!st.empty())
+    {
+        ans += st.top();
+        st.pop();
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
