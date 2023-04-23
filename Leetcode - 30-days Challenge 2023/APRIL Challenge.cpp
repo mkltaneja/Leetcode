d2d4e7c21bebbc01135c0a59b5745d7eb98cc281
Mukul Taneja <mkltaneja@gmail.com>
2023-04-24 01:25:50 +0530

    DAY 23

--- Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/APRIL Challenge.cpp
@@ -586,3 +586,26 @@ int minInsertions(string s)
 
     return n - maxComSubseq;
 }
+
+// DAY 23 (1416. Restore The Array)======================================================================================
+
+int mod = 1e9 + 7;
+int numberOfArrays(string s, int k) 
+{
+    int n = s.size();
+    vector<int> dp(n+1, 0);
+    dp[n] = 1;
+    for(int i = n-1; i >= 0; i--)
+    {
+        long x = 0;
+        if(s[i] == '0') continue;
+        for(int j = i; j < n; j++)
+        {
+            x = x*10 + (s[j]-'0');
+            if(x > k) break;
+            dp[i] = (dp[i] % mod + dp[j+1] % mod) % mod;
+        }
+    }
+
+    return dp[0];
+}
