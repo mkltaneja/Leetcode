93747852382ba462881b072addfb5530639bd7f3
Mukul Taneja <mkltaneja@gmail.com>
2023-05-07 00:11:49 +0530

    DAY 6

--- Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
@@ -69,3 +69,38 @@ int maxVowels(string s, int k)
     }
     return ans;
 }
+
+// DAY 6 (1498. Number of Subsequences That Satisfy the Given Sum Condition)=========================================================================
+
+int mod = 1e9 + 7;
+
+int binpowmod(int a, int b)
+{
+    long ans = 1;
+    while(b)
+    {
+        if(b & 1)
+            ans = (ans % mod * a % mod) % mod;
+        a = ((long)a % mod * a % mod) % mod;
+        b >>= 1;
+    }
+
+    return ans % mod;
+}
+
+int numSubseq(vector<int>& nums, int target) 
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    int ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int j = upper_bound(nums.begin() + i, nums.end(), target - nums[i]) - nums.begin();
+        if(i == j) continue;
+        int x = binpowmod(2, j - i - 1);
+        ans = (ans % mod + x % mod) % mod;
+    }
+
+    return ans;
+}
