d5af95003675ae1975ab552e58a1d67a736a94d5
Mukul Taneja <mkltaneja@gmail.com>
2023-05-08 23:23:42 +0530

    DAY 8

--- Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
@@ -104,3 +104,14 @@ int numSubseq(vector<int>& nums, int target)
 
     return ans;
 }
+
+// DAY 9 (1572. Matrix Diagonal Sum)======================================================================================
+
+int diagonalSum(vector<vector<int>>& mat) 
+{
+    int n = mat.size();
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += mat[i][i] + mat[i][n-i-1];
+    return sum - ((n&1)? mat[n/2][n/2] : 0);
+}
