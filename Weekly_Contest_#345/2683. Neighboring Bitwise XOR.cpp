dbb1dece01598e2cf9e42de374571d97bf50ca8c
Mukul Taneja <mkltaneja@gmail.com>
2023-05-14 09:58:19 +0530

    Update 2683. Neighboring Bitwise XOR.cpp

--- Weekly_Contest_#345/2683. Neighboring Bitwise XOR.cpp
+++ Weekly_Contest_#345/2683. Neighboring Bitwise XOR.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1
+
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) 
@@ -8,5 +11,19 @@ public:
         for(int i = 1; i < n; i++)
             ans[i] = (ans[i-1] ^ derived[i-1]);
         return (ans[n-1] ^ ans[0]) == derived[n-1];
+    }
+};
+
+// APPROACH 2 (Simplified)
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) 
+    {
+        int x = 1;
+        for(int y : derived)
+            x ^= y;
+        
+        return x;
     }
 };
