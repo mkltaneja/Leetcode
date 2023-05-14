7fd6c56f0cabbef9c444a4e663819b47811cbaec
Mukul Taneja <mkltaneja@gmail.com>
2023-05-14 09:57:11 +0530

    Update 2682. Find the Losers of the Circular Game.cpp

--- Weekly_Contest_#345/2682. Find the Losers of the Circular Game.cpp
+++ Weekly_Contest_#345/2682. Find the Losers of the Circular Game.cpp
@@ -22,3 +22,26 @@ public:
         return ans;
     }
 };
+
+// APPROACH 2 (Simplified)
+
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) 
+    {
+        vector<int> vis(n);
+        for(int i = 1, x = 0; i <= n; i++)
+        {
+            if(vis[x]) break;
+            vis[x] = 1;
+            x = (x + i*k) % n;
+        }
+        
+        vector<int> ans;
+        for(int i = 0; i < n; i++)
+            if(!vis[i])
+                ans.push_back(i+1);
+        
+        return ans;
+    }
+};
