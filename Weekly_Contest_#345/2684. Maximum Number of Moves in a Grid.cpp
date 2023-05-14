0a4e49167226de974ab1597a29ff838cee071965
Mukul Taneja <mkltaneja@gmail.com>
2023-05-14 09:58:42 +0530

    Create 2684. Maximum Number of Moves in a Grid.cpp

---
+++ Weekly_Contest_#345/2684. Maximum Number of Moves in a Grid.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    #define f first
+    #define s second
+    int maxMoves(vector<vector<int>>& grid) 
+    {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+        set<pair<int,int>> vis;
+        queue<pair<int,int>> que;
+        for(int i = 0; i < n; i++)
+        {
+            que.push({i, 0});
+            vis.insert({i, 0});
+        }
+        
+        while(!que.empty())
+        {
+            int sz = que.size();
+            int added = 0;
+            while(sz--)
+            {
+                int i = que.front().f;
+                int j = que.front().s;
+                que.pop();
+                if(j == m-1) return m-1;
+
+                if(i-1 >= 0 && grid[i-1][j+1] > grid[i][j] && !vis.count({i-1, j+1})) 
+                {
+                    que.push({i-1, j+1});
+                    vis.insert({i-1, j+1});
+                    added = 1;
+                }
+                if(grid[i][j+1] > grid[i][j] && !vis.count({i, j+1})) 
+                {
+                    que.push({i, j+1});
+                    vis.insert({i, j+1});
+                    added = 1;
+                }
+                if(i+1 < n && grid[i+1][j+1] > grid[i][j] && !vis.count({i+1, j+1})) 
+                {
+                    que.push({i+1, j+1});
+                    vis.insert({i+1, j+1});
+                    added = 1;
+                }
+            }
+            ans += added;
+        }
+        
+        return ans;
+    }
+};
