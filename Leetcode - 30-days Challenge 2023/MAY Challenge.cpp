5cdbdd779feec175bcbbaea8589605a3d56118d9
Mukul Taneja <mkltaneja@gmail.com>
2023-05-23 21:39:16 +0530

    DAY 23

--- Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
@@ -604,3 +604,35 @@ vector<int> topKFrequent(vector<int>& nums, int k)
 
     return ans;
 }
+
+// DAY 23 703. Kth Largest Element in a Stream)============================================================================================
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+    KthLargest(int k, vector<int>& nums) 
+    {
+        this->k = k;
+        for(int x : nums)
+        {
+            pq.push(x);
+            if(pq.size() > k)
+                pq.pop();
+        }
+    }
+    
+    int add(int val) 
+    {
+        pq.push(val);
+        if(pq.size() > k)
+            pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
