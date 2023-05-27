b95a5a7b8257730d2e8c30281af30b4213ab1cbe
Mukul Taneja <mkltaneja@gmail.com>
2023-05-27 19:27:04 +0530

    DAY 27 Tabulated

--- Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/MAY Challenge.cpp
@@ -787,3 +787,21 @@ string stoneGameIII(vector<int>& stoneValue)
 
     return ans > 0? "Alice" : ans < 0? "Bob" : "Tie";
 }
+
+// METHOD 2 (Tabulated)
+
+string stoneGameIII(vector<int>& stoneValue) 
+{
+    int n = stoneValue.size();
+    vector<int> dp(n+1, 0);
+    for(int i = n-1; i >= 0; i--)
+    {
+        dp[i] = stoneValue[i] - dp[i+1];
+        if(i+2 <= n)
+            dp[i] = max(dp[i], stoneValue[i] + stoneValue[i+1] - dp[i+2]);
+        if(i+3 <= n)
+            dp[i] = max(dp[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3]);
+    }
+
+    return dp[0] > 0? "Alice" : dp[0] < 0? "Bob" : "Tie";
+}
