46519def08a6fd9230e7f586a54deec5eb1f7f12
Mukul Taneja <mkltaneja@gmail.com>
2023-06-04 01:32:22 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2023/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/JUNE Challenge.cpp
@@ -37,3 +37,52 @@ int shortestPathBinaryMatrix(vector<vector<int>>& grid)
 
   return -1;
 }
+
+// DAY 2 (2101. Detonate the Maximum Bombs)=========================================================================================================
+
+long euclidianDistance(vector<int> &bomb1, vector<int> &bomb2)
+{
+    long x1 = bomb1[0], y1 = bomb1[1];
+    long x2 = bomb2[0], y2 = bomb2[1];
+
+    long euclidianDist = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+
+    return euclidianDist;
+}
+
+void dfs(int u, vector<vector<int>> &gp, unordered_set<int> &vis)
+{
+    vis.insert(u);
+    for(int v : gp[u])
+        if(!vis.count(v))
+            dfs(v, gp, vis);
+}
+
+int maximumDetonation(vector<vector<int>>& bombs)
+{
+    int n = bombs.size();
+    vector<vector<int>> gp(n);
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            long euclidianDist = euclidianDistance(bombs[i], bombs[j]);
+            long r1square = (long)bombs[i][2]*bombs[i][2], 
+                r2square = (long)bombs[j][2]*bombs[j][2];
+            if(euclidianDist <= r1square)
+                gp[i].push_back(j);
+            if(euclidianDist <= r2square)
+                gp[j].push_back(i);
+        }
+    }
+
+    int maxBombs = 0;
+    for(int i = 0; i < n; i++)
+    {
+        unordered_set<int> vis;
+        dfs(i, gp, vis);
+        maxBombs = max(maxBombs, (int)vis.size());
+    }
+
+    return maxBombs;
+}
