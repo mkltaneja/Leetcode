9369e4259f0d075a9ee41f65c2590fc1ca888f35
Mukul Taneja <mkltaneja@gmail.com>
2023-06-28 02:47:05 +0530

    DAY 27

--- Leetcode - 30-days Challenge 2023/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/JUNE Challenge.cpp
@@ -693,3 +693,41 @@ long long totalCost(vector<int>& costs, int k, int candidates)
 
     return ans;
 }
+
+// DAY 27 (373. Find K Pairs with Smallest Sums)=================================================================================
+
+#define f first
+#define s second
+vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
+{
+    int n = nums1.size();
+    int m = nums2.size();
+    
+    vector<vector<int>> ans;
+    priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+    pq.push({nums1[0] + nums2[0], {0, 0}});
+
+    set<pair<int,int>> vis;
+
+    while(!pq.empty() && k--)
+    {
+        int x = pq.top().f;
+        int i = pq.top().s.f;
+        int j = pq.top().s.s;
+        pq.pop();
+        ans.push_back({nums1[i], nums2[j]});
+
+        if(i+1 < n && !vis.count({i+1, j}))
+        {
+            pq.push({nums1[i+1] + nums2[j], {i+1, j}});
+            vis.insert({i+1, j});
+        }
+        if(j+1 < m && !vis.count({i, j+1}))
+        {
+            pq.push({nums1[i] + nums2[j+1], {i, j+1}});
+            vis.insert({i, j+1});
+        }
+    }
+
+    return ans;
+}
