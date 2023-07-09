80fbbeb3269af7d68042c6667b957cbe6a21cef3
Mukul Taneja <mkltaneja@gmail.com>
2023-07-09 11:14:42 +0530

    Update 2770. Maximum Number of Jumps to Reach the Last Index.cpp

--- Weekly_Contest_#353/2770. Maximum Number of Jumps to Reach the Last Index.cpp
+++ Weekly_Contest_#353/2770. Maximum Number of Jumps to Reach the Last Index.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (DFS) --> Time = O(n), Space O(n)
+
 class Solution {
 public:
     
@@ -27,5 +30,24 @@ public:
         vector<int> dp(n, -1);
         int ans = dfs(0, n, gp, dp);
         return (ans < 0 || ans > n)? -1 : ans;
+    }
+};
+
+// APPROACH 2 (Tabulation) --> Time = O(n), Space = O(n) [OPTIMIZED]
+
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[0] = 0;
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < i; j++)
+                if(abs(nums[i] - nums[j]) <= target && dp[j] != -1)
+                    dp[i] = max(dp[i], dp[j] + 1);
+        
+        return dp[n-1];
     }
 };
