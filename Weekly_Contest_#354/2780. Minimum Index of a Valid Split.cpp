6b53bc6c9c18f1e8f8b633997337234c54b35085
Mukul Taneja <mkltaneja@gmail.com>
2023-07-16 11:38:06 +0530

    Update 2780. Minimum Index of a Valid Split.cpp

--- Weekly_Contest_#354/2780. Minimum Index of a Valid Split.cpp
+++ Weekly_Contest_#354/2780. Minimum Index of a Valid Split.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (Using Priority Queue and map to store dominant count at every step --> Generic Approach) --> Time = O(2*n*logn), Space = O(6*n)
+
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) 
@@ -30,5 +33,34 @@ public:
         }
         
         return ans;
+    }
+};
+
+// APPROACH 2 (Using the fact that overall one element is dominant) --> Time = O(4*n), Space = O(2*n) [OPTIMIZED]
+
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) 
+    {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int x : nums)
+            mp[x]++;
+        pair<int,int> mx = {-1,-1};
+        for(auto [x, y] : mp)
+            mx = max(mx, {y, x});
+        int dom = mx.second;
+        
+        vector<int> psum(n, 0);
+        for(int i = 0; i < n; i++)
+        {
+            psum[i] = ((nums[i] == dom)? 1 : -1) + (i? psum[i-1] : 0);
+        }
+        
+        for(int i = 0; i < n-1; i++)
+            if(psum[i] > 0 && (psum[n-1] - psum[i]) > 0)
+                return i;
+        
+        return -1;
     }
 };
