7da4ca3d2e45e7d48408991b23c2750442f27fff
Mukul Taneja <mkltaneja@gmail.com>
2023-07-25 19:27:29 +0530

    DAY 25

--- Leetcode - 30-days Challenge 2023/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/JULY Challenge.cpp
@@ -981,3 +981,24 @@ double myPow(double x, int n)
 
 	return n > 0? ans : 1.0/ans;
 }
+
+// DAY 25 (852. Peak Index in a Mountain Array)============================================================================
+
+int peakIndexInMountainArray(vector<int>& arr) 
+{
+	int n = arr.size();
+	int lo = 0, hi = n - 1;
+	while(lo <= hi)
+	{
+		int mid = lo + ((hi - lo) >> 1);
+
+		if(mid > 0 && mid < n-1 && arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
+			return mid;
+		if(mid == 0 || arr[mid] > arr[mid-1])
+			lo = mid + 1;
+		else if(mid == n-1 || arr[mid] > arr[mid+1])
+			hi = mid - 1;
+	}
+
+	return -1;
+}
