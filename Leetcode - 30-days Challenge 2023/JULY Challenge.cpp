89cc52bf1cb8b0d56a88c8ee96d24a740400b833
Mukul Taneja <mkltaneja@gmail.com>
2023-07-29 04:56:49 +0530

    DAY 28

--- Leetcode - 30-days Challenge 2023/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/JULY Challenge.cpp
@@ -1029,3 +1029,21 @@ int minSpeedOnTime(vector<int>& dist, double hour)
 
 	return hi == INT_MAX? -1 : lo;
 }
+
+// DAY 28 (486. Predict the Winner)===============================================================================================
+
+int maxDiff(int l, int r, vector<int> &nums, vector<vector<int>> &dp)
+{
+	if(l > r) return 0;
+	if(dp[l][r] != -1) return dp[l][r];
+
+	return dp[l][r] = max(nums[l] - maxDiff(l+1, r, nums, dp),
+				nums[r] - maxDiff(l, r-1, nums, dp));
+}
+
+bool PredictTheWinner(vector<int>& nums) 
+{
+	int n = nums.size();
+	vector<vector<int>> dp(n, vector<int> (n, -1));
+	return maxDiff(0, n-1, nums, dp) >= 0;
+}
