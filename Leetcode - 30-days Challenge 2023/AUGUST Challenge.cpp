de795ffd9ef8a58920efee9475bdd53794975d38
Mukul Taneja <mkltaneja@gmail.com>
2023-08-05 02:43:11 +0530

    DAY 4

--- Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
@@ -26,3 +26,57 @@ vector<string> letterCombinations(string digits)
     dfs(0, word, digits);
     return ans;
 }
+
+// DAY 4 (139. Word Break)===============================================================================================
+
+class Trie
+{
+    public:
+    vector<Trie*> child;
+    bool wordEnd;
+    Trie()
+    {
+        this->child.assign(26, nullptr);
+        this->wordEnd = false;
+    }
+
+    void insert(string &word, Trie* root)
+    {
+        Trie* curr = root;
+        for(char c : word)
+        {
+            if(!curr->child[c-'a'])
+                curr->child[c-'a'] = new Trie();
+            curr = curr->child[c-'a'];
+        }
+        curr->wordEnd = true;
+    }
+};
+Trie* root = new Trie();
+
+bool canSegment(int i, Trie* itr, string &s, vector<int> &dp)
+{
+    if(i == s.size()) return true;
+    if(dp[i] != -1) return dp[i];
+
+    Trie* curr = itr;
+    for(int j = i; j < s.size() && curr->child[s[j]-'a']; j++)
+    {
+        Trie* nxt = curr->child[s[j]-'a'];
+        if(nxt->wordEnd)
+            if(canSegment(j+1, root, s, dp))
+                return dp[j] = 1;
+        curr = nxt;
+    }
+    return dp[i] = 0;
+}
+
+bool wordBreak(string s, vector<string>& wordDict) 
+{
+    for(string &word : wordDict)
+        root->insert(word, root);
+    
+    Trie* itr = root;
+    vector<int> dp(s.size(), -1);
+    return canSegment(0, itr, s, dp);
+}
