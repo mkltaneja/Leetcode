67f2bc3fc0789fefac4c28492a0dd28cc115bdfb
Mukul Taneja <mkltaneja@gmail.com>
2023-08-07 19:25:53 +0530

    DAY 7

--- Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
@@ -163,3 +163,25 @@ int numMusicPlaylists(int n, int goal, int k)
     vector<vector<ll>> dp(n+1, vector<ll>(goal+1, -1));
     return totalPlaylists(n, goal, dp);
 }
+
+// DAY 7 (74. Search a 2D Matrix)=============================================================================================
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) 
+{
+    int n = matrix.size(), m = matrix[0].size();
+    int lo = 0, hi = n*m-1;
+    while(lo <= hi)
+    {
+        int mid = lo + ((hi - lo) >> 1);
+        int r = mid / m;
+        int c = mid % m;
+        if(matrix[r][c] == target)
+            return true;
+        if(matrix[r][c] < target)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+    }
+
+    return false;
+}
