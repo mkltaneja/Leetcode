8748d7c8dcd8c2caee29605befd4f9a3c72c4812
Mukul Taneja <mkltaneja@gmail.com>
2023-08-13 15:00:23 +0530

    Update 2815. Max Pair Sum in an Array.cpp

--- Weekly_Contest_#358/2815. Max Pair Sum in an Array.cpp
+++ Weekly_Contest_#358/2815. Max Pair Sum in an Array.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (Using max to store elements, and sort to find max 2) --> time = O(>n + n*logn)
+
 class Solution {
 public:
     int maxSum(vector<int>& nums) 
@@ -21,6 +24,40 @@ public:
             if(p.second.size() < 2) continue;
             sort(p.second.begin(), p.second.end());
             ans = max(ans, p.second.back() + p.second[p.second.size()-2]);
+        }
+        
+        return ans;
+    }
+};
+
+// APPROACH 2 (Using Array to store max 2 and taking sum along with it) --> time = O(n) --> [OPTIMIZED]
+
+class Solution {
+public:
+    int maxSum(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int ans = -1;
+        vector<vector<int>> topMax(10, vector<int> (2, 0));
+        for(int i = 0; i < n; i++)
+        {
+            int mx = 0, x = nums[i];
+            while(x)
+            {
+                mx = max(mx, x % 10);
+                x /= 10;
+            }
+            
+            if(nums[i] >= topMax[mx][0])
+            {
+                topMax[mx][1] = topMax[mx][0];
+                topMax[mx][0] = nums[i];
+            }
+            else
+                topMax[mx][1] = max(topMax[mx][1], nums[i]);
+            
+            if(topMax[mx][0] && topMax[mx][1])
+                ans = max(ans, topMax[mx][0] + topMax[mx][1]);
         }
         
         return ans;
