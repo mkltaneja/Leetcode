c7538a0954776d6fe950b7866e810f13af5d1711
Mukul Taneja <mkltaneja@gmail.com>
2023-08-17 02:05:34 +0530

    DAY 16 3 APPROACHES

--- Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
@@ -510,3 +510,27 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k)
     }
     return ans;
 }
+
+// APPROACH 3 (Using Prefix Max, and Suffix Max arrays) --> time = O(2*n), space = O(3*n)
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+{
+    int n = nums.size();
+    vector<int> ans(n-k+1), pmax(n), smax(n);
+    pmax[0] = nums[0];
+    smax[n-1] = nums[n-1];
+    for(int i = 1, j = n-2; i < n && j >= 0; i++, j--)
+    {
+        if(i % k == 0)
+            pmax[i] = nums[i];
+        else pmax[i] = max(pmax[i-1], nums[i]);
+
+        if(j % k == k-1)
+            smax[j] = nums[j];
+        else smax[j] = max(smax[j+1], nums[j]);
+    }
+
+    for(int i = 0; i < n-k+1; i++)
+        ans[i] = max(smax[i], pmax[i+k-1]);
+    return ans;
+}
