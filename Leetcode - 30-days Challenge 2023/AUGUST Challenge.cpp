fd36ed784ed2c5919eefc04b8bb571111da0d310
Mukul Taneja <mkltaneja@gmail.com>
2023-08-26 02:37:32 +0530

    DAY 25

--- Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
@@ -916,3 +916,30 @@ vector<string> fullJustify(vector<string>& words, int maxWidth)
     }
     return ans;
 }
+
+// DAY 25 (97. Interleaving String)============================================================================================================
+
+bool dfs(int i, int j, string &s1, string &s2, string &s3, vector<vector<int>> &dp)
+{
+    if(i+j == s3.size())
+        return true;
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    if(i < s1.size() && s3[i+j] == s1[i])
+        if(dfs(i+1, j, s1, s2, s3, dp))
+            return dp[i][j] = true;
+    if(j < s2.size() && s3[i+j] == s2[j])
+        if(dfs(i, j+1, s1, s2, s3, dp))
+            return dp[i][j] = true;
+    
+    return dp[i][j] = false;
+}
+
+bool isInterleave(string s1, string s2, string s3) 
+{
+    if(s3.size() != s1.size() + s2.size())
+        return false;
+    vector<vector<int>> dp(s1.size()+1, vector<int> (s2.size()+1, -1));
+    return dfs(0, 0, s1, s2, s3, dp);
+}
