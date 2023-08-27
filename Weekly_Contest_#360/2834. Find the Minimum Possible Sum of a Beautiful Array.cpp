b5825b370c070bb3734e704943cdca8815b77228
Mukul Taneja <mkltaneja@gmail.com>
2023-08-27 22:49:01 +0530

    Update 2834. Find the Minimum Possible Sum of a Beautiful Array.cpp

--- Weekly_Contest_#360/2834. Find the Minimum Possible Sum of a Beautiful Array.cpp
+++ Weekly_Contest_#360/2834. Find the Minimum Possible Sum of a Beautiful Array.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (Using set) --> time = O(2*n), space = O(n)
+
 class Solution {
 public:
     long long minimumPossibleSum(int n, int target) 
@@ -16,5 +19,17 @@ public:
             n--;
         }
         return sum;
+    }
+};
+
+// APPROACH 2 (Mathematically) --> time = O(1), space = O(1)
+
+class Solution {
+public:
+    long long minimumPossibleSum(int n, int target) 
+    {
+        int mid = (target >> 1) + 1;
+        return (1ll * n * (n+1) >> 1ll) + 
+            ((mid > n || target <= 2)? 0ll : 1ll * (target - mid) * (n - mid + 1));
     }
 };
