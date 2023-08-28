7c57bfabad03183fe0ddbe63dc6c890af941d366
Mukul Taneja <mkltaneja@gmail.com>
2023-08-28 19:29:23 +0530

    DAY 28

--- Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/AUGUST Challenge.cpp
@@ -1027,3 +1027,54 @@ bool canCross(vector<int>& stones)
         stonesMap[stones[i]] = i;
     return dfs(1, 1, stones);
 }
+
+// DAY 28 (225. Implement Stack using Queues)=========================================================================================
+
+class MyStack {
+public:
+
+    queue<int> que;
+    int size;
+    MyStack() 
+    {
+        this->size = 0;
+    }
+    
+    void push(int x) 
+    {
+        que.push(x);
+        int currSize = size++;
+        while(currSize--)
+        {
+            que.push(que.front());
+            que.pop();
+        }
+    }
+    
+    int pop() 
+    {
+        int top = que.front();
+        que.pop();
+        size--;
+        return top;
+    }
+    
+    int top() 
+    {
+        return que.front();
+    }
+    
+    bool empty() 
+    {
+        return size == 0;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
