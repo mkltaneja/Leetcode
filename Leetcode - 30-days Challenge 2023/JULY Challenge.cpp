e55055bd3e215284d03cb0a9130dba9ae14c0d26
Mukul Taneja <mkltaneja@gmail.com>
2023-09-02 00:01:03 +0530

    JULY - DAY 30

--- Leetcode - 30-days Challenge 2023/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/JULY Challenge.cpp
@@ -1080,6 +1080,26 @@ double soupServings(int n)
 	return dp[m][m];
 }
 
+// DAY 30 (664. Strange Printer)=====================================================================================================
+
+int dfs(int l, int r, string &s, vector<vector<int>> &dp)
+{
+	if(l == r) return 1;
+	if(dp[l][r] != -1) return dp[l][r];
+
+	int minPrints = r - l + 1;
+	for(int i = l; i < r; i++)
+		minPrints = min(minPrints, dfs(l, i, s, dp) + dfs(i+1, r, s, dp));
+	return dp[l][r] = minPrints - (s[l] == s[r]);
+}
+
+int strangePrinter(string s) 
+{
+	int n = s.size();
+	vector<vector<int>> dp(n, vector<int> (n, -1));
+	return dfs(0, n-1, s, dp);
+}
+
 // DAY 31 (712. Minimum ASCII Delete Sum for Two Strings)===========================================================================
 
 vector<int> remSum1, remSum2;
