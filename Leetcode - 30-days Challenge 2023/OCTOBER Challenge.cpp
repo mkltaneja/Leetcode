cd612333108073e286f94f8da8cbf7d94efb90c3
Mukul Taneja <mkltaneja@gmail.com>
2023-10-02 00:43:32 +0530

    OCTOBER - DAY 1

---
+++ Leetcode - 30-days Challenge 2023/OCTOBER Challenge.cpp
@@ -0,0 +1,19 @@
+
+// DAY 1 (557. Reverse Words in a String III)=========================================================================
+
+string reverseWords(string s) 
+{
+    string ans = "", prev = "";
+    for(int i = 0; i <= s.size(); i++)
+    {
+        if(i == s.size() || s[i] == ' ')
+        {
+            reverse(prev.begin(), prev.end());
+            ans += prev + (i != s.size()? " " : "");
+            prev = "";
+        }
+        else prev += s[i];
+    }
+
+    return ans;
+}
