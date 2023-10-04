4d8945d4eb2784e3b7d197d53fd11f45b4be197e
Mukul Taneja <mkltaneja@gmail.com>
2023-10-04 22:53:53 +0530

    DAY 4

--- Leetcode - 30-days Challenge 2023/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/OCTOBER Challenge.cpp
@@ -59,3 +59,39 @@ int numIdenticalPairs(vector<int>& nums)
 
     return ans;
 }
+
+// DAY 4 (706. Design HashMap)=============================================================================================
+
+class MyHashMap {
+public:
+
+    vector<int> map;
+    int size = 1e6+1;
+    MyHashMap() 
+    {
+        map.assign(size, -1);
+    }
+    
+    void put(int key, int value) 
+    {
+        map[key] = value;
+    }
+    
+    int get(int key) 
+    {
+        return map[key];
+    }
+    
+    void remove(int key) 
+    {
+        map[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
