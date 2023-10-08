b28de70f5cf1bae89f4f9f33d519a3e7b5f9aa29
Mukul Taneja <mkltaneja@gmail.com>
2023-10-08 20:46:13 +0530

    Update 2895. Minimum Processing Time.cpp

--- Weekly_Contest_#366/2895. Minimum Processing Time.cpp
+++ Weekly_Contest_#366/2895. Minimum Processing Time.cpp
@@ -1,3 +1,6 @@
+
+// METHOD 1 (Comparing every element) --> Time = O(n)
+
 class Solution {
 public:
     int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) 
@@ -13,6 +16,25 @@ public:
             for(int j = i*4; j < i*4+4; j++)
                 ans = max(ans, processorTime[i] + tasks[j]);
         }
+        
+        return ans;
+    }
+};
+
+// METHOD 2 (Comparing only necessary elements) --> Time = O(m) [OPTIMIZED]
+
+class Solution {
+public:
+    int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) 
+    {
+        int n = tasks.size();
+        int m = processorTime.size();
+        sort(tasks.rbegin(), tasks.rend());
+        sort(processorTime.begin(), processorTime.end());
+        
+        int ans = 0;
+        for(int i = 0, j = 0; i < m; i++, j += 4)
+            ans = max(ans, processorTime[i] + tasks[j]);
         
         return ans;
     }
