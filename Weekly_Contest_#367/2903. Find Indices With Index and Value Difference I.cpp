fe4901f3359180d269a5bc1205470d4f3e38836b
Mukul Taneja <mkltaneja@gmail.com>
2023-10-15 14:53:56 +0530

    Update 2903. Find Indices With Index and Value Difference I.cpp

--- Weekly_Contest_#367/2903. Find Indices With Index and Value Difference I.cpp
+++ Weekly_Contest_#367/2903. Find Indices With Index and Value Difference I.cpp
@@ -27,3 +27,27 @@ public:
         return {-1, -1};
     }
 };
+
+// APPROACH 2 (By Storing Maximum and minimum till i - id indexed numbers) --> Time = O(n), Space = O(1) [OPTIMIZED]
+
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int id, int vd) 
+    {
+        int n = nums.size();
+        int mni = 0, mxi = 0;
+        for(int i = 0, j = i + id; j < n; i++, j++)
+        {
+            if(nums[i] < nums[mni])
+                mni = i;
+            if(nums[i] > nums[mxi])
+                mxi = i;
+            if(abs(nums[j] - nums[mni]) >= vd)
+                return {mni, j};
+            if(abs(nums[j] - nums[mxi]) >= vd)
+                return {mxi, j};
+        }
+        
+        return {-1, -1};
+    }
+};
