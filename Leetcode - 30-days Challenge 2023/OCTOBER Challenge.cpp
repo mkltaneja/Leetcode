e884c3ca46699632a1827b97786b9cb374496875
Mukul Taneja <mkltaneja@gmail.com>
2023-10-16 00:57:48 +0530

    DAY 15

--- Leetcode - 30-days Challenge 2023/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/OCTOBER Challenge.cpp
@@ -524,3 +524,24 @@ int paintWalls(vector<int>& cost, vector<int>& time)
     memset(cache, -1, sizeof(cache));
     return dfs(0, time.size(), cost, time);
 }
+
+// DAY 15 (1269. Number of Ways to Stay in the Same Place After Some Steps)===============================================================================
+
+int mod = 1e9 + 7;
+int dfs(int i, int steps, int n, vector<vector<int>> &dp)
+{
+    if(i == -1 || i == n) return 0;
+    if(steps == 0) return i == 0;
+    if(dp[i][steps] != -1) return dp[i][steps];
+
+    return dp[i][steps] = 
+        ((dfs(i+1, steps-1, n, dp) % mod + 
+        dfs(i, steps-1, n, dp) % mod) % mod + 
+        dfs(i-1, steps-1, n, dp) % mod) % mod;
+}
+
+int numWays(int steps, int arrLen)
+{
+    vector<vector<int>> dp(steps+1, vector<int>(steps+1, -1));
+    return dfs(0, steps, arrLen, dp);
+}
