11531c6e4e2eca53a98beba1ebd4c11687b4d882
Mukul Taneja <mkltaneja@gmail.com>
2023-11-05 16:27:55 +0530

    Update 2923. Find Champion I.cpp

--- Weekly_Contest_#370/2923. Find Champion I.cpp
+++ Weekly_Contest_#370/2923. Find Champion I.cpp
@@ -1,3 +1,6 @@
+
+// APPROACH 1 (By Counting indegree) --> Time = O(n + n^2), Space = O(n)
+
 class Solution {
 public:
     int findChampion(vector<vector<int>>& grid) 
@@ -12,5 +15,20 @@ public:
             if(inDeg[i] == 0)
                 return i;
         return -1;
+    }
+};
+
+// APPROACH 2 (Finding answer linearly) --> Time = O(n), Space = O(1)
+
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int ans = 0;
+        for(int i = 1; i < n; i++)
+            if(grid[ans][i] == 0)
+                ans = i;
+        return ans;
     }
 };
