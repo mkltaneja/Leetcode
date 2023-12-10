57dd42c5d61a0bba97683610b3241695af38c230
Mukul Taneja <mkltaneja@gmail.com>
2023-12-10 10:07:18 +0530

    Create 2961. Double Modular Exponentiation.cpp

---
+++ Weekly_Contest_#375/2961. Double Modular Exponentiation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    int pow(int a, int b, int m)
+    {
+        int ans = 1;
+        while(b)
+        {
+            if(b & 1)
+                ans = (ans * a) % m;
+            a = (a * a) % m;
+            b >>= 1;
+        }
+        return ans;
+    }
+    
+    vector<int> getGoodIndices(vector<vector<int>>& variables, int target) 
+    {
+        vector<int> ans;
+        int i = 0;
+        for(vector<int> &v : variables)
+        {
+            int a = v[0], b = v[1], c = v[2], m = v[3];
+            
+            int aa = pow(a, b, 10);
+            aa = pow(aa, c, m);
+            
+            if(aa == target)
+                ans.push_back(i);
+            i++;
+        }
+        return ans;
+    }
+};
