6f2358a9f019cd1107cee2d13f1b2349c178d2c7
Mukul Taneja <mkltaneja@gmail.com>
2023-12-10 17:40:16 +0530

    DAY 10

--- Leetcode - 30-days Challenge 2023/DECEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2023/DECEMBER Challenge.cpp
@@ -175,3 +175,15 @@ vector<int> inorderTraversal(TreeNode* root)
     }
     return ans;
 }
+
+// DAY 10 (867. Transpose Matrix)=============================================================================
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) 
+{
+    int n = matrix.size(), m = matrix[0].size();
+    vector<vector<int>> ansMatrix(m, vector<int>(n));
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            ansMatrix[j][i] = matrix[i][j];
+    return ansMatrix;
+}
