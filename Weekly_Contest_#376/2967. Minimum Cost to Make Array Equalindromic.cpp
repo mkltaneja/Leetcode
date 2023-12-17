f0c8eccd211b2ed58adfd16cc4843a0b418488ea
Mukul Taneja <mkltaneja@gmail.com>
2023-12-17 16:22:54 +0530

    Update 2967. Minimum Cost to Make Array Equalindromic.cpp

--- Weekly_Contest_#376/2967. Minimum Cost to Make Array Equalindromic.cpp
+++ Weekly_Contest_#376/2967. Minimum Cost to Make Array Equalindromic.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Constructing every possible pallindromic number) -> Time = O(n + 1e5), Space = O(n + 1e5)
+
 class Solution {
 public:
     
@@ -66,5 +68,80 @@ public:
         }
         
         return minCost;
+    }
+};
+
+
+// APPROACH 2 -> (Greedy) --> Time = O(nlogn + n), Space = O(n)
+
+class Solution {
+public:
+    
+    #define ll long long
+    
+    string findSmallerPal(string s)
+    {
+        string t = s;
+        int n = s.size();
+        for(int i = 0, j = n-1; i < j; t[j--] = t[i++]);
+        
+        if(t <= s) return t;
+        
+        for(int i = ((n - 1) >> 1); i >= 0; i--)
+        {
+            if(t[i] == '0') continue;
+            if(i == 0 && t[i] == '1') continue;
+            
+            t[n-i-1] = --t[i];
+            while(++i <= n-i-1)
+                t[n-i-1] = t[i] = '9';
+            return t;
+        }
+        
+        return string(n-1, '9');
+    }
+    
+    string findGreaterPal(string s)
+    {
+        string t = s;
+        int n = s.size();
+        for(int i = 0, j = n-1; i < j; t[j--] = t[i++]);
+        
+        if(t >= s) return t;
+        
+        for(int i = ((n - 1) >> 1); i >= 0; i--)
+        {
+            if(t[i] == '9') continue;
+            
+            t[n-i-1] = ++t[i];
+            while(++i <= n-i-1)
+                t[n-i-1] = t[i] = '0';
+            return t;
+        }
+        
+        string ret = string(n+1, '0');
+        ret[0] = ret[n] = '1';
+        return ret;
+    }
+    
+    ll findCost(ll pal, vector<int> &nums)
+    {
+        ll cost = 0;
+        for(int num : nums)
+            cost += abs(pal - num);
+        return cost;
+    }
+    
+    long long minimumCost(vector<int>& nums) 
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        string sPal = findSmallerPal(to_string(nums[n >> 1]));
+        string gPal = findGreaterPal(to_string(nums[n >> 1]));
+        ll smallerCost = findCost(stoll(sPal), nums);
+        ll greaterCost = findCost(stoll(gPal), nums);
+        
+        return min(smallerCost, greaterCost);
     }
 };
