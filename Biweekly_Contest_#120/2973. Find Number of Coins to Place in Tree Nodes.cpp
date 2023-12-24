e3317cd231cdbc4cc8fad6b73805466a2aae8f09
Mukul Taneja <mkltaneja@gmail.com>
2023-12-24 21:19:35 +0530

    Update 2973. Find Number of Coins to Place in Tree Nodes.cpp

--- Biweekly_Contest_#120/2973. Find Number of Coins to Place in Tree Nodes.cpp
+++ Biweekly_Contest_#120/2973. Find Number of Coins to Place in Tree Nodes.cpp
@@ -1,5 +1,6 @@
 
-// Time = O(n^3), Space = O(n^2)
+// APPROACH 1 (Return type: 2D vector: handling positive and negative values separately)
+// Time = O(n^3 * logn), Space = O(n^2)
 
 class Solution {
 public:
@@ -70,6 +71,71 @@ public:
     vector<long long> placedCoins(vector<vector<int>>& edges, vector<int>& cost) 
     {
         int n = cost.size();
+        vector<ll> coin(n, -1);
+        vector<vector<int>> gp(n);
+        for(auto &e : edges)
+        {
+            gp[e[0]].push_back(e[1]);
+            gp[e[1]].push_back(e[0]);
+        }
+        
+        dfs(0, -1, gp, cost, coin);
+        
+        return coin;
+    }
+};
+
+// APPROACH 2 (Return type: void: handling positive and negative values at the same time)
+// Time = O(n^3 * logn), Space = O(n^2)
+
+class Solution {
+public:
+    
+    #define ll long long
+    vector<ll> subSize;
+    vector<vector<ll>> costs;
+    
+    void dfs(int u, int p, vector<vector<int>> &gp, vector<int> &cost, vector<ll> &coin)
+    {
+        ll csize = 1;
+        vector<ll> pos, neg;
+        vector<ll> currCosts;
+        currCosts.push_back(cost[u]);
+        
+        for(int v : gp[u])
+        {
+            if(v == p) continue;
+            dfs(v, u, gp, cost, coin);
+            
+            csize += subSize[v];
+            int size = costs[v].size();
+            if(size > 3)
+                currCosts.push_back(costs[v][0]);
+            if(size > 4)
+                currCosts.push_back(costs[v][1]);
+            for(int i = size-1; i >= max(size-3, 0); i--)
+                currCosts.push_back(costs[v][i]);
+        }
+        subSize[u] = csize;
+        
+        sort(currCosts.begin(), currCosts.end());
+        if(csize <= 2)
+            coin[u] = 1;
+        else 
+        {
+            int n = currCosts.size();
+            coin[u] = max({0ll, currCosts[n-1] * currCosts[n-2] * currCosts[n-3], currCosts[0] * currCosts[1] * currCosts[n-1]});
+        }
+        costs[u] = currCosts;
+        
+        return;
+    }
+    
+    vector<long long> placedCoins(vector<vector<int>>& edges, vector<int>& cost) 
+    {
+        int n = cost.size();
+        subSize.resize(n);
+        costs.resize(n);
         vector<ll> coin(n, -1);
         vector<vector<int>> gp(n);
         for(auto &e : edges)
