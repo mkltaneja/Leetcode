cb569c271c444d6469d262f2f9937a660d160013
Mukul Taneja <mkltaneja@gmail.com>
2023-12-31 00:26:22 +0530

    Update 1054. Distant Barcodes.cpp - OPTIMIZED

--- 1054. Distant Barcodes.cpp
+++ 1054. Distant Barcodes.cpp
@@ -42,3 +42,37 @@ public:
         return ans;
     }
 };
+
+
+// APPROACH 2 (Without Priority queue, with sorted count array)
+
+// Time = O(n*logn), Space = O(n)
+
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) 
+    {
+        int size = barcodes.size();
+        vector<int> ans(size);
+        unordered_map<int,int> freqMap;
+        vector<pair<int,int>> freqArr;
+        for(int barcode : barcodes)
+            freqMap[barcode]++;
+        for(auto pair : freqMap)
+            freqArr.push_back({pair.second, pair.first});
+        sort(freqArr.rbegin(), freqArr.rend());
+
+        int itr1 = 0, itr2 = 0;
+        while(itr1 < freqArr.size())
+        {
+            while(freqArr[itr1].first--)
+            {
+                ans[itr2] = freqArr[itr1].second;
+                itr2 = itr2 + 2 < size? itr2 + 2 : 1;
+            }
+            itr1++;
+        }
+
+        return ans;
+    }
+};
