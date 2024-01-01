48329cff2f7bd2c4ade156efe0cf2221e13b4ab7
Mukul Taneja <mkltaneja@gmail.com>
2024-01-02 01:50:20 +0530

    Update JANUARY Challenge.cpp

--- Leetcode - 30-days Challenge 2024/JANUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/JANUARY Challenge.cpp
@@ -1,7 +1,9 @@
 
 // DAY 1 (455. Assign Cookies)===========================================================================================
 
-// Time Complexity = O(n*logn)
+// APPROACH 1 (Using Map)
+
+// Time Complexity = O(n + m*logn)
 // Space Complexity = O(n)
 
 int findContentChildren(vector<int>& g, vector<int>& s) 
@@ -20,6 +22,29 @@ int findContentChildren(vector<int>& g, vector<int>& s)
         if(--(*justSmallerGreed).second == 0)
             gMap.erase(justSmallerGreed);
         satisfiedCount++;
+    }
+    return satisfiedCount;
+}
+
+// APPROACH 2 (2 pointer approach)
+
+// Time Complexity = O(max(m*logn, n*logn))
+// Space Complexity = O(1)
+
+int findContentChildren(vector<int>& g, vector<int>& s)
+{
+    int gSize = g.size(), sSize = s.size();
+    sort(g.rbegin(), g.rend());
+    sort(s.rbegin(), s.rend());
+    int satisfiedCount = 0;
+    
+    for(int p1 = 0, p2 = 0; p1 < gSize && p2 < sSize; p1++)
+    {
+        if(g[p1] <= s[p2])
+        {
+            satisfiedCount++;
+            p2++;
+        }
     }
     return satisfiedCount;
 }
