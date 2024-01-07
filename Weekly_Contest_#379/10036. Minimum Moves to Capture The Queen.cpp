b0eea2b56c8a1a2300fda5c54e54515eebe9cae7
Mukul Taneja <mkltaneja@gmail.com>
2024-01-07 23:43:52 +0530

    Update 10036. Minimum Moves to Capture The Queen.cpp

--- Weekly_Contest_#379/10036. Minimum Moves to Capture The Queen.cpp
+++ Weekly_Contest_#379/10036. Minimum Moves to Capture The Queen.cpp
@@ -1,3 +1,9 @@
+
+// APPROACH 1 (Checking via loops)
+
+// Time = O(8*8)
+// Space = O(1)
+
 class Solution {
 public:
     
@@ -45,6 +51,32 @@ public:
         if(tc == e && td == f)
             return 1;
         
+        return 2;
+    }
+};
+
+
+// APPROACH 2 (Mathematically)
+
+// Time = O(1)
+// Space = O(1)
+
+class Solution {
+public:
+    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) 
+    {
+        if(a == c && a == e && abs(b - f) == abs(b - d) + abs(d - f))
+            return 2;
+        if(b == d && b == f && abs(a - e) == abs(a - c) + abs(c - e))
+            return 2;
+        if(abs(c - a) == abs(d - b) && abs(c - e) == abs(d - f) && abs(a - e) == abs(b - f) && abs(c - e) == abs(c - a) + abs(a - e))
+            return 2;
+
+        if(a == e || b == f)
+            return 1;
+        if(abs(c - e) == abs(d - f))
+            return 1;
+
         return 2;
     }
 };
