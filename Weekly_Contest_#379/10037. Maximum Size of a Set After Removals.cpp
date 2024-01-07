765cc8f2b79146b224e31d41eb96bf15b68be1f9
Mukul Taneja <mkltaneja@gmail.com>
2024-01-07 23:46:09 +0530

    Update 10037. Maximum Size of a Set After Removals.cpp

--- Weekly_Contest_#379/10037. Maximum Size of a Set After Removals.cpp
+++ Weekly_Contest_#379/10037. Maximum Size of a Set After Removals.cpp
@@ -70,3 +70,36 @@ public:
         return st.size();
     }
 };
+
+// APPROACH 2 (Finding uniques and commons mathematically)
+
+// Time = O(n)
+// Space = O(n)
+
+class Solution {
+public:
+
+    int maximumSetSize(vector<int>& nums1, vector<int>& nums2) 
+    {
+        unordered_set<int> st1, st2;
+        int n = nums1.size(), m = (n >> 1);
+        for(int x : nums1)
+            st1.insert(x);
+        for(int x : nums2)
+            st2.insert(x);
+        int uni1 = 0, uni2 = 0;
+        for(int x : st1)
+            if(!st2.count(x))
+                uni1++;
+        for(int x : st2)
+            if(!st1.count(x))
+                uni2++;
+        int common = st1.size() - uni1;
+        uni1 = min(uni1, m);
+        uni2 = min(uni2, m);
+        
+        int ans = uni1 + uni2 + min(n - (uni1 + uni2), common);
+        
+        return ans;
+    }
+};
