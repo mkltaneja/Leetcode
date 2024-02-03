f9f964b20b79f766cbd6c8230830f5f0070a3bdb
Mukul Taneja <mkltaneja@gmail.com>
2024-02-04 02:08:25 +0530

    Create 3024. Type of Triangle II.cpp

---
+++ Biweekly_Contest_#123/3024. Type of Triangle II.cpp
@@ -0,0 +1,18 @@
+
+// Time = O(1)
+// Space = O(1)
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        if(nums[0] + nums[1] <= nums[2])
+            return "none";
+        if(nums[0] == nums[1] && nums[0] == nums[2])
+            return "equilateral";
+        if(nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2])
+            return "isosceles";
+        return "scalene";
+    }
+};
