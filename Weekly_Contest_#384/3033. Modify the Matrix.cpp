1da7f6ba6086e10b6c5e3670d0372f8ae3b34357
Mukul Taneja <mkltaneja@gmail.com>
2024-02-11 12:04:12 +0530

    Update 3033. Modify the Matrix.cpp

--- Weekly_Contest_#384/3033. Modify the Matrix.cpp
+++ Weekly_Contest_#384/3033. Modify the Matrix.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Finding max with space)
+
 // Time Complexity = O(2 * n * m)
 // Space Complexity = O(m)
 
@@ -16,6 +18,30 @@ public:
             for(int j = 0; j < m; j++)
                 if(matrix[i][j] == -1)
                     matrix[i][j] = mx[j];
+        return matrix;
+    }
+};
+
+// APPROACH 2 (Finding max without extra space)
+
+// Time Complexity = O(2 * n * m)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) 
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int j = 0; j < m; j++)
+        {
+            int mx = -1;
+            for(int i = 0; i < n; i++)
+                mx = max(mx, matrix[i][j]);
+            for(int i = 0; i < n; i++)
+                if(matrix[i][j] == -1)
+                    matrix[i][j] = mx;
+        }
         return matrix;
     }
 };
