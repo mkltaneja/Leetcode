08e6879b262425a428af769beaa369f4a9862b8f
Mukul Taneja <mkltaneja@gmail.com>
2024-02-18 17:52:11 +0530

    Update 3043. Find the Length of the Longest Common Prefix.cpp

--- Weekly_Contest_#385/3043. Find the Length of the Longest Common Prefix.cpp
+++ Weekly_Contest_#385/3043. Find the Length of the Longest Common Prefix.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Implementing with String)
+
 // Time Complexity = O(n*m)
 // Space Complexity = O(n*m)
 
@@ -33,5 +35,39 @@ public:
         }
         
         return ans;
+    }
+};
+
+
+// APPROACH 2 (Implementing with integer only) [OPTIMIZED]
+
+// Time Complexity = O(n*m)
+// Space Complexity = O(n)
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) 
+    {
+        int n = arr1.size(), m = arr2.size();
+        unordered_set<int> st;
+        
+        for(int x : arr1)
+            for(; x; x /= 10)
+                st.insert(x);
+        
+        int ans = 0;
+        for(int x : arr2)
+        {
+            for(; x > ans; x /= 10)
+            {
+                if(st.count(x))
+                {
+                    ans = x;
+                    break;
+                }
+            }
+        }
+        
+        return ans? to_string(ans).size() : 0;
     }
 };
