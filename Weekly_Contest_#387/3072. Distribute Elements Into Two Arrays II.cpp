fcd24e542de18f524a0dd67013bf597974b44b4f
Mukul Taneja <mkltaneja@gmail.com>
2024-03-03 13:33:42 +0530

    Update 3072. Distribute Elements Into Two Arrays II.cpp

--- Weekly_Contest_#387/3072. Distribute Elements Into Two Arrays II.cpp
+++ Weekly_Contest_#387/3072. Distribute Elements Into Two Arrays II.cpp
@@ -1,4 +1,6 @@
 
+// APPROACH 1 (Using Fenwick Tree with Numbers)
+
 // Time Complexity = O(n * log(mx) + n)
 // Space Complexity = O(mx + 2*n)
 
@@ -73,6 +75,100 @@ public:
             {
                 arr1.push_back(nums[i]);
                 ft1.add(nums[i]);
+            }
+        }
+        
+        res.insert(res.end(), arr1.begin(), arr1.end());
+        res.insert(res.end(), arr2.begin(), arr2.end());
+        
+        return res;
+    }
+};
+
+
+// APPROACH 2 (Using Fenwick Tree with Indices) **[EFFECIENT WAY TO USE FENWICK TREE WHEN WE WANT TO FIND BIGGER NUMBERS IN THE STREAM/ARRAY]**
+
+// Time Complexity = O(3*n*logn + 3*n)
+// Space Complexity = O(4*n)
+
+class Solution {
+public:
+
+    class Ftree
+    {
+        int n;
+        vector<int> fMap;
+        public:
+        Ftree(int n)
+        {
+            this->n = n;
+            this->fMap.assign(n, 0);
+        }
+        
+        int get(int x)
+        {
+            int ans = 0;
+            while(x < n)
+            {
+                ans += fMap[x];
+                x += x & -x;
+            }
+            return ans;
+        }
+        void add(int x)
+        {
+            while(x)
+            {
+                fMap[x]++;
+                x -= x & -x;
+            }
+        }
+    };
+    
+    vector<int> resultArray(vector<int>& nums) 
+    {
+        int n = nums.size();
+        Ftree ft1(n+1);
+        Ftree ft2(n+1);
+        vector<pair<int,int>> sortPos(n);
+        
+        for(int i = 0; i < n; i++)
+            sortPos[i] = {nums[i], i+1};
+        sort(sortPos.begin(), sortPos.end());
+        for(int i = 0; i < n; i++)
+            sortPos[i] = {sortPos[i].second, i+1};
+        sort(sortPos.begin(), sortPos.end());
+
+        vector<int> arr1, arr2, res;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        ft1.add(sortPos[0].second);
+        ft2.add(sortPos[1].second);
+        
+        for(int i = 2; i < n; i++)
+        {
+            int g1 = ft1.get(sortPos[i].second);
+            int g2 = ft2.get(sortPos[i].second);
+            
+            if(g1 > g2)
+            {
+                arr1.push_back(nums[i]);
+                ft1.add(sortPos[i].second);
+            }
+            else if(g1 < g2)
+            {
+                arr2.push_back(nums[i]);
+                ft2.add(sortPos[i].second);
+            }
+            else if(arr2.size() < arr1.size())
+            {
+                arr2.push_back(nums[i]);
+                ft2.add(sortPos[i].second);
+            }
+            else 
+            {
+                arr1.push_back(nums[i]);
+                ft1.add(sortPos[i].second);
             }
         }
         
