808cae58a3ac5a08736714814e4704e6507449d1
Mukul Taneja <mkltaneja@gmail.com>
2024-04-03 00:29:29 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2024/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/APRIL Challenge.cpp
@@ -18,3 +18,24 @@ int lengthOfLastWord(string s)
     }
     return len;
 }
+
+// DAY 2 (205. Isomorphic Strings)====================================================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(256)
+
+bool isIsomorphic(string s, string t) 
+{
+    unordered_map<char,char> sCharMap, tCharMap;
+    for(int idx = 0; idx < s.size(); idx++)
+    {
+        char a = s[idx], b = t[idx];
+        if(sCharMap[a] && sCharMap[a] != b)
+            return false;
+        if(!sCharMap[a] && tCharMap[b] && tCharMap[b] != a)
+            return false;
+        sCharMap[a] = b;
+        tCharMap[b] = a;
+    }
+    return true;
+}
