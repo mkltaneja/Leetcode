cfa4c6cbb283c90486fd99b3b87f8bb8dbfcdb27
Mukul Taneja <mkltaneja@gmail.com>
2024-04-04 02:23:09 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2024/APRIL Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/APRIL Challenge.cpp
@@ -39,3 +39,36 @@ bool isIsomorphic(string s, string t)
     }
     return true;
 }
+
+// DAY 3 (79. Word Search)=====================================================================================================
+
+// Time Complexity = O(rows * cols * 4^wordSize)
+// Space Complexity = O(wordSize) [Stack Space]
+
+bool isWordPresent(int row, int col, int idx, int rows, int cols, int wordSize, string &word, vector<vector<char>> &board)
+{
+    if(idx == wordSize)
+        return true;
+    if(row == -1 || col == -1 || row == rows || col == cols || word[idx] != board[row][col])
+        return false;
+
+    char currChar = board[row][col];
+    board[row][col] = '#';
+    bool ans = isWordPresent(row+1, col, idx+1, rows, cols, wordSize, word, board) ||
+        isWordPresent(row, col+1, idx+1, rows, cols, wordSize, word, board) ||
+        isWordPresent(row-1, col, idx+1, rows, cols, wordSize, word, board) ||
+        isWordPresent(row, col-1, idx+1, rows, cols, wordSize, word, board);
+    board[row][col] = currChar;
+
+    return ans;
+}
+
+bool exist(vector<vector<char>>& board, string word) 
+{
+    int rows = board.size(), cols = board[0].size(), wordSize = word.size();
+    for(int row = 0; row < rows; row++)
+        for(int col = 0; col < cols; col++)
+            if(isWordPresent(row, col, 0, rows, cols, wordSize, word, board))
+                return true;
+    return false;
+}
