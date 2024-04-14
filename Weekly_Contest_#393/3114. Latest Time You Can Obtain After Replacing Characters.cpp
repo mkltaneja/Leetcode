b1f2bd7c719e89394661eae68f007a8337443f1b
Mukul Taneja <mkltaneja@gmail.com>
2024-04-15 01:53:14 +0530

    Create 3114. Latest Time You Can Obtain After Replacing Characters.cpp

---
+++ Weekly_Contest_#393/3114. Latest Time You Can Obtain After Replacing Characters.cpp
@@ -0,0 +1,19 @@
+
+// Time Complexity = O(1)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    string findLatestTime(string s) 
+    {
+        if(s[0] == '?')
+            s[0] = (s[1] == '0' || s[1] == '1' || s[1] == '?')? '1' : '0';
+        if(s[1] == '?')
+            s[1] = s[0] == '0'? '9' : '1';
+        if(s[3] == '?')
+            s[3] = '5';
+        if(s[4] == '?')
+            s[4] = '9';
+        return s;
+    }
+};
