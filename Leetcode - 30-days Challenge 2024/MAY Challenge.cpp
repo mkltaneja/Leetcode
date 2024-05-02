968d8cc93b051fb7d6420dab751b128965d7ca02
Mukul Taneja <mkltaneja@gmail.com>
2024-05-03 00:31:04 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2024/MAY Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/MAY Challenge.cpp
@@ -21,3 +21,21 @@ string reversePrefix(string word, char ch)
 
     return ans;
 }
+
+// DAY 2 (2441. Largest Positive Integer That Exists With Its Negative)===================================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
+int findMaxK(vector<int>& nums) 
+{
+    unordered_set<int> numSet;
+    int ans = -1;
+    for(int num : nums)
+    {
+        if(numSet.count(-num))
+            ans = max(ans, abs(num));
+        numSet.insert(num);
+    }
+    return ans;
+}
