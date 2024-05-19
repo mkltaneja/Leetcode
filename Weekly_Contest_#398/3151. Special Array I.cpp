db6e1e178280c8211d11d4f68d0c1f91ba455ead
Mukul Taneja <mkltaneja@gmail.com>
2024-05-19 21:44:39 +0530

    Create 3151. Special Array I.cpp

---
+++ Weekly_Contest_#398/3151. Special Array I.cpp
@@ -0,0 +1,14 @@
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) 
+    {
+        for(int i = 0; i < nums.size()-1; i++)
+            if((nums[i]&1) == (nums[i+1]&1))
+                return false;
+        return true;
+    }
+};
