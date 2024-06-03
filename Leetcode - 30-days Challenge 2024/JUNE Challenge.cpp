fa5dce3f0e67e614546d5f7c4b67867ed782a51f
Mukul Taneja <mkltaneja@gmail.com>
2024-06-03 20:18:40 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2024/JUNE Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/JUNE Challenge.cpp
@@ -23,3 +23,21 @@ void reverseString(vector<char>& s)
     while(start < end)
         swap(s[start++], s[end--]);
 }
+
+// DAY 3 (2486. Append Characters to String to Make Subsequence)====================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+int appendCharacters(string s, string t)
+{
+    int sSize = s.size(), tSize = t.size();
+    int idx1 = 0, idx2 = 0;
+    while(idx1 < sSize && idx2 < tSize)
+    {
+        if(s[idx1] == t[idx2])
+            idx2++;
+        idx1++;
+    }
+    return tSize - idx2;
+}
