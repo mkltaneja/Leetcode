cfc827529c65edcb001a9219a640e83a4845086b
Mukul Taneja <mkltaneja@gmail.com>
2024-07-04 01:28:03 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2024/JULY Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/JULY Challenge.cpp
@@ -29,3 +29,61 @@ vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
             ans.push_back(num);
     return ans;
 }
+
+// DAY 3 (1509. Minimum Difference Between Largest and Smallest Value in Three Moves)===========================================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+int minDifference(vector<int>& nums)
+{
+    if(nums.size() <= 4)
+        return 0;
+    int min1 = INT_MAX, min2 = INT_MAX, min3 = INT_MAX, min4 = INT_MAX;
+    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN, max4 = INT_MIN;
+    for(int num : nums)
+    {
+        if(num >= max1)
+        {
+            max4 = max3;
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+        }
+        else if(num >= max2)
+        {
+            max4 = max3;
+            max3 = max2;
+            max2 = num;
+        }
+        else if(num >= max3)
+        {
+            max4 = max3;
+            max3 = num;
+        }
+        else if(num >= max4)
+            max4 = num;
+        
+        if(num <= min1)
+        {
+            min4 = min3;
+            min3 = min2;
+            min2 = min1;
+            min1 = num;
+        }
+        else if(num <= min2)
+        {
+            min4 = min3;
+            min3 = min2;
+            min2 = num;
+        }
+        else if(num <= min3)
+        {
+            min4 = min3;
+            min3 = num;
+        }
+        else if(num <= min4)
+            min4 = num;
+    }
+    return min({max1 - min4, max2 - min3, max3 - min2, max4 - min1});
+}
