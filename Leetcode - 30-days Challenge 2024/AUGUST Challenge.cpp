90245aef3aa7dea3e96a3007b1b94e2ae5f2c265
Mukul Taneja <mkltaneja@gmail.com>
2024-08-03 01:05:45 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2024/AUGUST Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/AUGUST Challenge.cpp
@@ -11,3 +11,30 @@ int countSeniors(vector<string>& details)
     
     return totalPassengers;
 }
+
+// DAY 2 (2134. Minimum Swaps to Group All 1's Together II)========================================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+int minSwaps(vector<int>& nums)
+{
+    int size = nums.size();
+    int totalOnes = 0;
+    for(int num : nums)
+        totalOnes += num;
+    
+    int currOnes = 0;
+    for(int idx = 0; idx < totalOnes; idx++)
+        currOnes += nums[idx];
+    
+    int minSwaps = totalOnes - currOnes;
+    for(int idx = 0; idx < size; idx++)
+    {
+        currOnes += nums[(idx + totalOnes) % size];
+        currOnes -= nums[idx];
+        minSwaps = min(minSwaps, totalOnes - currOnes);
+    }
+
+    return minSwaps;
+}
