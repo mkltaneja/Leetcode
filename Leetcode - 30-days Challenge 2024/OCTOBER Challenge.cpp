ae171629fb40ac2df8c6e9e030c811ac85dd3bba
Mukul Taneja <mkltaneja@gmail.com>
2024-10-02 23:57:48 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2024/OCTOBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/OCTOBER Challenge.cpp
@@ -15,3 +15,24 @@ bool canArrange(vector<int>& arr, int k)
             return false;
     return true;
 }
+
+// DAY 2 (1331. Rank Transform of an Array)===========================================================================
+
+// Time Complexity = O(n*logn)
+// Space Complexity = O(n)
+
+vector<int> arrayRankTransform(vector<int>& arr)
+{
+    int size = arr.size();
+    vector<pair<int,int>> arrIdx(size);
+    vector<int> rank(size);
+    int currRank = 1;
+    for(int idx = 0; idx < size; idx++)
+        arrIdx[idx] = {arr[idx], idx};
+    sort(arrIdx.begin(), arrIdx.end());
+
+    for(int idx = 0; idx < size; idx++)
+        rank[arrIdx[idx].second] = idx && arrIdx[idx].first == arrIdx[idx-1].first? rank[arrIdx[idx-1].second] : currRank++;
+    
+    return rank;
+}
