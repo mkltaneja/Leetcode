b2d8d09a65c11c1ecb6f637e0a74192e0c19bc49
Mukul Taneja <mkltaneja@gmail.com>
2024-11-04 00:05:18 +0530

    DAY 3

--- Leetcode - 30-days Challenge 2024/NOVEMBER Challenge.cpp
+++ Leetcode - 30-days Challenge 2024/NOVEMBER Challenge.cpp
@@ -27,3 +27,35 @@ bool isCircularSentence(string sentence) {
             return false;
     return sentence[0] == sentence.back();
 }
+
+// DAY 3 (796. Rotate String)================================================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
+vector<int> getLpsArray(string &s) {
+    vector<int> lps(s.size(), 0);
+    int len = 0;
+    for(int idx = 1; idx < s.size(); ) {
+        if(s[idx] == s[len])
+            lps[idx++] = ++len;
+        else {
+            if(len)
+                len = len? lps[len-1] : 0;
+            else idx++;
+        }
+    }
+    return lps;
+}
+
+bool rotateString(string s, string goal) {
+    if(s.size() != goal.size())
+        return false;
+    string kmpStr = goal + "#" + s + s;
+    vector<int> lps = getLpsArray(kmpStr);
+    for(int idx = 0; idx < lps.size(); idx++) {
+        if(lps[idx] == goal.size())
+            return true;
+    }
+    return false;
+}
