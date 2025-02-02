a38621cab7d873a2ee56525d16ea618ee272da35
Mukul Taneja <mkltaneja@gmail.com>
2025-02-02 20:36:44 +0530

    DAY 2

--- Leetcode - 30-days Challenge 2025/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2025/FEBRUARY Challenge.cpp
@@ -12,3 +12,19 @@ bool isArraySpecial(vector<int>& nums) {
     }
     return true;
 }
+
+// DAY 2 (1752. Check if Array Is Sorted and Rotated)======================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+bool check(vector<int>& nums) {
+    int size = nums.size();
+    int partitionCount = 0;
+    for(int idx = 0; idx < size; idx++) {
+        if(nums[idx] > nums[(idx+1) % size]) {
+            partitionCount++;
+        }
+    }
+    return partitionCount < 2;
+}
