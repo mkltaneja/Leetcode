4665e38504fc724a0f58e9c51c535f6225e58cb5
Mukul Taneja <mkltaneja@gmail.com>
2025-02-21 18:53:34 +0530

    DAY 21

--- Leetcode - 30-days Challenge 2025/FEBRUARY Challenge.cpp
+++ Leetcode - 30-days Challenge 2025/FEBRUARY Challenge.cpp
@@ -595,3 +595,45 @@ string findDifferentBinaryString(vector<string>& nums) {
     }
     return ans;
 }
+
+// DAY 21 (1261. Find Elements in a Contaminated Binary Tree)===================================================================
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+
+    void createTree(TreeNode* &node, int val) {
+        if(!node) {
+            return;
+        }
+        node->val = val;
+        treeNodes.insert(val);
+        createTree(node->left, 2*val + 1);
+        createTree(node->right, 2*val + 2);
+    }
+
+    unordered_set<int> treeNodes;
+    FindElements(TreeNode* root) {
+        createTree(root, 0);
+    }
+    
+    bool find(int target) {
+        return treeNodes.count(target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
