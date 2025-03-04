5310d9b0e07321809659d7c20e044b776a79e203
Mukul Taneja <mkltaneja@gmail.com>
2025-03-05 00:18:39 +0530

    DAY 4

--- Leetcode - 30-days Challenge 2025/MARCH Challenge.cpp
+++ Leetcode - 30-days Challenge 2025/MARCH Challenge.cpp
@@ -80,3 +80,65 @@ vector<int> pivotArray(vector<int>& nums, int pivot) {
     
     return nums;
 }
+
+// DAY 4 (1780. Check if Number is a Sum of Powers of Three)=========================================================
+
+// APPROACH 1 (Recursive)
+
+// Time Complexity = O(2^log3(n)) = O(n)
+// Space Complexity = O(log3(n))
+
+bool checkPowersOfThreeDFS(int n, int prod) {
+    if(n == 0) {
+        return true;
+    }
+    if(prod > n) {
+        return false;
+    }
+    
+    bool includeProd = checkPowersOfThreeDFS(n - prod, prod * 3);
+    bool excludeProd = checkPowersOfThreeDFS(n, prod * 3);
+
+    return includeProd || excludeProd;
+}
+
+bool checkPowersOfThree(int n) {
+    return checkPowersOfThreeDFS(n, 1);
+}
+
+// APPROACH 2 (Iterative)
+
+// Time Complexity = O(log3(n))
+// Space Complexity = O(1)
+
+bool checkPowersOfThree(int n) {
+    int prod = 1;
+    while(prod * 3 <= n) {
+        prod *= 3;
+    }
+    while(prod) {
+        if(n / prod) {
+            n -= prod;
+        }
+        if(n / prod) {
+            return false;
+        }
+        prod /= 3;
+    }
+    return true;
+}
+
+// APPROACH 3 (Iterative - Ternary Representation) --> [OPTIMIZED]
+
+// Time Complexity = O(log3(n))
+// Space Complexity = O(1)
+
+bool checkPowersOfThree(int n) {
+    while(n) {
+        if(n % 3 == 2) {
+            return false;
+        }
+        n /= 3;
+    }
+    return true;
+}
