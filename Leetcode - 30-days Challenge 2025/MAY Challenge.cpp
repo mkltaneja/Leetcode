5cd2a514585065ef4c737ab0347072071f3a6b3a
Mukul Taneja <mkltaneja@gmail.com>
2025-05-03 02:28:21 +0530

    MAY DAY 2

---
+++ Leetcode - 30-days Challenge 2025/MAY Challenge.cpp
@@ -0,0 +1,42 @@
+
+// DAY 2 (838. Push Dominoes)==============================================================================================
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+void fillDominoes(char op, int st, int end, string &dominoes) {
+    for(int idx = st; idx <= end; idx++) {
+        dominoes[idx] = op;
+    }
+}
+
+string pushDominoes(string dominoes) {
+    int size = dominoes.size();
+    char prevOp = '#';
+    for(int idx = 0; idx < size; idx++) {
+        if(dominoes[idx] == '.') {
+            int tempItr = idx;
+            while(tempItr < size && dominoes[tempItr] == '.') {
+                tempItr++;
+            }
+            char currOp = tempItr == size? '#' : dominoes[tempItr];
+            if((prevOp == 'R' && currOp != 'L') || (prevOp != 'R' && currOp == 'L')) {
+                char op = prevOp == 'R'? prevOp : currOp;
+                fillDominoes(op, idx, tempItr-1, dominoes);
+            }
+            else if(prevOp == 'R' && currOp == 'L') {
+                int intervalSize = tempItr-idx;
+                int halfInterval = intervalSize/2;
+                int st1 = idx, st2 = idx + halfInterval + (intervalSize & 1);
+                int end1 = st1 + halfInterval - 1, end2 = st2 + halfInterval - 1;
+                fillDominoes('R', st1, end1, dominoes);
+                fillDominoes('L', st2, end2, dominoes);
+            }
+            idx = tempItr;
+            prevOp = currOp;
+        } else {
+            prevOp = dominoes[idx];
+        }
+    }
+    return dominoes;
+}
